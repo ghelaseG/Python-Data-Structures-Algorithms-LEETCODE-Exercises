bb3a9450d251dcefab78ca32a71e3220eb22b034
ghelaseG <ghelase.georgian@gmail.com>
2024-04-15 18:12:55 +0100

    7th exercise

--- LL Interview/LeetCode Exercises/reverse_between.py
+++ LL Interview/LeetCode Exercises/reverse_between.py
@@ -76,32 +76,56 @@ class LinkedList:
         self.head = None
         self.length = 0
 
-    def reverse_between(self, start_index, end_index) -> int:
+    def reverse_between(self, start_index, end_index):
+        #we check if the list is empty
+        if self.length <= 1:
+            return None
+        #creating this dummy node, will simplify our code
         dummy = Node(0)
         dummy.next = self.head
 
-        left_previous = dummy
-        current = self.head
+        previous_node = dummy
 
-        for i in range(self.length):
-            if i < start_index - 1:
-                left_previous = left_previous.next
-                current = current.next
+        for _ in range(start_index):
+            previous_node = previous_node.next
 
-            sublist = current
+        current = previous_node.next
 
-            previous = None
+        for _ in range(end_index - start_index):
+            node_to_move = current.next
+            current.next = node_to_move.next
+            node_to_move.next = previous_node.next
+            previous_node.next = node_to_move
 
-        for i in range(self.length):
-            if i <= end_index:
-                next = current
-                current.next = previous
-                previous = current
-                current = next
-
-        left_previous.next = previous
-        sublist.next = current
-
-        return dummy.next
-
+        self.head = dummy.next
         
+linked_list = LinkedList(1)
+linked_list.append(2)
+linked_list.append(3)
+linked_list.append(4)
+linked_list.append(5)
+
+print("Original linked list: ")
+linked_list.print_list()
+
+# Reverse a sublist within the linked list
+linked_list.reverse_between(2, 4)
+print("Reversed sublist (2, 4): ")
+linked_list.print_list()
+
+# Reverse another sublist within the linked list
+linked_list.reverse_between(0, 4)
+print("Reversed entire linked list: ")
+linked_list.print_list()
+
+# Reverse a sublist of length 1 within the linked list
+linked_list.reverse_between(3, 3)
+print("Reversed sublist of length 1 (3, 3): ")
+linked_list.print_list()
+
+# Reverse an empty linked list
+empty_list = LinkedList(0)
+empty_list.make_empty
+empty_list.reverse_between(0, 0)
+print("Reversed empty linked list: ")
+empty_list.print_list()
