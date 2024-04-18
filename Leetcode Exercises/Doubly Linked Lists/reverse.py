03cb081abbba5dc55c0f752a26dab95ebd84aaf8
ghelaseG <ghelase.georgian@gmail.com>
2024-04-18 23:10:53 +0100

    2nd exercise

--- Leetcode Exercises/Doubly Linked Lists/reverse.py
+++ Leetcode Exercises/Doubly Linked Lists/reverse.py
@@ -9,7 +9,7 @@ class DoublyLinkedList:
         new_node = Node(value)
         self.head = new_node
         self.tail = new_node
-        self.length +=1
+        self.length = 1
     
     def print_list(self):
         temp = self.head
@@ -18,7 +18,7 @@ class DoublyLinkedList:
             temp = temp.next
 
     def append(self, value):
-        new_node = Node(0)
+        new_node = Node(value)
         if self.head is None:
             self.head = new_node
             self.tail = new_node
@@ -30,6 +30,8 @@ class DoublyLinkedList:
         return True
 
     def reverse(self):
+        """
+        - first way:
         #set the current node to head
         current = self.head
         
@@ -40,5 +42,38 @@ class DoublyLinkedList:
             current.prev = temp_next
             current = temp_next
 
-        self.head, self.tail = self.tail, self.head
+        self.head, self.tail = self.tail, self.head"""
+
+        temp = self.head
+        while temp is not None:
+            #swap the prev and next pointers,
+            #this reverses the link direction for the node,
+            #prev becomes next, and vice versa
+            temp.prev, temp.next = temp.next, temp.prev
+
+            #move to the next node
+            #because we reverse the pointers, prev is actually next
+            temp = temp.prev
+
+            #swap the head and tail pointers
+            self.head, self.tail = self.tail, self.head
+
+
+my_doubly_linked_list = DoublyLinkedList(1)
+my_doubly_linked_list.append(2)
+my_doubly_linked_list.append(3)
+my_doubly_linked_list.append(4)
+my_doubly_linked_list.append(5)
+
+
+print('DLL before reverse():')
+my_doubly_linked_list.print_list()
+
+
+my_doubly_linked_list.reverse()
+
+
+print('\nDLL after reverse():')
+my_doubly_linked_list.print_list()
+
 
