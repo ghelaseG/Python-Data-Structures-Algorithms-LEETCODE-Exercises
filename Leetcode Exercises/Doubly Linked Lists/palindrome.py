2e1997fcadef405b342e36277b4da2f5c08e89a3
ghelaseG <ghelase.georgian@gmail.com>
2024-04-20 01:44:10 +0100

    exercise nr 3

--- Leetcode Exercises/Doubly Linked Lists/palindrome.py
+++ Leetcode Exercises/Doubly Linked Lists/palindrome.py
@@ -38,9 +38,14 @@ class DoublyLinkedList:
         return True
     
     def is_palindrome(self):
-        temp = self.head
-        while temp is not None:
-            if temp.prev, temp.next == temp.next, temp.prev:
-                return True
-            else:
+        if self.length <= 1:
+            return True
+        
+        forward_node = self.head
+        backward_node = self.tail
+        for _ in range(self.length // 2):
+            if forward_node.value != backward_node.value:
                 return False
+        forward_node = forward_node.next
+        backward_node = backward_node.prev
+        return True
