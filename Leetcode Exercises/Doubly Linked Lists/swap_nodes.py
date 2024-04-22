5e6ead2dcbd8d407cde9408f581f5e95942c0aa1
ghelaseG <ghelase.georgian@gmail.com>
2024-04-22 22:47:35 +0100

    exercise 4, comments + visualisation link

--- Leetcode Exercises/Doubly Linked Lists/swap_nodes.py
+++ Leetcode Exercises/Doubly Linked Lists/swap_nodes.py
@@ -16,6 +16,9 @@ Note: You must solve the problem WITHOUT MODIFYING THE VALUES in the list's node
 
 """
 
+# for a better understanding, please use this link: 
+# https://pythontutor.com/render.html#code=class%20Node%3A%0A%20%20%20%20def%20__init__%28self,%20value%29%3A%0A%20%20%20%20%20%20%20%20self.value%20%3D%20value%0A%20%20%20%20%20%20%20%20self.next%20%3D%20None%0A%20%20%20%20%20%20%20%20self.prev%20%3D%20None%0A%20%20%20%20%20%20%20%20%0A%0Aclass%20DoublyLinkedList%3A%0A%20%20%20%20def%20__init__%28self,%20value%29%3A%0A%20%20%20%20%20%20%20%20new_node%20%3D%20Node%28value%29%0A%20%20%20%20%20%20%20%20self.head%20%3D%20new_node%0A%20%20%20%20%20%20%20%20self.length%20%3D%201%0A%0A%20%20%20%20def%20print_list%28self%29%3A%0A%20%20%20%20%20%20%20%20output%20%3D%20%5B%5D%0A%20%20%20%20%20%20%20%20current_node%20%3D%20self.head%0A%20%20%20%20%20%20%20%20while%20current_node%20is%20not%20None%3A%0A%20%20%20%20%20%20%20%20%20%20%20%20output.append%28str%28current_node.value%29%29%0A%20%20%20%20%20%20%20%20%20%20%20%20current_node%20%3D%20current_node.next%0A%20%20%20%20%20%20%20%20print%28%22%20%3C-%3E%20%22.join%28output%29%29%0A%20%20%20%20%20%20%20%20%0A%20%20%20%20def%20append%28self,%20value%29%3A%0A%20%20%20%20%20%20%20%20new_node%20%3D%20Node%28value%29%0A%20%20%20%20%20%20%20%20if%20self.head%20is%20None%3A%0A%20%20%20%20%20%20%20%20%20%20%20%20self.head%20%3D%20new_node%0A%20%20%20%20%20%20%20%20else%3A%0A%20%20%20%20%20%20%20%20%20%20%20%20temp%20%3D%20self.head%0A%20%20%20%20%20%20%20%20%20%20%20%20while%20temp.next%20is%20not%20None%3A%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20temp%20%3D%20temp.next%0A%20%20%20%20%20%20%20%20%20%20%20%20temp.next%20%3D%20new_node%0A%20%20%20%20%20%20%20%20%20%20%20%20new_node.prev%20%3D%20temp%0A%20%20%20%20%20%20%20%20self.length%20%2B%3D%201%0A%20%20%20%20%20%20%20%20return%20True%0A%0A%20%20%20%20def%20swap_pairs%28self%29%3A%0A%20%20%20%20%20%20%20%20dummy%20%3D%20Node%280%29%0A%20%20%20%20%20%20%20%20dummy.next%20%3D%20self.head%0A%0A%20%20%20%20%20%20%20%20previous%20%3D%20dummy%0A%0A%20%20%20%20%20%20%20%20while%20self.head%20is%20not%20None%20and%20self.head.next%20is%20not%20None%3A%0A%20%20%20%20%20%20%20%20%20%20%20%20first_node%20%3D%20self.head%0A%20%20%20%20%20%20%20%20%20%20%20%20second_node%20%3D%20self.head.next%0A%20%20%20%20%20%20%20%20%20%20%20%20previous.next%20%3D%20second_node%0A%20%20%20%20%20%20%20%20%20%20%20%20first_node.next%20%3D%20second_node.next%0A%20%20%20%20%20%20%20%20%20%20%20%20second_node.next%20%3D%20first_node%0A%20%20%20%20%20%20%20%20%20%20%20%20second_node.prev%20%3D%20previous%0A%20%20%20%20%20%20%20%20%20%20%20%20first_node.prev%20%3D%20second_node%0A%20%20%20%20%20%20%20%20%20%20%20%20%0A%20%20%20%20%20%20%20%20%20%20%20%20if%20first_node.next%20is%20not%20None%3A%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20first_node.next.prev%20%3D%20first_node%0A%20%20%20%20%20%20%20%20%20%20%20%20self.head%20%3D%20first_node.next%0A%20%20%20%20%20%20%20%20%20%20%20%20previous%20%3D%20first_node%0A%20%20%20%20%20%20%20%20%20%20%20%20%0A%20%20%20%20%20%20%20%20self.head%20%3D%20dummy.next%0A%20%20%20%20%20%20%20%20%20%20%20%20%0A%20%20%20%20%20%20%20%20if%20self.head%20is%20not%20None%3A%0A%20%20%20%20%20%20%20%20%20%20%20%20self.head.prev%20%3D%20None%0A%0A%0A%0Amy_dll%20%3D%20DoublyLinkedList%281%29%0Amy_dll.append%282%29%0Amy_dll.append%283%29%0Amy_dll.append%284%29%0A%0Aprint%28'my_dll%20before%20swap_pairs%3A'%29%0Amy_dll.print_list%28%29%0A%0Amy_dll.swap_pairs%28%29%20%0A%0A%0Aprint%28'my_dll%20after%20swap_pairs%3A'%29%0Amy_dll.print_list%28%29&cumulative=false&curInstr=104&heapPrimitives=nevernest&mode=display&origin=opt-frontend.js&py=3&rawInputLstJSON=%5B%5D&textReferences=false
+
 class Node:
     def __init__(self, value):
         self.value = value
@@ -33,7 +36,7 @@ class DoublyLinkedList:
         current_node = self.head
         while current_node is not None:
             output.append(str(current_node.value))
-            current_node = current_node.value
+            current_node = current_node.next
         print("< - >".join(output))
     
     def append(self, value):
@@ -79,7 +82,7 @@ class DoublyLinkedList:
                exp: 2 <-> 1 -> 3 <-> 4"""
             second_node.next = first_node
             """let's update the previous pointers for a doubly ll,
-               exp: """
+               exp: 0 <-> 2 <-> 1 <-> 3 <-> 4"""
             second_node.prev = previous
             first_node.prev = second_node
             
@@ -99,3 +102,17 @@ LL: 1 <-> 2 <-> 3 <-> 4
 Result: 2 <-> 1 <-> 4 <-> 3
 
 """
+
+my_dll = DoublyLinkedList(1)
+my_dll.append(2)
+my_dll.append(3)
+my_dll.append(4)
+
+print('my_dll before swap pairs: ')
+my_dll.print_list()
+
+my_dll.swap_pairs()
+
+print('my_dll after swap pairs: ')
+
+my_dll.print_list()
