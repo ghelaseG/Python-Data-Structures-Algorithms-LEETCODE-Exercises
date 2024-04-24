ef926f42d9722aad6d5838acfa05d23de68ddc63
ghelaseG <ghelase.georgian@gmail.com>
2024-04-24 10:58:15 +0100

    stack_list

--- Leetcode Exercises/Stack/stack_list.py
+++ Leetcode Exercises/Stack/stack_list.py
@@ -1,3 +1,7 @@
+"""
+Create a constructor for Class Stack that implements a new stack with an empty list called stack_list
+"""
+
 class Stack:
     def __init__(self):
         self.stack_list = []
