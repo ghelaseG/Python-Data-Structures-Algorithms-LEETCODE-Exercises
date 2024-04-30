fff604af274c6f7e9b7db4e9802954bf51add3da
ghelaseG <ghelase.georgian@gmail.com>
2024-04-30 23:59:12 +0100

    HT - exercise4

--- Leetcode Exercises/Hash Tables/find_duplicates.py
+++ Leetcode Exercises/Hash Tables/find_duplicates.py
@@ -3,13 +3,18 @@ Given an array of integers nums, find all the duplicates in the array using a ha
 """
 
 def find_duplicates(nums):
-    array = []
+    num_counts = {} #we first initialize a dict to count numbers
 
-    if array == None:
-        return False
-    
     for i in nums:
-        for j in nums:
-            if i == j:
-                array += i
+        num_counts[i] = num_counts.get(num, 0) + 1 #here we check how many times each number appears in the list
+    
+    duplicates = [] #here we store the duplicates
+
+    for num, count in num_counts.items():
+        if count > 1:
+            duplicates.append(num)
+
+    return duplicates
+
+
     
