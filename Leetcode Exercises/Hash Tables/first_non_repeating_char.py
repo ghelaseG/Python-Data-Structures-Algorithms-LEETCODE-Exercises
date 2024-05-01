7f01242fd631bd1651847e02dbb4ce763e8f6908
ghelaseG <ghelase.georgian@gmail.com>
2024-05-01 15:52:40 +0100

    HT - exercise5, trial

--- Leetcode Exercises/Hash Tables/first_non_repeating_char.py
+++ Leetcode Exercises/Hash Tables/first_non_repeating_char.py
@@ -5,3 +5,15 @@ Write a function called first_non_repeating_char(string) that finds the first no
 
 For example, if the input string is "leetcode", the function should return "l" because "l" is the first character that appears only once in the string. Similarly, if the input string is "hello", the function should return "h" because "h" is the first non-repeating character in the string.
 """
+
+def first_non_repeating_char(letters):
+    check_letters = {}
+
+    for letter in letters:
+        check_letters[letter] = check_letters.get(letter, 0) + 1
+
+    for letter, count in check_letters.items():
+        if count = 1:
+            print(letter)
+        else:
+            return None
