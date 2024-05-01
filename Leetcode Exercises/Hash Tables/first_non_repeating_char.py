425bf64f59ca3484c969f1735c5e3e20af7bf48d
ghelaseG <ghelase.georgian@gmail.com>
2024-05-01 15:28:09 +0100

    HT - exercise5

---
+++ Leetcode Exercises/Hash Tables/first_non_repeating_char.py
@@ -0,0 +1,7 @@
+"""
+You have been given a string of lowercase letters.
+
+Write a function called first_non_repeating_char(string) that finds the first non-repeating character in the given string using a hash table (dictionary). If there is no non-repeating character in the string, the function should return None.
+
+For example, if the input string is "leetcode", the function should return "l" because "l" is the first character that appears only once in the string. Similarly, if the input string is "hello", the function should return "h" because "h" is the first non-repeating character in the string.
+"""
