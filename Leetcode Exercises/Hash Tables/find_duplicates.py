bd3ea68036c759c47872238e9b98e25defe990c6
ghelaseG <ghelase.georgian@gmail.com>
2024-05-01 12:49:07 +0100

    HT - exercise4, comments

--- Leetcode Exercises/Hash Tables/find_duplicates.py
+++ Leetcode Exercises/Hash Tables/find_duplicates.py
@@ -6,15 +6,25 @@ def find_duplicates(nums):
     num_counts = {} #we first initialize a dict to count numbers
 
     for i in nums:
-        num_counts[i] = num_counts.get(i, 0) + 1 #here we check how many times each number appears in the list
-    
+        num_counts[i] = num_counts.get(i, 0) + 1 
+        # if for example, the number is not in the dict, the key it will be the same, and the value it will increment by 1
+        # after this, if we get the same i, let's say our list is [1,1,1,2], then the second i= it will be again 1,
+        # but now this key, got the value 1, from previous itteration, so then will increment by 1, it will become, num_counts = {1, 2}
     duplicates = [] #here we store the duplicates
 
     for num, count in num_counts.items():
+        # now we continue our example, here we iterate through let's say {1, 2} - the items of the dictionary,
+        # therefore, count will be = 2, now if 2 > 1, the value of the key, then we add it to our empty array called 'duplicates'
         if count > 1:
             duplicates.append(num)
 
     return duplicates
 
 
-    
+print( find_duplicates( [1, 2, 3, 4, 5]) )
+print( find_duplicates([1, 1, 2, 2, 3]) )
+print( find_duplicates([1,1,1,1,1]) )
+print( find_duplicates([1,2,3,3,3,4,4,5]) )
+print( find_duplicates([1,1,2,2,2,3,3,3,3]) )
+print( find_duplicates([1,1,1,2,2,2,3,3,3,3]) )
+print( find_duplicates([]) )
