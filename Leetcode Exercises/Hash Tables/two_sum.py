e6b042f960de9469286d8e74eb621518ac9b0d7f
ghelaseG <ghelase.georgian@gmail.com>
2024-05-02 17:53:33 +0100

    HT - exercise 7

---
+++ Leetcode Exercises/Hash Tables/two_sum.py
@@ -0,0 +1,21 @@
+"""
+Problem:
+Given an array of integers nums and a target integer target, find the indices of two numbers in the array that add up to the target.
+
+The main challenge here is to implement this function in one pass through the array. This means you should not iterate over the array more than once. Therefore, your solution should have a time complexity of O(n), where n is the number of elements in nums.
+
+
+
+Input:
+
+A list of integers nums .
+
+A target integer target.
+
+
+
+Output:
+
+A list of two integers representing the indices of the two numbers in the input array nums that add up to the target. If no two numbers in the input array add up to the target, return an empty list [].
+"""
+
