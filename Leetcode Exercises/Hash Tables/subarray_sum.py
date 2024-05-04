218282f8512992b27b382d805a29cf5587aa32f5
ghelaseG <ghelase.georgian@gmail.com>
2024-05-04 23:54:03 +0100

    HT - exercise 8, trial

--- Leetcode Exercises/Hash Tables/subarray_sum.py
+++ Leetcode Exercises/Hash Tables/subarray_sum.py
@@ -12,16 +12,46 @@ Your function should return a list of two integers representing the starting and
 """
 
 def subarray_sum(nums, target):
-    res = 0
-    curSum = 0
-    prefSum = {0:1}
+    #let's create a dict to store the sums as keys with their index
+    sum_index = {0: -1}
+    # 0: will be the default sum when we search for subarrays
+    #-1: shows that there's no subarray yet
+    ## this setup helps handle special cases, for example when the first element itself is equal to the target
 
-    for i in nums:
-        curSum += i
-        diff = curSum - target
+    current_sum = 0 #here we keep track of the running sum, as we iterate through the array
 
-        res += prefSum.get(diff, 0)
-        prefSum[curSum] = 1 + prefSum.get(curSum, 0)
+    for i, num in enumerate(nums):
+        current_sum += num #here we update the current sum by adding the current 'num'
+
+        #we then check if a subarray exists with the sum that we need
+        ##we basically check if sum - target is a key in the sum_index
+        if current_sum - target in sum_index:
+            #if it is, then we have found the subarray, and
+            ##we return the indices as a list
+            ###we add 1 to move past the element before the subarray starts
+            ####i is the end index
+            return [sum_index[current_sum - target]+1, i]
+        
+        #if we haven't yet found it, we add the current sum and its index to our dict
+        sum_index[current_sum] = i
+
+        #and if there's no subarray, we return an empty list
+        return []
     
-    return res
+
+nums = [1, 2, 3, 4, 5]
+target = 9
+print( subarray_sum(nums, target))
+
+nums = [-1, 2, 3, -4, 5]
+target = 0
+print(subarray_sum(nums, target))
+
+nums = [2,3,4,5,6]
+target = 3
+print( subarray_sum(nums, target))
+
+nums = []
+target = 0
+print(subarray_sum(nums, target))
 
