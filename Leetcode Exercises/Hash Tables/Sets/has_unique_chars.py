4a297feab1d87954ba5b17b5561ed7aa639b9919
ghelaseG <ghelase.georgian@gmail.com>
2024-05-06 20:45:10 +0100

    Sets - exercise2

--- Leetcode Exercises/Hash Tables/Sets/has_unique_chars.py
+++ Leetcode Exercises/Hash Tables/Sets/has_unique_chars.py
@@ -4,5 +4,14 @@ Write a function called has_unique_chars that takes a string as input and return
 For example, has_unique_chars('abcdefg') should return True, while has_unique_chars('hello') should return False.
 """
 
-def has_unique_chars(string):
-    
+def has_unique_chars(my_string):
+    if len(set(my_string)) != len(my_string):
+        return False
+    else:
+        return True
+
+print(has_unique_chars('abcdefg'))
+print(has_unique_chars('hello'))
+print(has_unique_chars(''))
+print(has_unique_chars('0123456789'))
+print(has_unique_chars('abacadaeaf'))
