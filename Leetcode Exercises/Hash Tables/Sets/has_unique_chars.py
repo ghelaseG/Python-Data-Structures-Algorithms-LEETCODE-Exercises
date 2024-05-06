f56011d8da4a889f7c79e799c7a7e4fd929f316e
ghelaseG <ghelase.georgian@gmail.com>
2024-05-06 20:27:26 +0100

    Sets - exercise2

---
+++ Leetcode Exercises/Hash Tables/Sets/has_unique_chars.py
@@ -0,0 +1,8 @@
+"""
+Write a function called has_unique_chars that takes a string as input and returns True if all the characters in the string are unique, and False otherwise.
+
+For example, has_unique_chars('abcdefg') should return True, while has_unique_chars('hello') should return False.
+"""
+
+def has_unique_chars(string):
+    
