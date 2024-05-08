a70ad2289765fd836d1e339cd4a29fcdebda0dec
ghelaseG <ghelase.georgian@gmail.com>
2024-05-08 18:51:20 +0100

    Heaps - sink down

--- Data Structures/Heaps/sink_down.py
+++ Data Structures/Heaps/sink_down.py
@@ -21,19 +21,6 @@ class MaxHeap:
         while current > 0 and self.heap[current] > self.heap[self._parent(current)]:
             self._swap(current, self._parent(current))
             current = self._parent(current)
-    
-    def remove(self):
-        if len(self.heap) == 0:
-            return None
-        
-        if len(self.heap) == 1:
-            return self.heap.pop()
-        
-        max_value = self.heap[0]
-        self.heap[0] = self.heap.pop()
-        self._sink_down(0)
-
-        return max_value
 
     def _sink_down(self, index):
         max_index = index
@@ -54,6 +41,21 @@ class MaxHeap:
                 index = max_index
             else:
                 return
+    
+    def remove(self):
+        if len(self.heap) == 0:
+            return None
+        
+        if len(self.heap) == 1:
+            return self.heap.pop()
+        
+        max_value = self.heap[0]
+        self.heap[0] = self.heap.pop()
+        self._sink_down(0)
+
+        return max_value
+
+    
             
 
 my_heap = MaxHeap()
