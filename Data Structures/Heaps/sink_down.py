9013c7bc29dce8f459783b93833b5a8eaa230621
ghelaseG <ghelase.georgian@gmail.com>
2024-05-08 17:45:27 +0100

    Heaps - sink down

--- Data Structures/Heaps/sink_down.py
+++ Data Structures/Heaps/sink_down.py
@@ -1,19 +1,77 @@
-def _sink_down(self, index):
-    max_index = index
-    while True:
-        left_index = self._left_child(index)
-        right_index = self._right_child(index)
+class MaxHeap:
+    def __init__(self):
+        self.heap = {}
 
-        if (left_index < len(self.heap) and self.heap[left_index] >
-            self.heap[max_index]):
-            max_index = left_index
+    def _left_child(self, index):
+        return 2 * index + 1
+    
+    def _right_child(self, index):
+        return 2 * index + 2
+    
+    def _parent(self, index):
+        return (index - 1) // 2
+    
+    def _swap(self, index1, index2):
+        self.heap[index1], self.heap[index2] = self.heap[index2], self.heap[index1] 
 
-        if (right_index < len(self.heap) and self.heap[right_index] >
-            self.heap[max_index]):
-            max_index = right_index
+    def insert(self, value):
+        index = len(self.heap)
+        self.heap.append(value)
+        parent = self._parent(index)
 
-        if max_index != index:
-            self._swap(index, max_index)
-            index = max_index
-        else:
-            return
+        while index > 0 and self.heap[parent] < value:
+            self._swap(index, parent)
+            parent = self._parent(index)
+    
+    def remove(self):
+        if len(self.heap) == 0:
+            return None
+        
+        if len(self.heap) == 1:
+            return self.heap.pop()
+        
+        max_value = self.heap[0]
+        self.heap[0] = self.heap.pop()
+        self._sink_down(0)
+
+        return max_value
+
+    def _sink_down(self, index):
+        max_index = index
+        while True:
+            left_index = self._left_child(index)
+            right_index = self._right_child(index)
+
+            if (left_index < len(self.heap) and self.heap[left_index] >
+                self.heap[max_index]):
+                max_index = left_index
+
+            if (right_index < len(self.heap) and self.heap[right_index] >
+                self.heap[max_index]):
+                max_index = right_index
+
+            if max_index != index:
+                self._swap(index, max_index)
+                index = max_index
+            else:
+                return
+            
+
+my_heap = MaxHeap()
+my_heap.insert(95)
+my_heap.insert(75)
+my_heap.insert(80)
+my_heap.insert(55)
+my_heap.insert(60)
+my_heap.insert(50)
+my_heap.insert(65)
+
+print(my_heap.heap)
+
+my_heap.remove()
+
+print(my_heap.heap)
+
+my_heap.remove()
+
+print(my_heap.heap)
