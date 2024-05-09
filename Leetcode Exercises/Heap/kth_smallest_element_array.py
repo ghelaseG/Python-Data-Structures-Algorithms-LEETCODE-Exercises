853b253d34e81d38846565b5fb6c1dd465a2a36b
ghelaseG <ghelase.georgian@gmail.com>
2024-05-09 17:13:58 +0100

    Heaps - exercise 1, test cases

--- Leetcode Exercises/Heap/kth_smallest_element_array.py
+++ Leetcode Exercises/Heap/kth_smallest_element_array.py
@@ -97,4 +97,22 @@ class MaxHeap:
     
 
 def find_kth_smallest(nums, k):
+    nums = nums.sort()
     
+    for i in range(len(nums)):
+        if i == k:
+            return i
+        
+#Test cases
+nums = [[3,2,1,5,6,4],[6,5,4,3,2,1],[1,2,3,4,5,6],[3,2,3,1,2,4,5,5,6]]
+kth = [2,3,4,7]
+expected_outputs = [2,3,4,5]
+
+for i in range(len(nums)):
+    print(f'Test case {i+1}...')
+    print(f'Input: {nums[i]} with k = {ks[i]}')
+    result = find_kth_smallest(nums[i], ks[i])
+    print(f'Output: {result}')
+    print(f'Expected output: {expected_outputs[i]}')
+    print(f'Test passed: {result == expected_outputs[i]}')
+    print('-------------------------------')
