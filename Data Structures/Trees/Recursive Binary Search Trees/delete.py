31dbb02e30837ba1adc718ea44a75cf023295f5d
ghelaseG <ghelase.georgian@gmail.com>
2024-05-13 23:58:35 +0100

    Recursive Binary Search Tree - delete(part1)

---
+++ Data Structures/Trees/Recursive Binary Search Trees/delete.py
@@ -0,0 +1,15 @@
+def __delete_node(self, current_node, value):
+    if current_node == None:
+        return None
+    if value < current_node.value:
+        current_node.left = self.__delete_node(current_node.left, value)
+    elif value > current_node.value:
+        current_node.right = self.__delete_node(current_node.right, value)
+    else:
+        
+    return current_node
+
+
+
+def delete_node(self, value):
+    self.root = self.__delete_node(self.root, value)
