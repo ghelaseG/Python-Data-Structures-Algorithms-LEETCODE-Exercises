bf6194155b5fcbf03d3cd6d91ba2356e4d42e601
ghelaseG <ghelase.georgian@gmail.com>
2024-05-14 13:37:13 +0100

    Recursive Binary Search Tree - delete(part2 - traverse to the left)

--- Data Structures/Trees/Recursive Binary Search Trees/delete.py
+++ Data Structures/Trees/Recursive Binary Search Trees/delete.py
@@ -6,7 +6,13 @@ def __delete_node(self, current_node, value):
     elif value > current_node.value:
         current_node.right = self.__delete_node(current_node.right, value)
     else:
-        
+        if current_node.left == None and current_node.right == None:
+            return None
+        elif current_node.left == None:
+            current_node = current_node.right
+        elif current_node.right == None:
+            current_node = current_node.left
+        else:
     return current_node
 
 
