10338a542237b6286b42deb1ab39cb2e2f0908bf
ghelaseG <ghelase.georgian@gmail.com>
2024-05-16 20:45:01 +0100

    Recursive Binary Search Tree - exercise2, notes

---
+++ Leetcode Exercises/Binary Search Tree/invert_tree.py
@@ -0,0 +1,18 @@
+"""
+Objective: Write a method to invert (or mirror) a binary tree. This means that for every node in the binary tree, you will swap its left and right children.
+
+Requirements:
+
+The method must be recursive. It should work by traversing the tree and swapping the left and right children of every node encountered.
+
+If the input tree is empty (i.e., node is None), the method should return None.
+
+The inversion should happen in-place, meaning you should not create a new tree but instead modify the existing tree structure.
+
+The method should handle binary trees of any size and structure, ensuring that every node's left and right children are swapped.
+
+Note: This problem requires understanding binary trees, recursion, and the ability to manipulate tree nodes directly. The solution should ensure that every node's left and right children are swapped all the way down the tree, effectively creating a mirror image of the original structure.
+"""
+
+def __invert_tree(self, node):
+    
