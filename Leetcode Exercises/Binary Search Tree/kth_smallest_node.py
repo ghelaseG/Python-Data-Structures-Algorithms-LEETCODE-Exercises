8cb96f664fa1ae326835648b2ef0dd5e8e9d8b5c
ghelaseG <ghelase.georgian@gmail.com>
2024-05-20 23:39:10 +0100

    Binary Search Tree - kth smallest node - exercise 2,trial

--- Leetcode Exercises/Binary Search Tree/kth_smallest_node.py
+++ Leetcode Exercises/Binary Search Tree/kth_smallest_node.py
@@ -41,6 +41,17 @@ class BinarySearchTree:
                 temp = temp.right
     
     def kth_smallest(self):
+        def dfs_in_order(self):
+            results = []
+            def traverse(current_node):
+                if current_node.left is not None:
+                    traverse(current_node.left)
+                results.append(current_node.value)
+                if current_node.right is not None:
+                    traverse(current_node.right)
+            traverse(self.root)
+            return results
+
 
 
 bst = BinarySearchTree()
