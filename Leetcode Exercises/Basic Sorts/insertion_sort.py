7f0631ba1c5849c02b8698dd7dee2be56baed995
ghelaseG <ghelase.georgian@gmail.com>
2024-05-27 16:32:39 +0100

    Basic Sort - insertion sort - exercise 3, mock

--- Leetcode Exercises/Basic Sorts/insertion_sort.py
+++ Leetcode Exercises/Basic Sorts/insertion_sort.py
@@ -48,3 +48,49 @@ The method should be implemented in the LinkedList class.
 The method should not use any additional data structures to sort the linked list.
 
 """
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+    
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+
+    def insertion_sort(self):
+
+
+my_linked_list = LinkedList(4)
+my_linked_list.append(2)
+my_linked_list.append(6)
+my_linked_list.append(5)
+my_linked_list.append(1)
+my_linked_list.append(3)
+
+print("Linked List Before Sort:")
+my_linked_list.print_list()
+
+my_linked_list.insertion_sort()
+
+print("\nSorted linked list:")
+my_linked_list.print_list()
