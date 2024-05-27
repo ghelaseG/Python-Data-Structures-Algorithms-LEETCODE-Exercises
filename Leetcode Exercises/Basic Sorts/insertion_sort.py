b88199b0a288d8fa56cdf054ad076b3631574754
ghelaseG <ghelase.georgian@gmail.com>
2024-05-27 17:27:46 +0100

    Basic Sort - insertion sort - exercise 3

--- Leetcode Exercises/Basic Sorts/insertion_sort.py
+++ Leetcode Exercises/Basic Sorts/insertion_sort.py
@@ -87,13 +87,23 @@ class LinkedList:
         sorted_list_head.next = None
         
         while unsorted_list_head is not None:
-            if unsorted_list_head < sorted_list_head:
-                search_pointer = 
-                current = 
-                unsorted_elem, current = current, unsorted_elem
-            current = current.next
+            current= unsorted_list_head
+            unsorted_list_head = unsorted_list_head.next
             
-        return
+            if current.value < sorted_list_head.value:
+                current.next = sorted_list_head
+                sorted_list_head = current
+            else:
+                search_pointer = sorted_list_head
+                while search_pointer.next is not None and current.value > search_pointer.next.value:
+                    search_pointer = search_pointer.next
+                current.next = search_pointer.next
+                search_pointer.next = current
+        self.head = sorted_list_head
+        temp = self.head
+        while temp.next is not None:
+            temp = temp.next
+        self.tail = temp
         
 
 
