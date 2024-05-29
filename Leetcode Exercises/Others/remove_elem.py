9686dd7a55b2c04b8159da7b9369e8a628ba407b
ghelaseG <ghelase.georgian@gmail.com>
2024-05-29 23:04:26 +0100

    Leetcode Exercise - n.1

---
+++ Leetcode Exercises/Others/remove_elem.py
@@ -0,0 +1,27 @@
+"""
+Given a list of integers nums and an integer val, write a function remove_element that removes all occurrences of val in the list in-place and returns the new length of the modified list.
+
+The function should not allocate extra space for another list; instead, it should modify the input list in-place with O(1) extra memory.
+
+
+
+Input:
+
+A list of integers nums .
+
+An integer val representing the value to be removed from the list.
+
+
+
+Output:
+
+An integer representing the new length of the modified list after removing all occurrences of val.
+
+
+
+Constraints:
+
+Do not use any built-in list methods, except for pop() to remove elements.
+
+It is okay to have extra space at the end of the modified list after removing elements.
+"""
