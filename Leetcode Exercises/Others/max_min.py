726f346fed80ccb4d4279c09401a5ab8006b1711
ghelaseG <ghelase.georgian@gmail.com>
2024-05-30 22:03:29 +0100

    Leetcode Exercise - n.2

--- Leetcode Exercises/Others/max_min.py
+++ Leetcode Exercises/Others/max_min.py
@@ -12,11 +12,17 @@ The function should traverse the list and keep track of the current maximum and 
 
 For example, if the input list is [5, 3, 8, 1, 6, 9], the function should return (9, 1) since 9 is the maximum value and 1 is the minimum value.
 """
-
+#the way this program works, is very simple
+##we first initialize the variable max and min with the first element of the list, in our case is 5
+###after this we iterate through the list with the for loop and compare every element with those variable
+####for example, after we check 5, we then go to 3, and 3 < minimum(5), so then minimum becomes 3, while maximum is still 5, and so on
 def find_max_min(myList):
-    myList = sorted()
-    temp = []
-    for i in range(0, len(myList) + 1):
-        if i != 0:
-            temp = temp.append(myList[0], myList[-1])
-    myList = temp   
+    maximum = minimum = myList[0]
+    for num in myList:
+        if num > maximum:
+            maximum = num
+        elif num < minimum:
+            minimum = num
+    return maximum, minimum
+
+print(find_max_min([5,3,8,1,6,9])) 
