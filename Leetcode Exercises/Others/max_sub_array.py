5a459fe9230fafd3d2d161b0c1ac915e6b19af7a
ghelaseG <ghelase.georgian@gmail.com>
2024-06-04 07:52:36 +0100

    Leetcode Exercise - n.7

---
+++ Leetcode Exercises/Others/max_sub_array.py
@@ -0,0 +1,33 @@
+"""
+Given an array of integers nums, write a function max_subarray(nums) that finds the contiguous subarray (containing at least one number) with the largest sum and returns its sum.
+
+Remember to also account for an array with 0 items.
+
+
+
+Function Signature:
+
+def max_subarray(nums):
+
+
+Input:
+
+A list of integers nums.
+
+
+
+Output:
+
+An integer representing the sum of the contiguous subarray with the largest sum.
+
+
+
+Example:
+
+max_subarray([-2, 1, -3, 4, -1, 2, 1, -5, 4])
+Output: 6
+Explanation: The contiguous subarray [4, -1, 2, 1] has the largest sum, which is 6.
+"""
+
+def max_subarray(nums):
+    
