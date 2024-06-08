cdcf4ff4d27c81553532e4eec57050ca4cca6891
ghelaseG <ghelase.georgian@gmail.com>
2024-06-08 23:07:42 +0100

    Leetcode Exercise - n.1920 - Number of Good Pairs, fail

--- Leetcode Exercises/Array/1512.py
+++ Leetcode Exercises/Array/1512.py
@@ -31,8 +31,4 @@ from typing import List
 
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
-        count = 0
-        for i,j in enumerate(nums):
-            if nums[i] == nums[j]:
-                count += 1
-                return count
+        return [x for x,y in zip(nums) if x == y]
