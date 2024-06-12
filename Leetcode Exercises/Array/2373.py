1e7e03da5ee91a3c67ea0206e28d6fbd7b8d6698
ghelaseG <ghelase.georgian@gmail.com>
2024-06-12 23:37:46 +0100

    Leetcode Exercise - n.2373, Largest Local Value in a Matrix

--- Leetcode Exercises/Array/2373.py
+++ Leetcode Exercises/Array/2373.py
@@ -15,3 +15,18 @@ from typing import List
 
 class Solution:
     def largestLocal(self, grid: List[List[int]]) -> List[List[int]]:
+        n = len(grid)
+        #let's create a 2 dimensional matrix
+        ##we start by creating a zeros matrix 
+        result = [[0] * (n - 2) for _ in range(n-2)]
+        #next loop will populate the matrix
+        for i in range(n-2):
+            for j in range(n-2):
+                #here we check for the max value in our 3 by 3 matrix
+                for row in range(i, i + 3):
+                    for column in range(j, j + 3):
+                        result[i][j] = max(result[i][j], grid[row][column])
+        return result
+    
+example = Solution()
+print(example.largestLocal(grid=[[9,9,8,1],[5,6,2,6],[8,2,6,4],[6,2,2,2]]))
