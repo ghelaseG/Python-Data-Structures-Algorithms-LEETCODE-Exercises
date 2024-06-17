f8fde22cf02d220914c9d35c8abc7ed47189f0e2
ghelaseG <ghelase.georgian@gmail.com>
2024-06-17 22:42:18 +0100

    Leetcode Exercise - n.1 TwoSum, better time complexity

--- Leetcode Exercises/Array/1.py
+++ Leetcode Exercises/Array/1.py
@@ -9,13 +9,15 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        result = []
+        hashmap = {}
+
         for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+            complement = target - nums[i]
+            if complement in hashmap:
+                return [i, hashmap[complement]]
+            hashmap[nums[i]] = i
 
 example = Solution()
 print(example.twoSum(nums=[2,7,11,15], target=9))
 
-#Big(O) time complexity O(N**2)
+#Big(O) time complexity O(N)
