6a1badf5e1a3a57accca5f339f892a766800f3cb
ghelaseG <ghelase.georgian@gmail.com>
2024-06-19 21:51:29 +0100

    Leetcode Exercise - n.2824, Count Pairs Whose Sum is Less than Target

---
+++ Leetcode Exercises/Array/2824.py
@@ -0,0 +1,9 @@
+"""
+Given a 0-indexed integer array nums of length n and an integer target, return the number of pairs (i, j) where 0 <= i < j < n and nums[i] + nums[j] < target.
+ 
+"""
+
+from typing import List
+
+class Solution:
+    def countPairs(self, nums: List[int], target: int) -> int:
