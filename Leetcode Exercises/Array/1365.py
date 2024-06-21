f95ae4fab9f64ef228604fee27c50a357864e2ab
ghelaseG <ghelase.georgian@gmail.com>
2024-06-22 00:51:03 +0100

    Leetcode Exercise - n.1365 How many Number Are Smaller than the Current Number

--- Leetcode Exercises/Array/1365.py
+++ Leetcode Exercises/Array/1365.py
@@ -10,10 +10,10 @@ from typing import List
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
         result = []
-        count = 0
-
+        
         for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
+            count = 0
+            for j in range(len(nums)):
                 if nums[j] != nums[i] and nums[j] < nums[i]:
                     count += 1
             result.append(count)
