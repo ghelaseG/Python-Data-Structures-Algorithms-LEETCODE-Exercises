fe7cf6163fbbacbb066c051066eb4b9301fe4c8b
ghelaseG <ghelase.georgian@gmail.com>
2024-06-22 12:06:55 +0100

    Leetcode Exercise - n.1480 Running Sum of 1d Array

---
+++ Leetcode Exercises/Array/1480.py
@@ -0,0 +1,11 @@
+"""
+Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+
+Return the running sum of nums.
+"""
+
+from typing import List
+
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        
