12de6630efc7cb494f0ba8b28753fa337865af04
ghelaseG <ghelase.georgian@gmail.com>
2024-06-30 20:52:50 +0100

    Leetcode Exercise M - n.2610 Convert an Array into a 2D Array with Conditions

--- Leetcode Exercises/Array/Medium Difficulty/2610.py
+++ Leetcode Exercises/Array/Medium Difficulty/2610.py
@@ -9,6 +9,25 @@ Return the resulting array. If there are multiple answers, return any of them.
 Note that the 2D array can have a different number of elements on each row.
 """
 
+from typing import List
+
 class Solution:
     def findMatrix(self, nums: List[int]) -> List[List[int]]:
-        
+        #here we initialise the variable with a size that accommodates indexing
+        freq = [0] * (len(nums) + 1)
+        result = []
+
+        for i in nums:
+            #if the integer frequency is >= than the len of the result,
+            ##we add an empty subarray to result
+            if freq[i] >= len(result):
+                result.append([])
+            #then here we add the integer to the sublist and increment by 1 the frequency of the integer
+            result[freq[i]].append(i)
+            freq[i] += 1
+
+        return result
+    
+example = Solution()
+print(example.findMatrix(nums=[1,3,4,1,2,3,1]))
+
