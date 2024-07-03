fe713958a7bd9441eee15fe9e755f1c1635c54e1
ghelaseG <ghelase.georgian@gmail.com>
2024-07-03 19:47:08 +0100

    Leetcode Exercise XOR - n.3158 Find the XOR of Numbers Which Appear Twice, example and BigO

--- Leetcode Exercises/Array/XOR exercises/Easy/3158.py
+++ Leetcode Exercises/Array/XOR exercises/Easy/3158.py
@@ -3,16 +3,24 @@ You are given an array nums, where each number in the array appears either once 
 
 Return the bitwise XOR of all the numbers that appear twice in the array, or 0 if no number appears twice.
 """
+#we got a time and space complexity of O(N)
 
 from typing import List
 
 class Solution:
     def duplicateNumbersXOR(self, nums: List[int]) -> int:
+        hashmap = {}
+
+        for i in nums:
+            hashmap[i] = hashmap.get(i, 0) + 1
+        
         result = 0
 
-        for i in range(len(nums)):
-            for j in range(len(i+1, nums)):
-                if nums[i] == nums[j]:
-                    result = nums[i] ^ nums[j]
+        for num, count in hashmap.items():
+            if count >= 2:
+                result = result ^ num
         
         return result
+    
+example = Solution()
+print(example.duplicateNumbersXOR(nums=[1,2,2,1]))
