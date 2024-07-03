dafb111b0f954739824a6ea252c4cba284d1c25d
ghelaseG <ghelase.georgian@gmail.com>
2024-07-03 19:07:05 +0100

    Leetcode Exercise XOR - n.3158 Find the XOR of Numbers Which Appear Twice, trial1

--- Leetcode Exercises/Array/XOR exercises/Easy/3158.py
+++ Leetcode Exercises/Array/XOR exercises/Easy/3158.py
@@ -8,4 +8,11 @@ from typing import List
 
 class Solution:
     def duplicateNumbersXOR(self, nums: List[int]) -> int:
+        result = 0
+
+        for i in range(len(nums)):
+            for j in range(len(i+1, nums)):
+                if nums[i] == nums[j]:
+                    result = nums[i] ^ nums[j]
         
+        return result
