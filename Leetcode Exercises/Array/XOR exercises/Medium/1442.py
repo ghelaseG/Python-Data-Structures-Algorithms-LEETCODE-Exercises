96a57f172e4e7154cd2f96b5d1b27291013ca119
ghelaseG <ghelase.georgian@gmail.com>
2024-07-04 20:03:02 +0100

    Leetcode Exercise XOR - n.1442 Count Triplets That can form Two Arrays of Equal Xor, O(N**3)

--- Leetcode Exercises/Array/XOR exercises/Medium/1442.py
+++ Leetcode Exercises/Array/XOR exercises/Medium/1442.py
@@ -16,4 +16,20 @@ from typing import List
 
 class Solution:
     def countTriplets(slef, arr: List[int]) -> int:
+        N = len(arr)
+        count = 0
+
+        for i in range(N - 1):
+            a = 0
+            for j in range(i+1, N):
+                a = a ^ arr[j - 1]
+                b = 0
+                for k in range(j, N):
+                    b = b ^ arr[k]
+
+                    if a == b:
+                        count += 1
         
+        return count
+
+#Time complexity: O(N^3)
