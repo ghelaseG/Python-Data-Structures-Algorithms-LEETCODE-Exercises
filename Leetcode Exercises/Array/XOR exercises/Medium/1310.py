0c1edf6b6aed5ec625dbd0b7960b70ef7576f90c
ghelaseG <ghelase.georgian@gmail.com>
2024-07-07 23:32:26 +0100

    Leetcode Exercise XOR - n.1310 XOR Queries of a Subarray, mock

---
+++ Leetcode Exercises/Array/XOR exercises/Medium/1310.py
@@ -0,0 +1,13 @@
+"""
+You are given an array arr of positive integers. You are also given the array queries where queries[i] = [lefti, righti].
+
+For each query i compute the XOR of elements from lefti to righti (that is, arr[lefti] XOR arr[lefti + 1] XOR ... XOR arr[righti] ).
+
+Return an array answer where answer[i] is the answer to the ith query.
+"""
+
+from typing import List
+
+class Solution:
+    def xorQueries(self, arr: List[int], queries: List[List[int]]) -> List[int]:
+        
