4a16b50e0057fb2339dbdaba53ade8f950455fb2
ghelaseG <ghelase.georgian@gmail.com>
2024-07-12 19:45:36 +0100

    Practice Questions, n.9

--- Leetcode Exercises/Practice Hashmap/Exercises/9.py
+++ Leetcode Exercises/Practice Hashmap/Exercises/9.py
@@ -2,3 +2,8 @@
 Write a Python program to iterate over dictionaries using for loops.
 """
 
+d = {'Red': 1, 'Green': 2, 'Blue': 3}
+
+for key, value in d.items():
+    print(key, 'corresponds to: ', d[key])
+    print(key,":", value)
