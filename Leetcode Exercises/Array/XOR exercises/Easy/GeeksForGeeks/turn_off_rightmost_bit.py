990ddc4fd58f620d25416002ff8aad96ca16d39b
ghelaseG <ghelase.georgian@gmail.com>
2024-07-14 00:54:25 +0100

    GeeksForGeeks, practice XOR, 4. Turn off the rightmost set bit

--- Leetcode Exercises/Array/XOR exercises/Easy/GeeksForGeeks/turn_off_rightmost_bit.py
+++ Leetcode Exercises/Array/XOR exercises/Easy/GeeksForGeeks/turn_off_rightmost_bit.py
@@ -2,3 +2,10 @@
 Write a program that unsets the rightmost set bit of an integer. 
 """
 
+def fun(n):
+
+    return n & (n - 1)
+
+n = 7
+
+print("The number after unsetting the rightmost set bit", fun(n))
