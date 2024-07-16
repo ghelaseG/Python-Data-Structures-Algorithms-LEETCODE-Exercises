52c6032e510f796fd09327ff39c347f633fd1a4c
ghelaseG <ghelase.georgian@gmail.com>
2024-07-16 13:39:54 +0100

    Practice Questions, n.16

--- Leetcode Exercises/Practice Hashmap/Exercises/16.py
+++ Leetcode Exercises/Practice Hashmap/Exercises/16.py
@@ -2,3 +2,20 @@
 Write a Python program to get a dictionary from an object's fields.
 """
 
+# define a class with random name that inherits from the 'object' class
+
+class my_dict_object(object):
+    #define the constructor method '__init__' for initializing object attributes
+    def __init__(self):
+        self.x = 'Red'
+        self.y = 'Green'
+        self.z = 'Yellow'
+    
+    #define a method 'do_nothing' that doesn't perform any actions (placeholder)
+    def do_nothing(self):
+        pass
+
+#create an instance test of the dictObj class
+test = my_dict_object()
+
+print(test.__dict__)
