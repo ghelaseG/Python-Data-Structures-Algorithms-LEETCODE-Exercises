ddf8d60aded82df49867bffc837b288931c520f0
ghelaseG <ghelase.georgian@gmail.com>
2024-07-18 20:06:41 +0100

    GeeksForGeeks, practice XOR, 9. Find Position of the Only set Bit, mock

---
+++ Leetcode Exercises/Array/XOR exercises/Easy/GeeksForGeeks/find_pos_of_only_bits.py
@@ -0,0 +1,8 @@
+"""
+Find position of the only set bit
+
+Given a number N having only one ‘1’ and all other ’0’s in its binary representation, find position of the only set bit. If there are 0 or more than 1 set bit the answer should be -1. Position of set bit ‘1’ should be counted starting with 1 from the LSB side in the binary representation of the number.
+
+"""
+
+
