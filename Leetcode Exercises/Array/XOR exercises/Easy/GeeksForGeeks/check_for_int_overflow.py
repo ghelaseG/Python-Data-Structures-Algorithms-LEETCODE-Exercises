23a9471dafc0684bc3f7462471540228c380a538
ghelaseG <ghelase.georgian@gmail.com>
2024-07-19 22:00:29 +0100

    GeeksForGeeks, practice XOR, 10. Check for Integer overflow, mock

---
+++ Leetcode Exercises/Array/XOR exercises/Easy/GeeksForGeeks/check_for_int_overflow.py
@@ -0,0 +1,6 @@
+"""
+Write a “C” function, int addOvf(int* result, int a, int b) If there is no overflow, the function places the resultant = sum a+b in “result” and returns 0. Otherwise, it returns -1. The solution of casting to long and adding to find detecting the overflow is not allowed.
+Method 1 
+There can be overflow only if signs of two numbers are same, and sign of sum is opposite to the signs of numbers. 
+"""
+
