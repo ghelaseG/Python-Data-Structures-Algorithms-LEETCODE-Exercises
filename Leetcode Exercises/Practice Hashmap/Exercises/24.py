f6b6e37262a40975bded0cfcdf5e04725bba1d83
ghelaseG <ghelase.georgian@gmail.com>
2024-07-21 23:43:45 +0100

    Practice Questions, n.24

--- Leetcode Exercises/Practice Hashmap/Exercises/24.py
+++ Leetcode Exercises/Practice Hashmap/Exercises/24.py
@@ -5,3 +5,12 @@ Sample string : 'w3resource'
 Expected output: {'w': 1, '3': 1, 'r': 2, 'e': 2, 's': 1, 'o': 1, 'u': 1, 'c': 1}
 """
 
+string1 = 'w3resources'
+
+my_dict = {}
+
+for letter in string1:
+    #we use the get method with a default value of 0 and then incrementing by 1
+    my_dict[letter] = my_dict.get(letter, 0) + 1
+
+print(my_dict)
