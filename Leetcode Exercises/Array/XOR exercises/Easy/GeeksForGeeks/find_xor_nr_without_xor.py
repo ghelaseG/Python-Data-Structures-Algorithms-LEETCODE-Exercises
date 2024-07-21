3df1f0ee6ead46efb4a9f00e7c2e9808841479dd
ghelaseG <ghelase.georgian@gmail.com>
2024-07-21 23:50:37 +0100

    GeeksForGeeks, practice XOR, 11. Find Xor of two Number without Using Xor Operator, 2nd Approach

--- Leetcode Exercises/Array/XOR exercises/Easy/GeeksForGeeks/find_xor_nr_without_xor.py
+++ Leetcode Exercises/Array/XOR exercises/Easy/GeeksForGeeks/find_xor_nr_without_xor.py
@@ -2,35 +2,45 @@
 Given two integers, find XOR of them without using the XOR operator, i.e., without using ‘^‘ in C/C++.
 """
 
-def XorResult(x, y):
-    result = 0 #initialise result 
+# def XorResult(x, y):
+#     result = 0 #initialise result 
 
-    #we assume a 32 bit integer
-    for i in range(31, -1, -1):
+#     #we assume a 32 bit integer
+#     for i in range(31, -1, -1):
 
-        #find current bits in x and y
-        b1 = x & (1 << i)
-        b2 = y & (1 << i)
-        b1 = min(b1, 1)
-        b2 = min(b2, 1)
+#         #find current bits in x and y
+#         b1 = x & (1 << i)
+#         b2 = y & (1 << i)
+#         b1 = min(b1, 1)
+#         b2 = min(b2, 1)
 
-        #if both are 1 then is equal to 0,
-        #otherwise xor is same as or
+#         #if both are 1 then is equal to 0,
+#         #otherwise xor is same as or
 
-        xoredBit = 0
-        if (b1 & b2):
-            xoredBit = 0
-        else:
-            xoredBit = (b1 | b2)
+#         xoredBit = 0
+#         if (b1 & b2):
+#             xoredBit = 0
+#         else:
+#             xoredBit = (b1 | b2)
 
-        #update result
-        result <<= 1
-        result |= xoredBit
+#         #update result
+#         result <<= 1
+#         result |= xoredBit
     
-    return result
+#     return result
 
-#example
+# #example
 
+# x = 3
+# y = 5
+# print("XOR is:", XorResult(x, y))
+
+#2nd Approach
+
+def myXOR(x, y):
+    return ((x | y) & ( ~x | ~y ))
+
+#Example:
 x = 3
 y = 5
-print("XOR is:", XorResult(x, y))
+print('XOR is:', myXOR(x, y))
