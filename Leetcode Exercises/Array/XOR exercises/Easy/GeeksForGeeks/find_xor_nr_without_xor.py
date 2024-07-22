0425e1dd2a6ebf48d3c213c2adcf3aa567f729e8
ghelaseG <ghelase.georgian@gmail.com>
2024-07-22 21:33:11 +0100

    GeeksForGeeks, practice XOR, 11. Find Xor of two Number without Using Xor Operator, 4th Approach

--- Leetcode Exercises/Array/XOR exercises/Easy/GeeksForGeeks/find_xor_nr_without_xor.py
+++ Leetcode Exercises/Array/XOR exercises/Easy/GeeksForGeeks/find_xor_nr_without_xor.py
@@ -47,8 +47,17 @@ Given two integers, find XOR of them without using the XOR operator, i.e., witho
 
 #3rd Approach
 
+# def myXOR(x, y):
+#     return (x & (~y)) | ((~x) & y)
+
+# x = 3
+# y = 5
+# print('XOR is:', myXOR(x, y))
+
+#4th Approach
+
 def myXOR(x, y):
-    return (x & (~y)) | ((~x) & y)
+    return (x + y - (2 * (x & y)))
 
 x = 3
 y = 5
