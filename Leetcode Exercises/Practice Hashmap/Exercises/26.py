9606d5f1545fe32b3d3d0e8bec6ba877731b0704
ghelaseG <ghelase.georgian@gmail.com>
2024-07-23 16:21:39 +0100

    Practice Questions, n.26

--- Leetcode Exercises/Practice Hashmap/Exercises/26.py
+++ Leetcode Exercises/Practice Hashmap/Exercises/26.py
@@ -5,3 +5,12 @@ Expected Output:
 2
 """
 
+student = [{'id': 1, 'success': True, 'name': 'Lary'},
+ {'id': 2, 'success': False, 'name': 'Rabi'},
+ {'id': 3, 'success': True, 'name': 'Alex'}]
+
+#print the total of id's
+print(sum(d['id'] for d in student))
+
+#print the total of success
+print(sum(d['success'] for d in student))
