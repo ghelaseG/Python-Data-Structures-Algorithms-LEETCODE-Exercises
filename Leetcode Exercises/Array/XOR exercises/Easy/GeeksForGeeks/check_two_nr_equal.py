1c8b63be782e940dfcefc9d3af5162c81031ef56
ghelaseG <ghelase.georgian@gmail.com>
2024-07-24 07:31:26 +0100

    GeeksForGeeks, practice XOR, 12. Check if two numbers are equal without using arithmetic and comparision operators, method 2

--- Leetcode Exercises/Array/XOR exercises/Easy/GeeksForGeeks/check_two_nr_equal.py
+++ Leetcode Exercises/Array/XOR exercises/Easy/GeeksForGeeks/check_two_nr_equal.py
@@ -2,13 +2,24 @@
 Given two numbers, the task is to check if two numbers are equal without using Arithmetic and Comparison Operators or String functions.
 """
 
-def checkEqual(x, y):
+# def checkEqual(x, y):
 
-    #we can use XOR, because XOR of 2 same numbers is Zero
-    if ((x ^ y) ==0):
-        print("Same")
-    else:
-        print("Not the same")
+#     #we can use XOR, because XOR of 2 same numbers is Zero
+#     if ((x ^ y) ==0):
+#         print("Same")
+#     else:
+#         print("Not the same")
 
 
-checkEqual(2, 2)
+# checkEqual(2, 2)
+
+def is_equal(num1, num2):
+    mask = 1
+    for i in range(32):
+        if (num1 & mask) != (num2 & mask):
+            return False
+        mask <<= 1
+    return True
+
+print(is_equal(10, 10))
+print(is_equal(20, 10))
