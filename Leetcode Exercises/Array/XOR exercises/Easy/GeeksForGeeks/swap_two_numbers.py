c88a59561ea627ded6ca94f3c8631c1463067957
ghelaseG <ghelase.georgian@gmail.com>
2024-07-27 23:53:31 +0100

    GeeksForGeeks, practice XOR, 14. Swap two numbers without using temporary variable, method 2

--- Leetcode Exercises/Array/XOR exercises/Easy/GeeksForGeeks/swap_two_numbers.py
+++ Leetcode Exercises/Array/XOR exercises/Easy/GeeksForGeeks/swap_two_numbers.py
@@ -2,21 +2,32 @@
 Given two variables, x, and y, swap two variables without using a third variable. 
 """
 
-#method1: using addition and subtraction
+# #method1: using addition and subtraction
+
+# x = 10
+# y = 5
+
+# # x becomes 15
+
+# x = x + y
+
+# # y becomes 10
+
+# y = x - y
+
+# # x becomes 5
+
+# x = x - y
+
+# print(x,y)
+
+#method 2: using bitwise XOR
 
 x = 10
 y = 5
 
-# x becomes 15
+x = x ^ y #x becomes 15(1111)
+y = x ^ y #y becomes 10(1010)
+x = x ^ y #x becomes 5 (0101)
 
-x = x + y
-
-# y becomes 10
-
-y = x - y
-
-# x becomes 5
-
-x = x - y
-
-print(x,y)
+print(x, y)
