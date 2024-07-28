ee3d0b6ea6520d2b60f30fc4b799c74c89d9331d
ghelaseG <ghelase.georgian@gmail.com>
2024-07-28 23:31:51 +0100

    GeeksForGeeks, practice XOR, 14. Swap two numbers without using temporary variable, method 3

--- Leetcode Exercises/Array/XOR exercises/Easy/GeeksForGeeks/swap_two_numbers.py
+++ Leetcode Exercises/Array/XOR exercises/Easy/GeeksForGeeks/swap_two_numbers.py
@@ -21,13 +21,33 @@ Given two variables, x, and y, swap two variables without using a third variable
 
 # print(x,y)
 
-#method 2: using bitwise XOR
+# #method 2: using bitwise XOR
 
-x = 10
-y = 5
+# x = 10
+# y = 5
 
-x = x ^ y #x becomes 15(1111)
-y = x ^ y #y becomes 10(1010)
-x = x ^ y #x becomes 5 (0101)
+# x = x ^ y #x becomes 15(1111)
+# y = x ^ y #y becomes 10(1010)
+# x = x ^ y #x becomes 5 (0101)
 
-print(x, y)
+# print(x, y)
+
+# method 3: mixture of bitwise operators and arithmetic operations
+
+def swap(a, b):
+
+    #same as a = a + b
+    a = (a & b) + (a | b)
+
+    #same as b = a - b
+    b = a + (~b) + 1
+
+    #same as a = a - b
+    a = a + (~b) + 1
+
+    print('After swapping: a =', a, 'b =', b)
+
+a = 5
+b = 10
+
+swap(a, b)
