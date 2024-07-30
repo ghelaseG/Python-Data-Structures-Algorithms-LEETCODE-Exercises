07689b3ddcd54dfc202a88a2f7149100073cb4e3
ghelaseG <ghelase.georgian@gmail.com>
2024-07-31 00:02:04 +0100

    Practice Questions, n.36

--- Leetcode Exercises/Practice Hashmap/Exercises/36.py
+++ Leetcode Exercises/Practice Hashmap/Exercises/36.py
@@ -4,3 +4,18 @@ Sample lists: ['Class-V', 'Class-VI', 'Class-VII', 'Class-VIII'], [1, 2, 2, 3]
 Expected Output: defaultdict(<class 'set'>, {'Class-V': {1}, 'Class-VI': {2}, 'Class-VII': {2}, 'Class-VIII': {3}})
 """
 
+from collections import defaultdict
+
+class_list = ['Class-V', 'Class-VI', 'Class-VII', 'Class-VIII']
+id_list = [1, 2, 2, 3]
+
+#create a defaultdict with set as the default factory function
+
+temp = defaultdict(set)
+
+#iterate through pair elements using zip function
+for key, value in zip(class_list, id_list):
+    #add the value associated with the key in the temp dict
+    temp[key].add(value)
+
+print(temp)
