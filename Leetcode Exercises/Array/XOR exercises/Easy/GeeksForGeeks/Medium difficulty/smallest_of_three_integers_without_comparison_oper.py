6667efd87b150275e6231d028ecadd597e8fd84b
ghelaseG <ghelase.georgian@gmail.com>
2024-07-31 21:04:59 +0100

    GeeksForGeeks, practice XOR - Medium, 2. Smallest of Three Integers Without Comparison Operators, mock

---
+++ Leetcode Exercises/Array/XOR exercises/Easy/GeeksForGeeks/Medium difficulty/smallest_of_three_integers_without_comparison_oper.py
@@ -0,0 +1,7 @@
+"""
+Write a program to find the smallest of three integers, without using any of the comparison operators. 
+Let 3 input numbers be x, y and z.
+Method 1 (Repeated Subtraction) 
+Take a counter variable c and initialize it with 0. In a loop, repeatedly subtract x, y and z by 1 and increment c. The number which becomes 0 first is the smallest. After the loop terminates, c will hold the minimum of 3. 
+"""
+
