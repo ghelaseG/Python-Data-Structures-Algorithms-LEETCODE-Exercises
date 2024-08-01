873b763278ea164d9b98913863b8bc3bb58778dd
ghelaseG <ghelase.georgian@gmail.com>
2024-08-01 23:28:31 +0100

    GeeksForGeeks, practice XOR - Medium, 2. Smallest of Three Integers Without Comparison Operators, method 2

--- Leetcode Exercises/Array/XOR exercises/Easy/GeeksForGeeks/Medium difficulty/smallest_of_three_integers_without_comparison_oper.py
+++ Leetcode Exercises/Array/XOR exercises/Easy/GeeksForGeeks/Medium difficulty/smallest_of_three_integers_without_comparison_oper.py
@@ -5,22 +5,42 @@ Method 1 (Repeated Subtraction)
 Take a counter variable c and initialize it with 0. In a loop, repeatedly subtract x, y and z by 1 and increment c. The number which becomes 0 first is the smallest. After the loop terminates, c will hold the minimum of 3. 
 """
 
-# Method 1
-## Take a counter variable c and initialize it with 0. In a loop, repeatedly subtract x, y and z by 1 and increment c. The number which becomes 0 first is the smallest. After the loop terminates, c will hold the minimum of 3. 
+# # Method 1
+# ## Take a counter variable c and initialize it with 0. In a loop, repeatedly subtract x, y and z by 1 and increment c. The number which becomes 0 first is the smallest. After the loop terminates, c will hold the minimum of 3. 
 
-def minimumInteger(x, y, z):
-    count = 0
+# def minimumInteger(x, y, z):
+#     count = 0
 
-    while (x and y and z):
-        x = x - 1
-        y = y - 1
-        z = z - 1
-        count += 1
+#     while (x and y and z):
+#         x = x - 1
+#         y = y - 1
+#         z = z - 1
+#         count += 1
 
-    return count
+#     return count
 
-#Example
-x = 20
+# #Example
+# x = 20
+# y = 15
+# z = 5
+# print('Minimum of 3 numbers is: ', minimumInteger(x, y, z))
+
+# Method 2 : using bit operations
+
+CHAR_BIT = 8
+
+#we create a function to find the minimum of x and y
+def min(x, y):
+    return y + ((x - y) & (x - y) >> (32 * CHAR_BIT - 1))
+
+#function to find minimum of 3 numbers
+def smallest(x, y, z):
+    return min(x, min(y, z))
+
+
+#Example:
+x = 12
 y = 15
 z = 5
-print('Minimum of 3 numbers is: ', minimumInteger(x, y, z))
+print(smallest(x, y, z))
+
