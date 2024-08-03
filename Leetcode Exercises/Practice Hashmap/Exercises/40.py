74cfa481de116e9deae101928daac1165757d5a4
ghelaseG <ghelase.georgian@gmail.com>
2024-08-03 12:30:33 +0100

    Practice Questions, n.40, method 2

--- Leetcode Exercises/Practice Hashmap/Exercises/40.py
+++ Leetcode Exercises/Practice Hashmap/Exercises/40.py
@@ -11,11 +11,26 @@ y has value [21, 22, 23, 24, 25, 26, 27, 28, 29]
 z has value [31, 32, 33, 34, 35, 36, 37, 38, 39]
 """
 
-# Method 1
-dict = {'x': [11, 12, 13, 14, 15, 16, 17, 18, 19],
-'y': [21, 22, 23, 24, 25, 26, 27, 28, 29],
-'z': [31, 32, 33, 34, 35, 36, 37, 38, 39]}
+from pprint import pprint
 
-for key, value in dict.items():
-    print(value[4])
-    
+# # Method 1
+# dict = {'x': [11, 12, 13, 14, 15, 16, 17, 18, 19],
+# 'y': [21, 22, 23, 24, 25, 26, 27, 28, 29],
+# 'z': [31, 32, 33, 34, 35, 36, 37, 38, 39]}
+
+# for key, value in dict.items():
+#     print(value[4])
+
+# Method 2
+
+dict_nums = dict(x=list(range(11, 20)), y=list(range(21, 30)), z=list(range(31, 40)))
+
+pprint(dict_nums)
+
+print(dict_nums['x'][4])
+print(dict_nums['y'][4])
+print(dict_nums['z'][4])
+
+for k, v in dict_nums.items():
+    print(k, 'has value', v)
+
