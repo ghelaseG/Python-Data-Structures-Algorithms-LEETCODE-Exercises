4b08abae694978f2696d35ef0c65453033f6569c
ghelaseG <ghelase.georgian@gmail.com>
2024-08-04 23:51:56 +0100

    Practice Questions, n.41

---
+++ Leetcode Exercises/Practice Hashmap/Exercises/41.py
@@ -0,0 +1,14 @@
+"""
+Write a Python program to drop empty items from a given dictionary.
+Original Dictionary:
+{'c1': 'Red', 'c2': 'Green', 'c3': None}
+New Dictionary after dropping empty items:
+{'c1': 'Red', 'c2': 'Green'}
+"""
+
+dict1 = {'c1': 'Red', 'c2': 'Green', 'c3': None}
+
+
+my_dict = {key: value for (key,value) in dict1.items() if value is None}
+
+print(my_dict)
