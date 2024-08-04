b9dbf1a3ebfe67332e82b131c6a53045ec47dbb7
ghelaseG <ghelase.georgian@gmail.com>
2024-08-05 00:01:59 +0100

    GeeksForGeeks, practice XOR - Medium, 4. Smallest Power of 2 greater Than or Equal to N, mock

---
+++ Leetcode Exercises/Array/XOR exercises/Easy/GeeksForGeeks/Medium difficulty/smallest_power_of2_greaterOrequalToN.py
@@ -0,0 +1,10 @@
+"""
+Write a function that, for a given no n, finds a number p which is greater than or equal to n and is the smallest power of 2. 
+"""
+
+# method 1: using log2(number) 
+
+## calculate the log2N and store it into a variable 
+## check if pow(2, a) is equal to N
+        # return N
+## otherwise return pow(2, a + 1)
