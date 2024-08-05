de0e00b29d65f56b31aae77d6d837b4bfec54c96
ghelaseG <ghelase.georgian@gmail.com>
2024-08-05 21:11:57 +0100

    GeeksForGeeks, practice XOR - Medium, 4. Smallest Power of 2 greater Than or Equal to N, method 1

--- Leetcode Exercises/Array/XOR exercises/Easy/GeeksForGeeks/Medium difficulty/smallest_power_of2_greaterOrequalToN.py
+++ Leetcode Exercises/Array/XOR exercises/Easy/GeeksForGeeks/Medium difficulty/smallest_power_of2_greaterOrequalToN.py
@@ -8,3 +8,22 @@ Write a function that, for a given no n, finds a number p which is greater than 
 ## check if pow(2, a) is equal to N
         # return N
 ## otherwise return pow(2, a + 1)
+
+import math
+
+def nearestPowerOf2(N):
+    # calculate log 2 of N
+    a = int(math.log2(N))
+
+    # if 2 pow a is equal to N, return N
+    if 2**a == N:
+        return N
+    
+    # else, return 2 pow (a + 1)
+    return 2**(a + 1)
+
+# main function
+if __name__ == "__main__":
+    # input number
+    n = 10
+    print(nearestPowerOf2(n))
