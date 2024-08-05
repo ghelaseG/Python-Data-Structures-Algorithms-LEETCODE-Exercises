f9157c38d613dfd64276871daa8fc3d19a06458f
ghelaseG <ghelase.georgian@gmail.com>
2024-08-05 21:27:49 +0100

    GeeksForGeeks, practice XOR - Medium, 4. Smallest Power of 2 greater Than or Equal to N, method 2

--- Leetcode Exercises/Array/XOR exercises/Easy/GeeksForGeeks/Medium difficulty/smallest_power_of2_greaterOrequalToN.py
+++ Leetcode Exercises/Array/XOR exercises/Easy/GeeksForGeeks/Medium difficulty/smallest_power_of2_greaterOrequalToN.py
@@ -11,19 +11,37 @@ Write a function that, for a given no n, finds a number p which is greater than 
 
 import math
 
-def nearestPowerOf2(N):
-    # calculate log 2 of N
-    a = int(math.log2(N))
+# def nearestPowerOf2(N):
+#     # calculate log 2 of N
+#     a = int(math.log2(N))
 
-    # if 2 pow a is equal to N, return N
-    if 2**a == N:
-        return N
+#     # if 2 pow a is equal to N, return N
+#     if 2**a == N:
+#         return N
     
-    # else, return 2 pow (a + 1)
-    return 2**(a + 1)
+#     # else, return 2 pow (a + 1)
+#     return 2**(a + 1)
 
-# main function
-if __name__ == "__main__":
-    # input number
-    n = 10
-    print(nearestPowerOf2(n))
+# # main function
+# if __name__ == "__main__":
+#     # input number
+#     n = 10
+#     print(nearestPowerOf2(n))
+
+def nearestPowerOf2(n):
+    count = 0
+
+    #if n is zero then:
+    if (n and not(n & (n - 1))):
+        return n
+    
+    #else keep shifting n until it becomes zero and count no of shifts
+    while (n != 0):
+        n >>= 1
+        count += 1
+
+    return 1 << count
+
+n = 4
+
+print(nearestPowerOf2(n))
