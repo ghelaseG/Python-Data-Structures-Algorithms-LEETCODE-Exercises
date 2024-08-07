f5fa452dea622531bf362f68623d364050e9672a
ghelaseG <ghelase.georgian@gmail.com>
2024-08-07 23:44:24 +0100

    GeeksForGeeks, practice XOR - Medium, 5. Program to find Parity, method 2

--- Leetcode Exercises/Array/XOR exercises/Easy/GeeksForGeeks/Medium difficulty/program_to_find_parity.py
+++ Leetcode Exercises/Array/XOR exercises/Easy/GeeksForGeeks/Medium difficulty/program_to_find_parity.py
@@ -1,17 +1,27 @@
 """
 Parity: Parity of a number refers to whether it contains an odd or even number of 1-bits. The number has “odd parity” if it contains an odd number of 1-bits and is “even parity” if it contains an even number of 1-bits. 
 """
+# # Method 1
+# # return 1 if number is odd
+# # return 0 if number is even
 
-# return 1 if number is odd
-# return 0 if number is even
+# def getParity(n):
+
+#     parity = 0
+#     while n:
+#         parity = ~parity
+#         n = n & (n - 1)
+#     return parity
+
+# n = 10
+# print("Parity of nr", n,"is", "odd" if getParity(n) else "even")
+
+# Method 2
 
 def getParity(n):
+    return(bin(n).count("1"))%2
 
-    parity = 0
-    while n:
-        parity = ~parity
-        n = n & (n - 1)
-    return parity
+#example
+n = 7
+print("odd" if getParity(n) else "even")
 
-n = 10
-print("Parity of nr", n,"is", "odd" if getParity(n) else "even")
