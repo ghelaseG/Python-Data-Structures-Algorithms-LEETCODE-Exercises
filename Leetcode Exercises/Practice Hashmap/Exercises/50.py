ad1014b2821e0ae2dc5eb75caef4786bc4756056
ghelaseG <ghelase.georgian@gmail.com>
2024-08-13 23:47:40 +0100

    Practice Questions, n.50

--- Leetcode Exercises/Practice Hashmap/Exercises/50.py
+++ Leetcode Exercises/Practice Hashmap/Exercises/50.py
@@ -5,3 +5,16 @@ Original Dictionary:
 Clear the list values in the said dictionary:
 {'C1': [], 'C2': [], 'C3': []}
 """
+
+dictionary = { 
+    'C1': [10, 20, 30], 
+    'C2': [20, 30, 40],
+    'C3': [12, 34]
+}
+
+def clear_dictionary(dict):
+    for key in dict:
+        dict[key].clear()
+    return dict
+
+print(clear_dictionary(dictionary))
