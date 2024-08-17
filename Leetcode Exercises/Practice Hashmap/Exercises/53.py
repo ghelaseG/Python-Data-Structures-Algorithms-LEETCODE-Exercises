ae484cf7f0e840a4d689807d1ccae62c5f49f7d7
ghelaseG <ghelase.georgian@gmail.com>
2024-08-17 01:34:36 +0100

    Practice Questions, n.53

--- Leetcode Exercises/Practice Hashmap/Exercises/53.py
+++ Leetcode Exercises/Practice Hashmap/Exercises/53.py
@@ -1,4 +1,19 @@
 """
 Write a Python program to find the length of a dictionary of values.
-
 """
+
+color_dict = {1: 'red', 2: 'green', 3: 'black', 4: 'white', 5: 'black'}
+
+def check_length(dict):
+
+    result = {}
+    for value in dict.values():
+        #calculate the length of each value
+        result[value] = len(value)
+    return result
+
+print(check_length(color_dict))
+
+color_dict = {'1': 'Austin Little', '2': 'Natasha Howard', '3': 'Alfred Mullins', '4': 'Jamie Rowe'}
+
+print(check_length(color_dict))
