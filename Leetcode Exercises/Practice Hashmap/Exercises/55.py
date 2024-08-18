ce003764c47d071da90c4963305063ad260d0421
ghelaseG <ghelase.georgian@gmail.com>
2024-08-18 23:36:57 +0100

    Practice Questions, n.55

---
+++ Leetcode Exercises/Practice Hashmap/Exercises/55.py
@@ -0,0 +1,17 @@
+"""
+Write a Python program to access dictionary key's element by index.
+Expected Output:
+physics
+math
+chemistry
+"""
+
+num = {'physics': 80, 'math': 90, 'chemistry': 86}
+
+print("\nAccess the keys of the dictionary as a list:")
+
+print(list(num)[0])
+
+print(list(num)[1])
+
+print(list(num)[2])
