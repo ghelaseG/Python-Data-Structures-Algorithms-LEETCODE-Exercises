16d60d6f8b2d47ae8869680399ff883f7f19cf7d
ghelaseG <ghelase.georgian@gmail.com>
2024-08-27 00:03:37 +0100

    GeeksForGeeks, practice XOR - Medium, 13. Check if a Number is Bleak, example

--- Leetcode Exercises/Array/XOR exercises/Easy/GeeksForGeeks/Medium difficulty/check_if_number_is_bleak.py
+++ Leetcode Exercises/Array/XOR exercises/Easy/GeeksForGeeks/Medium difficulty/check_if_number_is_bleak.py
@@ -1,4 +1,18 @@
 """
 A number ‘n’ is called Bleak if it cannot be represented as sum of a positive number x and set bit count in x, i.e., x + countSetBits(x) is not equal to n for any non-negative number x.
+
+Example:
+
+Input : n = 3
+Output : false
+3 is not Bleak as it can be represented
+as 2 + countSetBits(2).
+
+Input : n = 4
+Output : true
+4 is t Bleak as it cannot be represented 
+as sum of a number x and countSetBits(x)
+for any number x.
 """
+
 
