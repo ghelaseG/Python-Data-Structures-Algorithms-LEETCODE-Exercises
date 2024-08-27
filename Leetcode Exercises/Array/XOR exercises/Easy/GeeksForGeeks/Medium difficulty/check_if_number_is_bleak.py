8738686a7b7caa2244784300f9bee0e9330792b7
ghelaseG <ghelase.georgian@gmail.com>
2024-08-27 20:53:19 +0100

    GeeksForGeeks, practice XOR - Medium, 13. Check if a Number is Bleak, method 2

--- Leetcode Exercises/Array/XOR exercises/Easy/GeeksForGeeks/Medium difficulty/check_if_number_is_bleak.py
+++ Leetcode Exercises/Array/XOR exercises/Easy/GeeksForGeeks/Medium difficulty/check_if_number_is_bleak.py
@@ -14,16 +14,70 @@ Output : true
 as sum of a number x and countSetBits(x)
 for any number x.
 """
-# method 1
+# # method 1
 
-# function to get the nr of set bits in binary representation of passed binary nr
+# # function to get the nr of set bits in binary representation of passed binary nr
+
+# def countSetBits(x):
+
+#     count = 0
+
+#     while (x):
+#         x = x & (x - 1)
+#         count += 1
+
+#     return count
+
+# # return True if n is Bleak
+
+# def isBleak(n):
+
+#     #check for all numbers 'x' smaller than n
+#     #if x + countSetBits(x) become n, then n can't be Bleak
+
+#     for x in range(1, n):
+
+#         if (x + countSetBits(x) == n):
+#             return False
+    
+#     return True
+
+# if (isBleak(3)):
+#     print('Yes')
+# else:
+#     print('No')
+
+# if (isBleak(4)):
+#     print('Yes')
+# else:
+#     print("No")
+
+
+
+# method 2 (efficient way)
+
+import math
 
 def countSetBits(x):
 
     count = 0
 
-    while (x):
-        x = x & (x - 1)
+    while x:
+        x = x & (x - 1) 
+        count = count + 1
+
+    return count
+
+# a function to return ceiling of log x in base 2
+#for exp, it returns 3 for 8 and 4 for 9
+
+def ceilLog2(x):
+
+    count = 0
+    x = x - 1
+
+    while (x > 0):
+        x = x >> 1
         count += 1
 
     return count
@@ -33,21 +87,23 @@ def countSetBits(x):
 def isBleak(n):
 
     #check for all numbers 'x' smaller than n
-    #if x + countSetBits(x) become n, then n can't be Bleak
+    #if x + countSetBits(x) becomes n, then n can't be Bleak
 
-    for x in range(1, n):
+    for x in range((n - ceilLog2(n)), n):
 
         if (x + countSetBits(x) == n):
+            
             return False
     
     return True
 
-if (isBleak(3)):
+#examples:
+if(isBleak(3)):
     print('Yes')
 else:
     print('No')
 
-if (isBleak(4)):
+if(isBleak(4)):
     print('Yes')
 else:
-    print("No")
+    print('No')
