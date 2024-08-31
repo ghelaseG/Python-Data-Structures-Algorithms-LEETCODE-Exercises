a40a331b948bfc6a82335d685f96dab32694c6f8
ghelaseG <ghelase.georgian@gmail.com>
2024-08-31 19:10:56 +0100

    GeeksForGeeks, practice XOR - Medium, 14. Grey to binary and binary to grey conversion, method 3

--- Leetcode Exercises/Array/XOR exercises/Easy/GeeksForGeeks/Medium difficulty/gray_to_binary_and_vice_versa.py
+++ Leetcode Exercises/Array/XOR exercises/Easy/GeeksForGeeks/Medium difficulty/gray_to_binary_and_vice_versa.py
@@ -59,17 +59,72 @@ Other bits of the output binary code can be obtained by checking the gray code b
 # print(greyConverter(n))
 
 
-# method 2
+# # method 2
 
-def binaryConverter(n):
-    result = n
+# def binaryConverter(n):
+#     result = n
 
-    while n > 0:
-        n >>= 1
-        result ^= n
+#     while n > 0:
+#         n >>= 1
+#         result ^= n
     
-    return result
+#     return result
 
-#example
-n = 4
-print(binaryConverter(n))
+# #example
+# n = 4
+# print(binaryConverter(n))
+
+
+# method 3
+
+#helper function to xor two characters
+def xor_c(a, b):
+    return '0' if(a == b) else '1'
+
+#helper function to flip the bit
+def flip(c):
+    return '1' if(c == '0') else '0'
+
+#function to convert binary string to gray string
+def binarytoGray(binary):
+    gray = ""
+
+    #masb of gray code is same as binary code
+    gray += binary[0]
+
+    #compute remaining bits, next bit is computed by doing XOR of
+    #previous and current in Binary
+    for i in range(1, len(binary)):
+
+        #Concatenate XOR of previous bit with current bit
+        gray += xor_c(binary[i - 1], binary[i])
+
+    return gray
+
+#function to convert gray code string to binary string
+def graytoBinary(gray):
+
+    binary = ""
+
+    #MSB of binary code is same as gray code
+    binary += gray[0]
+
+    #compute remaining bits
+    for i in range(1, len(gray)):
+
+        #if current bit is 0, concatenate previous bit
+        if (gray[i] == '0'):
+            binary += binary[i - 1]
+
+        #else, concatenate invert of previous bit
+        else:
+            binary += flip(binary[i - 1])
+    
+    return binary
+
+#example:
+binary = '01001'
+print("Gray code of", binary,"is", binarytoGray(binary))
+
+gray = "01101"
+print("Binary code of", gray, "is", graytoBinary(gray))
