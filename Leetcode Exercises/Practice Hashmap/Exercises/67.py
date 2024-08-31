8da35639b7b6e271e494563762c6af4b0ac4bbfe
ghelaseG <ghelase.georgian@gmail.com>
2024-08-31 13:00:42 +0100

    Practice Questions, n.67, mock

---
+++ Leetcode Exercises/Practice Hashmap/Exercises/67.py
@@ -0,0 +1,6 @@
+"""
+Write a Python program to invert a given dictionary with non-unique hashable values.
+Sample Output:
+{8: ['Ora Mckinney', 'Mathew Gilbert'], 7: ['Theodore Hollandl', 'Mae Fleming', 'Ivan Little']}
+"""
+
