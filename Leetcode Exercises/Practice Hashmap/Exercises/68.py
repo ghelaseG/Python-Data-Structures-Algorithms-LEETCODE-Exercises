bcd8bed9ebb776592b29f3444ec709020a51431b
ghelaseG <ghelase.georgian@gmail.com>
2024-09-01 23:59:36 +0100

    Practice Questions, n.68

---
+++ Leetcode Exercises/Practice Hashmap/Exercises/68.py
@@ -0,0 +1,9 @@
+"""
+Write a Python program to combine two or more dictionaries, creating a list of values for each key.
+Sample Output:
+Original dictionaries:
+{'w': 50, 'x': 100, 'y': 'Green', 'z': 400}
+{'x': 300, 'y': 'Red', 'z': 600}
+Combined dictionaries, creating a list of values for each key:
+{'w': [50], 'x': [100, 300], 'y': ['Green', 'Red'], 'z': [400, 600]}
+"""
