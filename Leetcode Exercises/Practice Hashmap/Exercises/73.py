89db849242dc17f9ff96ebd67448a2e2ca8696bc
ghelaseG <ghelase.georgian@gmail.com>
2024-09-06 23:35:51 +0100

    Practice Questions, n.73

--- Leetcode Exercises/Practice Hashmap/Exercises/73.py
+++ Leetcode Exercises/Practice Hashmap/Exercises/73.py
@@ -7,3 +7,15 @@ Convert a list of dictionaries into a list of values corresponding to the specif
 [18, 22, 20, 18]
 """
 
+def convert_to_list(dicts, key):
+    return [value.get(key) for value in dicts]
+
+#we create a list of dictionaries 
+students = [
+    {'name': 'Theodore', 'age': 18},
+    {'name': 'Mathew', 'age': 22},
+    {'name': 'Roxanne', 'age': 20},
+    {'name': 'David', 'age': 18}
+]
+
+print(convert_to_list(students, 'age'))
