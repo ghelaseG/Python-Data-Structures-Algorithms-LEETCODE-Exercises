5dc324141a2810f4d7351cd9ceb81ff2a97f542f
ghelaseG <ghelase.georgian@gmail.com>
2024-09-19 23:55:48 +0100

    Practice Questions, Python Class,  n.5

---
+++ Leetcode Exercises/Practice Python Class/Basic exercises/5.py
@@ -0,0 +1,8 @@
+"""
+Define a Python function student(). Using function attributes display the names of all arguments.
+"""
+
+def student(student_id, student_name, student_class):
+    return f'Student ID: {student_id}\nStudent Name: {student_name}\nStudent Class: {student_class}'
+
+print(student('GG123', 'Georgian Ghelase', '10D'))
