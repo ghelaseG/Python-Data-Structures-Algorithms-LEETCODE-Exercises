d40ec5cb79186751e1c97aea6069fa790777d3af
ghelaseG <ghelase.georgian@gmail.com>
2024-09-26 23:42:29 +0100

    Practice Questions, Python Class,  n.12

---
+++ Leetcode Exercises/Practice Python Class/Basic exercises/12.py
@@ -0,0 +1,5 @@
+"""
+Write a Python class named Student with two instances student1, student2 and assign values to the instances' attributes. Print all the attributes of the student1, student2 instances with their values in the given format.
+"""
+
+
