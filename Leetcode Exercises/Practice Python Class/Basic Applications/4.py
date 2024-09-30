a984e7f2e9320f6dc55d948aaa33f718efa848e7
ghelaseG <ghelase.georgian@gmail.com>
2024-09-30 23:52:27 +0100

    Practice Questions, Python Class, Basic Application, nr.4

--- Leetcode Exercises/Practice Python Class/Basic Applications/4.py
+++ Leetcode Exercises/Practice Python Class/Basic Applications/4.py
@@ -3,3 +3,14 @@ Write a Python class to get all possible unique subsets from a set of distinct i
 Input : [4, 5, 6]
 Output : [[], [6], [5], [5, 6], [4], [4, 6], [4, 5], [4, 5, 6]]
 """
+
+class Solution4:
+    def sub_sets(self, sset):
+        return self.subsetsRecur([], sorted(sset))
+    
+    def subsetsRecur(self, current, sset):
+        if sset:
+            return self.subsetsRecur(current, sset[1:]) + self.subsetsRecur(current + [sset[0]], sset[1:])
+        return [current]
+    
+print(Solution4().sub_sets([4,5,6]))
