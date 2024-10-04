bffaea9f097e20b056b70d0a69588b2248a69cd0
ghelaseG <ghelase.georgian@gmail.com>
2024-10-04 23:59:01 +0100

    Practice Questions, Python Class, Basic Application, nr.8

--- Leetcode Exercises/Practice Python Class/Basic Applications/8.py
+++ Leetcode Exercises/Practice Python Class/Basic Applications/8.py
@@ -7,6 +7,5 @@ Expected Output : '.py hello'
 class Solution8:
     def reverse_words(self, word):
         return ' '.join(reversed(word.split()))
-    
 
 print(Solution8().reverse_words('hello .py'))
