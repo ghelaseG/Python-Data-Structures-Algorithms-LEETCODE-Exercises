95f4c1ccebad5f6c8242a76d01a359af04796791
ghelaseG <ghelase.georgian@gmail.com>
2024-10-06 00:15:42 +0100

    Practice Questions, Python Class, Basic Application, nr.9

--- Leetcode Exercises/Practice Python Class/Basic Applications/9.py
+++ Leetcode Exercises/Practice Python Class/Basic Applications/9.py
@@ -2,3 +2,16 @@
 Write a Python class that has two methods: get_String and print_String , get_String accept a string from the user and print_String prints the string in upper case.
 """
 
+class Solution9:
+    def __init__(self):
+        self.str1 = ""
+
+    def get_String(self):
+        self.str1 = input()
+
+    def print_String(self):
+        print(self.str1.upper())
+
+str1 = Solution9()
+str1.get_String()
+str1.print_String()
