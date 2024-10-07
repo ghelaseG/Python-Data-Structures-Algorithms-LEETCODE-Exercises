efaeed11cb8ce3c89be3284360603939ddf453d4
ghelaseG <ghelase.georgian@gmail.com>
2024-10-07 22:59:58 +0100

    Practice Questions, Python Class, Real Life Problems, 1.py, done

--- Leetcode Exercises/Practice Python Class/Real life problems/1.py
+++ Leetcode Exercises/Practice Python Class/Real life problems/1.py
@@ -12,3 +12,48 @@ overtime = hours_worked - 50
 Overtime amount = (overtime * (salary / 50))
 """
 
+class Employee:
+    def __init__(self, emp_name, emp_id, emp_salary, emp_department):
+        self.name = emp_name
+        self.id = emp_id
+        self.salary = emp_salary
+        self.department = emp_department
+    
+    def assign_department(self, emp_department):
+        self.department = emp_department
+    
+    def calculate_emp_salary(self, salary, hours_worked):
+        overtime = 0
+        if hours_worked > 50:
+            overtime = hours_worked - 50
+        self.salary = self.salary + (overtime * (self.salary / 50))
+    
+    def print_employee_details(self):
+        print('\nName:', self.name)
+        print('ID:', self.id)
+        print('Salary:', self.salary)
+        print('Department:', self.department)
+        print('..------------------------..')
+
+
+employee1 = Employee('Adams', 'A1234', 50000, 'Accounting')
+employee2 = Employee('Kala', 'K1234', 55000, 'Sales')
+employee3 = Employee('George', 'G1234', 10000, 'Data Consultant')
+
+print('Original Employee Details:')
+employee1.print_employee_details()
+employee2.print_employee_details()
+employee3.print_employee_details()
+
+print("\nChanging the departments of some of the employees")
+employee1.assign_department('Operator')
+employee3.assign_department('Driver')
+
+print("\nCalculate the overtime of the employees who are eligible:")
+employee3.calculate_emp_salary(40000, 72)
+employee2.calculate_emp_salary(45000, 60)
+
+print("Updated employee details:")
+employee1.print_employee_details()
+employee2.print_employee_details()
+employee3.print_employee_details()
