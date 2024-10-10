fa33bd73b6297184ae8c80f017bced4b18adc801
ghelaseG <ghelase.georgian@gmail.com>
2024-10-10 23:34:48 +0100

    Practice Questions, Python Class, Real Life Problems, 4.py, trial

--- Leetcode Exercises/Practice Python Class/Real life problems/4.py
+++ Leetcode Exercises/Practice Python Class/Real life problems/4.py
@@ -4,12 +4,22 @@ Use a dictionary to store the item details, where the key is the item_id and the
 """
 
 class Inventory:
-    def __init__(self, item_id, item_name, stock_count, price):
-    
+    def __init__(self):
+        self.inventory = {}
 
-    def add_item(self,):
+    def add_item(self, item_id, item_name, stock_count, price):
+        self.inventory[item_id] = {"item_name": item_name, "stock_count": stock_count, "price": price}
 
-    def update_item(self,):
-    
-    def check_item_details(self,)
-        
+    def update_item(self, item_id, stock_count, price):
+        if item_id in self.inventory:
+            self.inventory[item_id]["stock_count"] = stock_count
+            self.inventory[item_id]["price"] = price
+        else:
+            print("Item not found in the inventory")
+
+    def check_item_details(self, item_id):
+        if item_id in self.inventory:
+            item = self.inventory[item_id]
+            return f"Product name: {item['item_name']}, Stock: {item['stock_count']}, Price: {item['price']}"
+        else:
+            return "Item not found"        
