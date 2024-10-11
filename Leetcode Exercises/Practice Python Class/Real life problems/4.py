13a340271f5250c2010a127b91abd9ab9dc9901f
ghelaseG <ghelase.georgian@gmail.com>
2024-10-11 23:42:31 +0100

    Practice Questions, Python Class, Real Life Problems, 4.py, done

--- Leetcode Exercises/Practice Python Class/Real life problems/4.py
+++ Leetcode Exercises/Practice Python Class/Real life problems/4.py
@@ -23,3 +23,21 @@ class Inventory:
             return f"Product name: {item['item_name']}, Stock: {item['stock_count']}, Price: {item['price']}"
         else:
             return "Item not found"        
+
+inventory = Inventory()
+
+inventory.add_item("I001", "Laptop", 100, 500.00)
+inventory.add_item("I002", "Mobile", 110, 450.00)
+inventory.add_item("I003", "Desktop", 120, 500.00)
+inventory.add_item("I004", "Tablet", 90, 550.00)
+print("Item Details:")
+print(inventory.check_item_details("I001"))
+print(inventory.check_item_details("I002"))
+print(inventory.check_item_details("I003"))
+print(inventory.check_item_details("I004"))
+print("\nUpdate the price of item code - 'I001':")
+inventory.update_item("I001", 100, 505.00)
+print(inventory.check_item_details("I001"))
+print("\nUpdate the stock of item code - 'I003':")
+inventory.update_item("I003", 115, 500.00)
+print(inventory.check_item_details("I003"))
