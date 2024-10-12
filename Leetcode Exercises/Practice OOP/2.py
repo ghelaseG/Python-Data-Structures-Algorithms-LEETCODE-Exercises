431dadc7d1df897af7937f93fbe5206890d2451b
ghelaseG <ghelase.georgian@gmail.com>
2024-10-12 23:54:03 +0100

    Practice Questions, Python Object Oriented Programming, practice questions, 2.py, tril

--- Leetcode Exercises/Practice OOP/2.py
+++ Leetcode Exercises/Practice OOP/2.py
@@ -2,3 +2,24 @@
 Write a Python program to create a person class. Include attributes like name, country and date of birth. Implement a method to determine the person's age.
 """
 
+from datetime import date
+
+class Person:
+    def __init__(self, name, country, date_of_birth):
+        self.name = name
+        self.country = country
+        self.date_of_birth = date_of_birth
+    
+    #let's calculate the age based on their date of birth
+    def calculate_age(self):
+        today = date.today()
+        age = today.year - self.date_of_birth.year
+        if today < date(today.year, self.date_of_birth.month, self.date_of_birth.day):
+            age -= 1
+        return age
+    
+person1 = Person("Ferdi Odilia", "France", date(1962, 7, 12))
+person2 = Person("Shweta Maddox", "Canada", date(1982, 10, 20))
+person3 = Person("Elizaveta Tilman", "USA", date(2000, 1, 1))
+
+
