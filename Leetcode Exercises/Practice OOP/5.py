83eada337f969437484b55347788b2b06051437d
ghelaseG <ghelase.georgian@gmail.com>
2024-10-15 22:04:33 +0100

    Practice Questions, Python Object Oriented Programming, practice questions, 5.py, mock

---
+++ Leetcode Exercises/Practice OOP/5.py
@@ -0,0 +1,10 @@
+"""
+Write a Python program to create a class representing a binary search tree. Include methods for inserting and searching for elements in the binary tree.
+"""
+
+"""
+Binary search tree:
+
+In computer science, a binary search tree (BST), also called an ordered or sorted binary tree, is a rooted binary tree data structure with the key of each internal node being greater than all the keys in the respective node's left subtree and less than the ones in its right subtree. The time complexity of operations on the binary search tree is directly proportional to the height of the tree.
+"""
+
