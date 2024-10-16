755c433279a320d99353104f65fc4451df16fd4f
ghelaseG <ghelase.georgian@gmail.com>
2024-10-16 23:48:22 +0100

    Practice Questions, Python Object Oriented Programming, practice questions, 6.py

--- Leetcode Exercises/Practice OOP/6.py
+++ Leetcode Exercises/Practice OOP/6.py
@@ -26,7 +26,39 @@ class Stack:
     def size(self):
         return len(self.items)
     
+    #peek at the top item of the stack without removing it, if the stack is not empty
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+        else:
+            return "Empty stack"
     
+#example
+stack = Stack()
+
+#push items into the stack
+stack.push(0)
+stack.push(1)
+stack.push(2)
+stack.push(3)
+stack.push(4)
+stack.push(5)
     
+print("Stack size:", stack.size())
+print("Top element:", stack.peek())
+
+# Pop an item from the stack, and print the popped item, and the updated size and top element
+popped_item = stack.pop()
+print("\nPopped item:", popped_item)
+print("\nStack size:", stack.size())
+print("Top element:", stack.peek())
+
+# Create another instance of the Stack class
+stack1 = Stack()
+
+# Print the size of the empty stack and attempt to pop an item (with an error message)
+print("\nStack size:", stack1.size())
+popped_item = stack1.pop()
+print("\nPopped item:", popped_item) 
 
 
