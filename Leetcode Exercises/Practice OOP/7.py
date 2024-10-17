e60faa934360550b9e5709f1ea6bfab90f129c83
ghelaseG <ghelase.georgian@gmail.com>
2024-10-17 17:33:57 +0100

    Practice Questions, Python Object Oriented Programming, practice questions, 7.py

---
+++ Leetcode Exercises/Practice OOP/7.py
@@ -0,0 +1,53 @@
+"""
+Write a Python program to create a class representing a linked list data structure. Include methods for displaying linked list data, inserting and deleting nodes.
+"""
+
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+
+    def __init__(self):
+        self.head = None
+
+    #display the elements
+    def display(self):
+        current = self.head
+        while current:
+            print(current.data, end = "")
+            current = current.next
+        print()
+
+    #insert a new node at the end of the linked list
+    def insert(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = new_node
+        
+    #delete a node with the given data
+    def delete(self, data):
+        if not self.head:
+            return
+
+        if self.head.data == data:
+            self.head = self.head.next
+            return
+        
+        current = self.head
+        prev = None
+        while current and current.data != data:
+            prev = current
+            current = current.next
+        
+        if current:
+            prev.next = current.next
+        
+
