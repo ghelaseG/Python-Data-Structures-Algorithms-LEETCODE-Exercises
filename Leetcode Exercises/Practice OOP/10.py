249068be21a65d27c5d15c2a5a9af5284f91c48a
ghelaseG <ghelase.georgian@gmail.com>
2024-10-19 07:06:50 +0100

    Practice Questions, Python Object Oriented Programming, practice questions, 10.py, corrected

--- Leetcode Exercises/Practice OOP/10.py
+++ Leetcode Exercises/Practice OOP/10.py
@@ -11,11 +11,12 @@ class Queue:
     def enqueue(self, item):
         return self.items.append(item)
     
+    #we remove from the front of the queue
     def dequeue(self):
-        if not self.items.is_empty():
+        if not self.is_empty():
             return self.items.pop(0)
         else:
-            raise IndexError("Cannot remove from an empty list")
+            raise IndexError("Cannot remove from an empty queue")
         
     def is_empty(self):
-        return len(self.items())
+        return len(self.items()) == 0
