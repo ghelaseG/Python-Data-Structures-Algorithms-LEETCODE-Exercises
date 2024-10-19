74f27db82212aafe406b44cba93d1bc1f5c46fc3
ghelaseG <ghelase.georgian@gmail.com>
2024-10-19 07:05:14 +0100

    Practice Questions, Python Object Oriented Programming, practice questions, 10.py, small errors

--- Leetcode Exercises/Practice OOP/10.py
+++ Leetcode Exercises/Practice OOP/10.py
@@ -3,3 +3,19 @@ Write a Python program to create a class representing a queue data structure. In
 """
 
 
+class Queue:
+
+    def __init__(self):
+        self.items = []
+
+    def enqueue(self, item):
+        return self.items.append(item)
+    
+    def dequeue(self):
+        if not self.items.is_empty():
+            return self.items.pop(0)
+        else:
+            raise IndexError("Cannot remove from an empty list")
+        
+    def is_empty(self):
+        return len(self.items())
