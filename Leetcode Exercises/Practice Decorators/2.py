6dc7baae4e95056e9f449eeff6ef6ad7259be218
ghelaseG <ghelase.georgian@gmail.com>
2024-10-22 23:18:41 +0100

    Practice Questions, Python Decorator,example n.02, example

--- Leetcode Exercises/Practice Decorators/2.py
+++ Leetcode Exercises/Practice Decorators/2.py
@@ -6,7 +6,7 @@ import time
 #let's create the decorator
 
 def measure_execution_time(func):
-
+    #let's add an extra wrapper function to add functionality to the main function
     def wrapper(*args, **kwargs):
         start_time = time.time()
         result = func(*args, **kwargs)
@@ -15,3 +15,15 @@ def measure_execution_time(func):
         print(f"Function {func.__name__} took {execution_time:.4f} seconds to execute")
         return result
     return wrapper
+
+#example
+@measure_execution_time
+def calculate_x(numbers):
+    total = 1
+    for x in numbers:
+        total = total * x
+    return total
+
+#call the decorated function
+result = calculate_x([1,2,3,4,5])
+print("Result:", result)
