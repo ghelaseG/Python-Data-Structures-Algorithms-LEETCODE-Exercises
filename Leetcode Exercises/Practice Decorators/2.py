426a9732c24d38a69b0cbf4b55bde01ec8ebdb99
ghelaseG <ghelase.georgian@gmail.com>
2024-10-22 23:14:19 +0100

    Practice Questions, Python Decorator,example n.02, code

--- Leetcode Exercises/Practice Decorators/2.py
+++ Leetcode Exercises/Practice Decorators/2.py
@@ -1,4 +1,17 @@
 """
 Write a Python program to create a decorator function to measure the execution time of a function.
 """
+import time
 
+#let's create the decorator
+
+def measure_execution_time(func):
+
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        execution_time = end_time - start_time
+        print(f"Function {func.__name__} took {execution_time:.4f} seconds to execute")
+        return result
+    return wrapper
