d474e0889b2841cbedfc9daf2bc14aa1ff481757
ghelaseG <ghelase.georgian@gmail.com>
2024-10-24 12:33:45 +0100

    Practice Questions, Python Decorator,example n.04, example

--- Leetcode Exercises/Practice Decorators/4.py
+++ Leetcode Exercises/Practice Decorators/4.py
@@ -18,3 +18,16 @@ def cache_result(func):
         return result
     
     return wrapper
+
+@cache_result
+def calculate_multiply(x, y):
+    print("Calculating the product of two numbers ...")
+    return x * y
+
+#Call the decorated function multiple times
+print(calculate_multiply(4, 5))  # Calculation is performed
+print(calculate_multiply(4, 5))  # Result is retrieved from cache
+print(calculate_multiply(5, 7))  # Calculation is performed
+print(calculate_multiply(5, 7))  # Result is retrieved from cache
+print(calculate_multiply(-3, 7))  # Calculation is performed
+print(calculate_multiply(-3, 7))  # Result is retrieved from cache
