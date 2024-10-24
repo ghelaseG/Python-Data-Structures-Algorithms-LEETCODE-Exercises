134be8848c2b1614e327abe87468309bc59c0221
ghelaseG <ghelase.georgian@gmail.com>
2024-10-24 11:59:25 +0100

    Practice Questions, Python Decorator,example n.04, code

--- Leetcode Exercises/Practice Decorators/4.py
+++ Leetcode Exercises/Practice Decorators/4.py
@@ -1,3 +1,20 @@
 """
 Write a Python program that implements a decorator to cache the result of a function.
 """
+
+def cache_result(func):
+    cache = {}
+
+    def wrapper(*args, **kwargs):
+        key = (*args, *kwargs.items())
+
+        if key in cache:
+            print("Retrieving result from cache ...")
+            return cache[key]
+        
+        result = func(*args, **kwargs)
+        cache[key] = result
+
+        return result
+    
+    return wrapper
