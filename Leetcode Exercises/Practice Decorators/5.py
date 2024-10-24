f892eb7c469a97c43d34776f9e4334b066e8b9cb
ghelaseG <ghelase.georgian@gmail.com>
2024-10-24 15:00:38 +0100

    Practice Questions, Python Decorator,example n.05, code + example

--- Leetcode Exercises/Practice Decorators/5.py
+++ Leetcode Exercises/Practice Decorators/5.py
@@ -5,4 +5,17 @@ Write a Python program that implements a decorator to validate function argument
 
 def validate_arguments(condition):
     def decorator(func):
-        
+        def wrapper(*args, **kwargs):
+            if condition(*args, **kwargs):
+                return func(*args, **kwargs)
+            else:
+                raise ValueError("Invalid arguments passed to the function")
+        return wrapper
+    return decorator
+
+@validate_arguments(lambda x: x > 0)
+def calculate_cube(x):
+    return x ** 3
+
+print(calculate_cube(5))
+print(calculate_cube(-2))
