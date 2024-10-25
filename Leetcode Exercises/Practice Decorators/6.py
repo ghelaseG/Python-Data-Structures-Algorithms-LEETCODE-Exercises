2f74291230ddf0a16c57a7cc38e34a33659fb723
ghelaseG <ghelase.georgian@gmail.com>
2024-10-25 23:27:18 +0100

    Practice Questions, Python Decorator,example n.06, code

--- Leetcode Exercises/Practice Decorators/6.py
+++ Leetcode Exercises/Practice Decorators/6.py
@@ -1,3 +1,21 @@
 """
 Write a Python program that implements a decorator to retry a function multiple times in case of failure.
 """
+
+import sqlite3
+import time
+
+def retry_on_failure(max_retries, delay = 1):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            for _ in range(max_retries):
+                try:
+                    result = func(*args, **kwargs)
+                    return result
+                except Exception as e:
+                    print(f"Error occurred: {e}. Retrying ...")
+                    time.sleep(delay)
+            raise Exception("Maximum retries exceeded. Function failed.")
+        
+        return wrapper
+    return decorator
