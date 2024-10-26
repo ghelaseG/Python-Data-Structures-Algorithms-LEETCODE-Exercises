aa35ee0c8eb55867bae13fd90d48f657efed6713
ghelaseG <ghelase.georgian@gmail.com>
2024-10-26 18:10:26 +0100

    Practice Questions, Tips & Tricks, 1.py,interactive ways

---
+++ Tips & Tricks/1.py
@@ -0,0 +1,15 @@
+""""
+You can use the following to run only one of the secondary functions interactive way:
+"""
+
+
+# %%
+
+def add_function(x, y):
+    return x + y
+# %%
+x = 2
+y = 3
+result = add_function(x, y)
+print(result)
+# %%
