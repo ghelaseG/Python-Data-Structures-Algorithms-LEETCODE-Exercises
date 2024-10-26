bcc7b7d9f232263e54483b40b7cdcb6d35901394
ghelaseG <ghelase.georgian@gmail.com>
2024-10-26 21:27:22 +0100

    Practice Questions, Python Decorator,example n.08, mock

---
+++ Leetcode Exercises/Practice Decorators/8.py
@@ -0,0 +1,8 @@
+"""
+Write a Python program that implements a decorator to add logging functionality to a function.
+"""
+
+"""
+Logging functionality refers to the capability of recording and storing information about program execution. It allows you to capture significant events, messages, and errors that occur during code execution.
+"""
+
