f7db7e52883c4b824d6d0fcfdd57f6d254985f67
ghelaseG <ghelase.georgian@gmail.com>
2024-10-26 10:48:39 +0100

    Practice Questions, Python Decorator,example n.07, code

--- Leetcode Exercises/Practice Decorators/7.py
+++ Leetcode Exercises/Practice Decorators/7.py
@@ -1,3 +1,30 @@
 """
 Write a Python program that implements a decorator to enforce rate limits on a function.
 """
+
+import time
+
+def rate_time_limits(max_calls, period):
+    
+    def decorator(func):
+        calls = 0 
+        last_reset = time.time()
+
+        def wrapper(*args, **kwargs):
+            nonlocal calls, last_reset
+
+            elapsed = time.time() - last_reset
+
+            if elapsed > period:
+                calls = 0
+                last_reset = time.time()
+
+            if calls >= max_calls:
+                raise Exception("Rate limit exceeded. Try again !!")
+            
+            calls += 1
+
+            return func(*args, **kwargs)
+        
+        return wrapper
+    return decorator
