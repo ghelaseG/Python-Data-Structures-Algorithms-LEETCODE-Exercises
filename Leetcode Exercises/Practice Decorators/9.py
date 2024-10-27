14c981c01e652da1f90a1edc4cf71745299b5eac
ghelaseG <ghelase.georgian@gmail.com>
2024-10-27 16:39:10 +0000

    Practice Questions, Python Decorator,example n.09, exp

--- Leetcode Exercises/Practice Decorators/9.py
+++ Leetcode Exercises/Practice Decorators/9.py
@@ -7,9 +7,17 @@ def handle_exceptions(default_response):
     def decorator(func):
         def wrapper(*args, **kwargs):
             try:
-                return func(*args, **kwargs):
+                return func(*args, **kwargs)
             except Exception as e:
                 print(f"Exception occurred: {e}")
                 return default_response
         return wrapper
     return decorator
+
+@handle_exceptions(default_response="Error!!!")
+
+def divide_num(x, y):
+    return x / y
+
+result = divide_num(7, 0)
+print("Result:", result)
