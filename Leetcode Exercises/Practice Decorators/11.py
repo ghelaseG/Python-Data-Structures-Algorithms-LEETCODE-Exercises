23628e0d1db3c1c151353188b78430376fbdbb4c
ghelaseG <ghelase.georgian@gmail.com>
2024-10-28 14:49:59 +0000

    Practice Questions, Python Decorator,example n.11, code

---
+++ Leetcode Exercises/Practice Decorators/11.py
@@ -0,0 +1,24 @@
+"""
+Write a Python program that implements a decorator to measure the memory usage of a function.
+"""
+
+import tracemalloc
+
+def measure_memory_usage(func):
+    def wrapper(*args, **kwargs):
+        tracemalloc.start()
+
+        result = func(*args, **kwargs)
+
+        snapshot = tracemalloc.take_snapshot()
+        top_stats = snapshot.statistics("lineno")
+
+        print(f"Memory usage of {func.__name__}:")
+        for stat in top_stats[:5]:
+            print(stat)
+
+        return result
+    
+    return wrapper
+
+
