7d13546654b97855b02e3a548682d10a4c78d473
ghelaseG <ghelase.georgian@gmail.com>
2024-10-28 22:54:53 +0000

    Practice Questions, Python Decorator,example n.11, code

--- Leetcode Exercises/Practice Decorators/12.py
+++ Leetcode Exercises/Practice Decorators/12.py
@@ -1,4 +1,20 @@
 """
 Write a Python program that implements a decorator to provide caching with expiration time for a function.
 """
+import time
 
+def cache_with_expiry(expiry_time):
+    def decorator(func):
+        cache = {}
+        def wrapper(*args, **kwargs):
+            key = (*args, *kwargs.items())
+            if key in cache:
+                value, timestamp = cache[key]
+                if time.time() - timestamp < expiry_time:
+                    print("Retrieving result from cache ...")
+                    return value
+            result = func(*args, **kwargs)
+            cache[key] = (result, time.time())
+            return result
+        return wrapper
+    return decorator
