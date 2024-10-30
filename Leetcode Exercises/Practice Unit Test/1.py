9ca857d0e6bb26b3db7e2067e4672d9e577174e6
ghelaseG <ghelase.georgian@gmail.com>
2024-10-30 21:40:40 +0000

    Practice Questions, Python Unit Test, exercise n.01, func

--- Leetcode Exercises/Practice Unit Test/1.py
+++ Leetcode Exercises/Practice Unit Test/1.py
@@ -2,3 +2,13 @@
 Write a Python unit test program to check if a given number is prime or not.
 """
 
+import unittest
+
+#define a function prime to check if a number is prime
+def check_num_prime(number):
+    if number < 2:
+        return False
+    for i in range(2, int(number ** 0.5) + 1):
+        if number % i == 0:
+            return False
+        
