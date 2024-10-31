e054b29a3af8a21bd4e44c1e6717e58755010eaa
ghelaseG <ghelase.georgian@gmail.com>
2024-10-31 23:56:41 +0000

    Practice Questions, Python Unit Test, exercise n.02, code

--- Leetcode Exercises/Practice Unit Test/2.py
+++ Leetcode Exercises/Practice Unit Test/2.py
@@ -1,3 +1,10 @@
 """
 Write a Python unit test program to check if a list is sorted in ascending order.
 """
+import unittest
+
+#we build a function to check if a list is sorted ascending order
+def is_sorted_ascending(lst):
+    #check if all elements at index i are less than or equal to elements at index i + 1
+    return all(lst[i] <= lst[i + 1] for i in range(len(lst) - 1))
+
