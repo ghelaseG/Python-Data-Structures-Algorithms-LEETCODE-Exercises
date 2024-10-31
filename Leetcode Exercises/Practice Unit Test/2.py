6272ada674c9eaef1d5afe43005a34f92dc0f811
ghelaseG <ghelase.georgian@gmail.com>
2024-10-31 23:59:37 +0000

    Practice Questions, Python Unit Test, exercise n.02, func test

--- Leetcode Exercises/Practice Unit Test/2.py
+++ Leetcode Exercises/Practice Unit Test/2.py
@@ -8,3 +8,11 @@ def is_sorted_ascending(lst):
     #check if all elements at index i are less than or equal to elements at index i + 1
     return all(lst[i] <= lst[i + 1] for i in range(len(lst) - 1))
 
+class TestSortedAsc(unittest.TestCase):
+    def test_sorted_list(self):
+        lst = [1, 2, 3, 4, 5]
+
+        print("Sorted list:", lst)
+
+        self.assertTrue(is_sorted_ascending(lst), "The list is not sorted in ascending order")
+        
