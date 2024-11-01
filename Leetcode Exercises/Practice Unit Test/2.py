343fb722df965b4f3ac5d3117cd892a91aeda95b
ghelaseG <ghelase.georgian@gmail.com>
2024-11-01 00:48:51 +0000

    Practice Questions, Python Unit Test, exercise n.02, final

--- Leetcode Exercises/Practice Unit Test/2.py
+++ Leetcode Exercises/Practice Unit Test/2.py
@@ -15,4 +15,12 @@ class TestSortedAsc(unittest.TestCase):
         print("Sorted list:", lst)
 
         self.assertTrue(is_sorted_ascending(lst), "The list is not sorted in ascending order")
-        
+
+    def test_unsorted_list(self):
+        lst = [3, 8, 9, 2, 10]
+        print("Unsorted list:", lst) 
+        self.assertFalse(is_sorted_ascending(lst), "The list is sorted in ascending order")
+
+
+if __name__ == '__main__':
+    unittest.main()  
