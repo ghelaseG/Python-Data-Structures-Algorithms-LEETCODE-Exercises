90314863286977271322fda8790e23361e1a4f56
ghelaseG <ghelase.georgian@gmail.com>
2024-11-03 00:27:38 +0000

    Practice Questions, Python Unit Test, exercise n.04, example

--- Leetcode Exercises/Practice Unit Test/4.py
+++ Leetcode Exercises/Practice Unit Test/4.py
@@ -7,3 +7,20 @@ import unittest
 def is_palindrome(string):
     return string == string[::-1]
 
+class TestPalindrome(unittest.TestCase):
+    def test_palindrome_string(self):
+        #palindrome = "madam"
+        palindrome = "hello"
+        print("Test palindrome:", palindrome)
+        self.assertTrue(is_palindrome(palindrome), "The string is a palindrome")
+
+    def test_non_palindrome(self):
+
+        non_palindrome = "hello"
+        print("Test non palindrome:", non_palindrome)
+
+        self.assertFalse(is_palindrome(non_palindrome), "The string is not a palindrome")
+
+
+if __name__ == '__main__':
+    unittest.main()
