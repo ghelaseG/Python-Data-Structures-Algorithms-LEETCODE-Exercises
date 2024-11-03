a033038ec715ab48a7fe0ad522409adfacb0ed22
ghelaseG <ghelase.georgian@gmail.com>
2024-11-03 23:20:16 +0000

    Practice Questions, Python Unit Test, exercise n.05, code with examples

--- Leetcode Exercises/Practice Unit Test/5.py
+++ Leetcode Exercises/Practice Unit Test/5.py
@@ -1,3 +1,30 @@
 """
 Write a Python unit test program to check if a file exists in a specified directory.
 """
+
+import os
+import unittest
+
+def file_exists(directory, filename):
+    #create the fill file path by joining the directory and filename
+    file_path = os.path.join(directory, filename)
+    #check if the file exists there
+    return os.path.exists(file_path)
+
+class TestFileExists(unittest.TestCase):
+    def test_existing_file(self):
+        #define the directory and filename for an existing file
+        directory = '/path/txt'
+        filename = 'test1.txt'
+        #assert that the file exists in the folder
+        self.assertTrue(file_exists(directory, filename), "The file does not exist in the specified directory")
+
+    def test_non_existent_file(self):
+        directory = '/path/txt'
+        filename = 'test2.txt'
+
+        self.assertFalse(file_exists(directory, filename), "The file exists in the specified directory")
+
+if __name__ == '__main__':
+    unittest.main()
+    
