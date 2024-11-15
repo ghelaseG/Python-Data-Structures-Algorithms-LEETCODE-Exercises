6f67fe3008aa26572b202b4919dd529e8b973f88
ghelaseG <ghelase.georgian@gmail.com>
2024-11-15 23:38:11 +0000

    Practice Questions, Python Exception Handling, exercise n.07, code

--- Leetcode Exercises/Practice Exception Handling/7.py
+++ Leetcode Exercises/Practice Exception Handling/7.py
@@ -2,3 +2,11 @@
 Write a Python program that prompts the user to input a number and handles a KeyboardInterrupt exception if the user cancels the input.
 """
 
+def keyboard_intr(prompt):
+    try:
+        prompt = int(input(prompt))
+        print("The number you chose is:", prompt)
+    except KeyboardInterrupt:
+        print("This has been cancelled")
+
+keyboard_intr("Write a number:") #CTRl + C
