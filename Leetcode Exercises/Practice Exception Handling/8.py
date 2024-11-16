01c90926e69708c7fd5453853ba0f46e7ffc49c9
ghelaseG <ghelase.georgian@gmail.com>
2024-11-16 23:37:14 +0000

    Practice Questions, Python Exception Handling, exercise n.08, code

--- Leetcode Exercises/Practice Exception Handling/8.py
+++ Leetcode Exercises/Practice Exception Handling/8.py
@@ -2,3 +2,14 @@
 Write a Python program that executes division and handles an ArithmeticError exception if there is an arithmetic error.
 """
 
+def division(dividend, divisor):
+    try:
+        result = dividend / divisor
+        print("Total: ", result)
+    except ArithmeticError:
+        print("Error: arithmetic error occurred")
+
+dividend = float(input("Write the dividend:"))
+divisor = float(input("Write the divisor:"))
+
+division(dividend, divisor)
