b224e0fe5d63e837c1d4602b20d1a7a1ba1c3382
ghelaseG <ghelase.georgian@gmail.com>
2024-11-16 23:55:20 +0000

    Practice Questions, Python Exception Handling, exercise n.10, code

--- Leetcode Exercises/Practice Exception Handling/10.py
+++ Leetcode Exercises/Practice Exception Handling/10.py
@@ -2,3 +2,15 @@
 Write a Python program that executes a list operation and handles an AttributeError exception if the attribute does not exist.
 """
 
+def list_operation(nums):
+    try:
+        result = len(nums)
+        print("Length:", result)
+    except AttributeError:
+        print("Error: the list does not have a 'length' attribute")
+
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+list_operation(nums)
+
+nums2 = ('abc')
+list_operation(nums2)
