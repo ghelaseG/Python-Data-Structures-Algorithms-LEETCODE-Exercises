ec93d88957fdb7396a820d6d1fc3ee8980c9eee1
ghelaseG <ghelase.georgian@gmail.com>
2024-11-17 00:38:40 +0000

    Practice Questions, Python Exception Handling, exercise n.10, some trials

--- Leetcode Exercises/Practice Exception Handling/10.py
+++ Leetcode Exercises/Practice Exception Handling/10.py
@@ -4,13 +4,22 @@ Write a Python program that executes a list operation and handles an AttributeEr
 
 def list_operation(nums):
     try:
-        result = len(nums)
-        print("Length:", result)
+        operation = input("write an operation you want to be executed:")
+        exec(operation)
+        #resulty = len(nums)
+        #print(type(resulty))
+        #result = operation, nums
+        #print(type(result))
+        #result2 = result.join()
+        #print(result2)
+        #result3 = result2(nums)
+        result = eval(f"{operation}({nums})")
+        print("Operation result is:", result)
     except AttributeError:
         print("Error: the list does not have a 'length' attribute")
 
 nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 list_operation(nums)
 
-nums2 = ('abc')
-list_operation(nums2)
+#nums2 = ('abc')
+#list_operation(nums2)
