2c881aed87ab10de52363b84b73f1948843a7d9d
ghelaseG <ghelase.georgian@gmail.com>
2024-11-18 23:57:25 +0000

    Practice Questions, Neetcode 150, Arrays and Hashing, exercise 1, code

--- Leetcode Exercises/Neetcode 150/Arrays and Hashing/contains_duplicates.py
+++ Leetcode Exercises/Neetcode 150/Arrays and Hashing/contains_duplicates.py
@@ -3,5 +3,16 @@ Given an integer array nums, return true if any value appears more than once in 
 """
 
 class Solution:
-    def hasDuplicate(self, nums: List[int]) -> bool:
-        
+    def hasDuplicate(self, nums: list[int]) -> bool:
+        hashset = set()
+
+        for n in nums:
+            if n in hashset:
+                return True
+            else:
+                hashset.add(n)
+        return False
+
+solution = Solution()
+nums = [1,2,3,4,1,2]
+print(solution.hasDuplicate(nums))
