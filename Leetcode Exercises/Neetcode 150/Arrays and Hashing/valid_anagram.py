e45ce2e3f3cac23c7836230cbc126535e35fe1b7
ghelaseG <ghelase.georgian@gmail.com>
2024-11-20 22:00:08 +0000

    Practice Questions, Neetcode 150, Arrays and Hashing, exercise 2, code 1

--- Leetcode Exercises/Neetcode 150/Arrays and Hashing/valid_anagram.py
+++ Leetcode Exercises/Neetcode 150/Arrays and Hashing/valid_anagram.py
@@ -4,3 +4,21 @@ Given two strings s and t, return true if the two strings are anagrams of each o
 An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
 """
 
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        return sorted(s) == sorted(t)
+
+solution = Solution() 
+
+s = "racecar"
+t = "carrace"
+
+print(solution.isAnagram(s, t))
+
+s = "jar"
+t = "jam"
+
+print(solution.isAnagram(s, t))
