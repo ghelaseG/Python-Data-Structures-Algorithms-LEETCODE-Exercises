f181d074d80d44e2e2fb1e0666111d81be556724
ghelaseG <ghelase.georgian@gmail.com>
2024-11-21 23:31:59 +0000

    Practice Questions, Neetcode 150, Arrays and Hashing, exercise 2, code 2

--- Leetcode Exercises/Neetcode 150/Arrays and Hashing/valid_anagram.py
+++ Leetcode Exercises/Neetcode 150/Arrays and Hashing/valid_anagram.py
@@ -4,21 +4,46 @@ Given two strings s and t, return true if the two strings are anagrams of each o
 An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
 """
 
+#class Solution:
+#     def isAnagram(self, s: str, t: str) -> bool:
+#         if len(s) != len(t):
+#             return False
+        
+#         return sorted(s) == sorted(t)
+
+# solution = Solution() 
+
+# s = "racecar"
+# t = "carrace"
+
+# print(solution.isAnagram(s, t))
+
+# s = "jar"
+# t = "jam"
+
+# print(solution.isAnagram(s, t))
+
 class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
+    def is_anagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
         
-        return sorted(s) == sorted(t)
+        countS, countT = {}, {}
+
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+        return countS == countT
 
 solution = Solution() 
 
 s = "racecar"
 t = "carrace"
 
-print(solution.isAnagram(s, t))
+print(solution.is_anagram(s, t))
 
 s = "jar"
 t = "jam"
 
-print(solution.isAnagram(s, t))
+print(solution.is_anagram(s, t))
+    
