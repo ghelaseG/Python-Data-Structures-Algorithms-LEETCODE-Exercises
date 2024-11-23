813212e307bfe8fdb7ed353fbc36cf6eaa4abf8f
ghelaseG <ghelase.georgian@gmail.com>
2024-11-23 23:20:57 +0000

    Practice Questions, Neetcode 150, Arrays and Hashing, exercise 3, code, hash map

--- Leetcode Exercises/Neetcode 150/Arrays and Hashing/tempCodeRunnerFile.py
+++ Leetcode Exercises/Neetcode 150/Arrays and Hashing/tempCodeRunnerFile.py
@@ -1,18 +1,33 @@
 from typing import List
 
-class Solution:
-    def hasDuplicates(self, nums: List[int]) ->bool:
-
-        hashmap = {}
-
-        for num in nums:
-            if num in hashmap:
-                return True
-            else:
-                hashmap[num] = 2
-        
-        return False
+# class Solution:
+#     def two_sum(self, nums: List[int], target: int) -> List[int]:
+#         for i in range(len(nums)):
+#             for j in range(i + 1, len(nums)):
+#                 if nums[i] + nums[j] == target:
+#                     return [i, j]
+#         return []
     
-solution = Solution()
-nums = [1,2,3,4,1,2]
-print(solution.hasDuplicates(nums))
+# nums = [3,4,5,6] 
+# target = 7
+
+# solution =  Solution()
+# print(solution.two_sum(nums, target))
+
+class Solution:
+    def two_sum(self, nums: List[int], target: int) -> List[int]:
+        indices = {} #value is the index
+
+        for i, j in enumerate(nums):
+            indices[j] = i
+
+        for i, j in enumerate(nums):
+            difference = target - j
+            if difference in indices and indices[difference] != i:
+                return [i, indices[difference]]
+
+nums = [3,4,5,6] 
+target = 7
+            
+solution =  Solution()
+print(solution.two_sum(nums, target))
--- Leetcode Exercises/Neetcode 150/Arrays and Hashing/two_sum.py
+++ Leetcode Exercises/Neetcode 150/Arrays and Hashing/two_sum.py
@@ -7,16 +7,34 @@ Return the answer with the smaller index first.
 """
 from typing import List
 
+# class Solution:
+#     def two_sum(self, nums: List[int], target: int) -> List[int]:
+#         for i in range(len(nums)):
+#             for j in range(i + 1, len(nums)):
+#                 if nums[i] + nums[j] == target:
+#                     return [i, j]
+#         return []
+    
+# nums = [3,4,5,6] 
+# target = 7
+
+# solution =  Solution()
+# print(solution.two_sum(nums, target))
+
 class Solution:
     def two_sum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
-        return []
-    
+        indices = {} #value is the index
+
+        for i, j in enumerate(nums):
+            indices[j] = i
+
+        for i, j in enumerate(nums):
+            difference = target - j
+            if difference in indices and indices[difference] != i:
+                return [i, indices[difference]]
+
 nums = [3,4,5,6] 
 target = 7
-
+            
 solution =  Solution()
 print(solution.two_sum(nums, target))
