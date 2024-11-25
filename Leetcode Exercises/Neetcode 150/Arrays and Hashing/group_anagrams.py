df74e2160b81d26a6a32fc247d86d4f0f93459dd
ghelaseG <ghelase.georgian@gmail.com>
2024-11-25 00:03:57 +0000

    Practice Questions, Neetcode 150, Arrays and Hashing, exercise 4, group anagrams, code

--- Leetcode Exercises/Neetcode 150/Arrays and Hashing/group_anagrams.py
+++ Leetcode Exercises/Neetcode 150/Arrays and Hashing/group_anagrams.py
@@ -3,3 +3,19 @@ Given an array of strings strs, group all anagrams together into sublists. You m
 
 An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
 """
+
+from typing import List
+from collections import defaultdict
+
+class Solution:
+    def group_anagrams(self, strs: List[str]) -> List[List[str]]:
+        result = defaultdict(list)
+        for i in strs:
+            sorted_s = ''.join(sorted(i))
+            result[sorted_s].append(i)
+        return list(result.values())
+    
+strs = ["act","pots","tops","cat","stop","hat"]
+
+solution = Solution()
+print(solution.group_anagrams(strs))
