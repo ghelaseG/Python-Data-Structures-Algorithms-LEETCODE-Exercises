b32bfdfb396904ced490a05f64be881640a3ddcf
ghelaseG <ghelase.georgian@gmail.com>
2024-11-26 23:35:17 +0000

    Practice Questions, Neetcode 150, Arrays and Hashing, exercise 4, group anagrams, code, part 3

--- Leetcode Exercises/Neetcode 150/Arrays and Hashing/group_anagrams.py
+++ Leetcode Exercises/Neetcode 150/Arrays and Hashing/group_anagrams.py
@@ -19,30 +19,54 @@ An anagram is a string that contains the exact same characters as another string
 
 #part 2
 
+# from typing import List
+# from collections import defaultdict
+
+# class Solution:
+#     def group_anagrams(self, strs: List[str]) -> List[List[str]]: 
+#         result = defaultdict(list)
+
+#         for s in strs:
+#             #in the following we store counts of each character from a to z
+#             count = [0] * 26
+
+#             for char in s:
+#                 #substract ASCII of c with "a" and store count of character in array
+#                 count[ord(char) - ord("a")] += 1
+#                 # Example ASCII(a) = 80, 80-80=0
+#                 #         ASCII(b) = 81, 81-80=1
+#                 #         ASCII(z) = 105, 105-80=25
+
+#             result[tuple(count)].append(s)
+
+#         return result.values()
+    
+
+# strs = ["act","pots","tops","cat","stop","hat"]
+
+# solution = Solution()
+# print(solution.group_anagrams(strs))
+
+
+#part 3
+
 from typing import List
 from collections import defaultdict
 
 class Solution:
-    def group_anagrams(self, strs: List[str]) -> List[List[str]]: 
-        result = defaultdict(list)
+    def group_anagrams(self, strs: List[str]) -> List[List[str]]:
+        hash_map = defaultdict(list)
+        result = []
 
         for s in strs:
-            #in the following we store counts of each character from a to z
-            count = [0] * 26
+            sorted_s = tuple(sorted(s))
+            hash_map[sorted_s].append(s)
 
-            for char in s:
-                #substract ASCII of c with "a" and store count of character in array
-                count[ord(char) - ord("a")] += 1
-                # Example ASCII(a) = 80, 80-80=0
-                #         ASCII(b) = 81, 81-80=1
-                #         ASCII(z) = 105, 105-80=25
+        for value in hash_map.values():
+            result.append(value)
 
-            result[tuple(count)].append(s)
-
-        return result.values()
+        return result
     
-
 strs = ["act","pots","tops","cat","stop","hat"]
-
 solution = Solution()
 print(solution.group_anagrams(strs))
--- Leetcode Exercises/Neetcode 150/Arrays and Hashing/tempCodeRunnerFile.py
+++ Leetcode Exercises/Neetcode 150/Arrays and Hashing/tempCodeRunnerFile.py
@@ -1,28 +1,20 @@
-
 from typing import List
 from collections import defaultdict
 
 class Solution:
-    def group_anagrams(self, strs: List[str]) -> List[List[str]]: 
-        result = defaultdict(list)
+    def group_anagrams(self, strs: List[str]) -> List[List[str]]:
+        hash_map = defaultdict(list)
+        result = []
 
         for s in strs:
-            #in the following we store counts of each character from a to z
-            count = [0] * 26
+            sorted_s = tuple(sorted(s))
+            hash_map[sorted_s].append(s)
 
-            for char in s:
-                #substract ASCII of c with "a" and store count of character in array
-                count[ord(char) - ord("a")] += 1
-                # Example ASCII(a) = 80, 80-80=0
-                #         ASCII(b) = 81, 81-80=1
-                #         ASCII(z) = 105, 105-80=25
+        for value in hash_map.values():
+            result.append(value)
 
-            result[tuple(count)].append(s)
-
-        return result.values()
+        return result
     
-
 strs = ["act","pots","tops","cat","stop","hat"]
-
 solution = Solution()
 print(solution.group_anagrams(strs))
