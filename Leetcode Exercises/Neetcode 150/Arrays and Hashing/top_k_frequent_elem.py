8b855444dd02aae6547cf484a736b29a03207401
ghelaseG <ghelase.georgian@gmail.com>
2024-11-28 21:46:15 +0000

    Practice Questions, Neetcode 150, Arrays and Hashing, exercise 5, top k frequent elements, code using heap

--- Leetcode Exercises/Neetcode 150/Arrays and Hashing/top_k_frequent_elem.py
+++ Leetcode Exercises/Neetcode 150/Arrays and Hashing/top_k_frequent_elem.py
@@ -5,27 +5,34 @@ The test cases are generated such that the answer is always unique.
 
 You may return the output in any order.
 """
-from typing import List
+# from typing import List
+
+# class Solution:
+#     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+
+#         count = {}
+#         for num in nums:
+#             count[num] = 1 + count.get(num, 0)
+
+#         arr = []
+#         for i, j in count.items():
+#             arr.append([j, i])
+#         arr.sort()
+
+#         res = []
+#         while len(res) < k:
+#             res.append(arr.pop()[1])
+#         return res
+    
+# nums = [1,2,2,3,3,3]
+# k = 2
+
+# solution = Solution()
+# print(solution.topKFrequent(nums, k))
+
+#part 2 using heap
 
 class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+    def top_k_frequent(self, nums: List[int], k: int) -> List[int]:
+        
 
-        count = {}
-        for num in nums:
-            count[num] = 1 + count.get(num, 0)
-
-        arr = []
-        for i, j in count.items():
-            arr.append([j, i])
-        arr.sort()
-
-        res = []
-        while len(res) < k:
-            res.append(arr.pop()[1])
-        return res
-    
-nums = [1,2,2,3,3,3]
-k = 2
-
-solution = Solution()
-print(solution.topKFrequent(nums, k))
