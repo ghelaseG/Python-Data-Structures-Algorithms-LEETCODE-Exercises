84d733319ab339af80429b549e5904ec24f7f47d
ghelaseG <ghelase.georgian@gmail.com>
2024-12-01 23:17:57 +0000

    Practice Questions, Neetcode 150, Arrays and Hashing, exercise 6, encode and decode, optimal code with example

--- Leetcode Exercises/Neetcode 150/Arrays and Hashing/encode_decode_strings.py
+++ Leetcode Exercises/Neetcode 150/Arrays and Hashing/encode_decode_strings.py
@@ -3,44 +3,82 @@ Design an algorithm to encode a list of strings to a single string. The encoded 
 
 Please implement encode and decode
 """
+# from typing import List
+
+# class Solution:
+
+#     def encode(self, strs: List[str]) -> str:
+#         if not strs:
+#             return ""
+        
+#         sizes, res = [], ""
+#         for s in strs:
+#             sizes.append(len(s))
+#         for sz in sizes:
+#             res += str(sz)
+#             res += ','
+#         res += '#'
+#         for s in strs:
+#             res += s
+#         return res
+
+
+#     def decode(self, s: str) -> List[str]:
+
+#         if not s:
+#             return []
+        
+#         sizes, res, i = [], [], 0
+#         while s[i] != '#':
+#             cur = ""
+#             while s[i] != ',':
+#                 cur += s[i]
+#                 i += 1
+#             sizes.append(int(cur))
+#             i += 1
+#         i += 1
+#         for sz in sizes:
+#             res.append(s[i:i + sz])
+#             i += sz
+#         return res
+
+# solution = Solution()
+
+# strs = ["gg","does","love","you"]
+# s = solution.encode(strs)
+
+# print(s)
+
+# r = solution.decode(s)
+
+# print(r)
+
+
 from typing import List
 
 class Solution:
 
     def encode(self, strs: List[str]) -> str:
-        if not strs:
-            return ""
-        
-        sizes, res = [], ""
+        result = ""
         for s in strs:
-            sizes.append(len(s))
-        for sz in sizes:
-            res += str(sz)
-            res += ','
-        res += '#'
-        for s in strs:
-            res += s
-        return res
-
-
+            result += str(len(s)) + "#" + s
+        return result
+    
     def decode(self, s: str) -> List[str]:
-
-        if not s:
-            return []
+        result = []
+        i = 0
         
-        sizes, res, i = [], [], 0
-        while s[i] != '#':
-            cur = ""
-            while s[i] != ',':
-                cur += s[i]
-                i += 1
-            sizes.append(int(cur))
-            i += 1
-        i += 1
-        for sz in sizes:
-            res.append(s[i:i + sz])
-            i += sz
-        return res
+        while i < len(s):
+            j = i
+            while s[j] != '#':
+                j += 1
+            length = int(s[i:j])
+            i = j + 1
+            j = i + length
+            result.append(s[i:j])
+            i = j
+        
+        return result
 
 solution = Solution()
 
--- Leetcode Exercises/Neetcode 150/Arrays and Hashing/tempCodeRunnerFile.py
+++ Leetcode Exercises/Neetcode 150/Arrays and Hashing/tempCodeRunnerFile.py
@@ -1,46 +1,28 @@
-"""
-Design an algorithm to encode a list of strings to a single string. The encoded string is then decoded back to the original list of strings.
-
-Please implement encode and decode
-"""
 from typing import List
 
 class Solution:
 
     def encode(self, strs: List[str]) -> str:
-        if not strs:
-            return ""
-        
-        sizes, res = [], ""
+        result = ""
         for s in strs:
-            sizes.append(len(s))
-        for sz in sizes:
-            res += str(sz)
-            res += ','
-        res += '#'
-        for s in strs:
-            res += s
-        return res
-
-
+            result += str(len(s)) + "#" + s
+        return result
+    
     def decode(self, s: str) -> List[str]:
-
-        if not s:
-            return []
+        result = []
+        i = 0
         
-        sizes, res, i = [], [], 0
-        while s[i] != '#':
-            cur = ""
-            while s[i] != ',':
-                cur += s[i]
-                i += 1
-            sizes.append(int(cur))
-            i += 1
-        i += 1
-        for sz in sizes:
-            res.append(s[i:i + sz])
-            i += sz
-        return res
+        while i < len(s):
+            j = i
+            while s[j] != '#':
+                j += 1
+            length = int(s[i:j])
+            i = j + 1
+            j = i + length
+            result.append(s[i:j])
+            i = j
+        
+        return result
 
 solution = Solution()
 
@@ -48,3 +30,7 @@ strs = ["gg","does","love","you"]
 s = solution.encode(strs)
 
 print(s)
+
+r = solution.decode(s)
+
+print(r)
