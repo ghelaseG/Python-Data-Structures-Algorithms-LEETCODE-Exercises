001adbec8fd59c209e74677a486592a324de56d3
ghelaseG <ghelase.georgian@gmail.com>
2024-12-03 18:59:52 +0000

    Practice Questions, Neetcode 150, Arrays and Hashing, exercise 7, products of array except self, 2nd example using division, mock

--- Leetcode Exercises/Neetcode 150/Arrays and Hashing/products_of_array_except_self.py
+++ Leetcode Exercises/Neetcode 150/Arrays and Hashing/products_of_array_except_self.py
@@ -10,26 +10,33 @@ n
 )
 O(n) time without using the division operation?
 """
+# from typing import List
+
+# class Solution:
+#     def product_except_self(self, nums: List[int]) -> List[int]:
+#         n = len(nums)
+#         res = [0] * n
+
+#         for i in range(n):
+#             prod = 1
+#             for j in range(n):
+#                 if i == j:
+#                     continue
+#                 prod *= nums[j]
+            
+#             res[i] = prod
+#         return res
+
+# solution = Solution()
+
+# nums = [1,2,4,6]
+
+# print(solution.product_except_self(nums))
+
+# part 2 using division
+
 from typing import List
 
 class Solution:
     def product_except_self(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        res = [0] * n
-
-        for i in range(n):
-            prod = 1
-            for j in range(n):
-                if i == j:
-                    continue
-                prod *= nums[j]
-            
-            res[i] = prod
-        return res
-
-solution = Solution()
-
-nums = [1,2,4,6]
-
-print(solution.product_except_self(nums))
-
+ 
