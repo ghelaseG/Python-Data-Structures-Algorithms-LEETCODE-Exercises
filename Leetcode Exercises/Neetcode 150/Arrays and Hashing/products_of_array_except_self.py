29ddaff33df5345cae4bf076855cdeac0ea63cee
ghelaseG <ghelase.georgian@gmail.com>
2024-12-03 23:04:44 +0000

    Practice Questions, Neetcode 150, Arrays and Hashing, exercise 7, products of array except self, 2nd example using division

--- Leetcode Exercises/Neetcode 150/Arrays and Hashing/products_of_array_except_self.py
+++ Leetcode Exercises/Neetcode 150/Arrays and Hashing/products_of_array_except_self.py
@@ -39,4 +39,25 @@ from typing import List
 
 class Solution:
     def product_except_self(self, nums: List[int]) -> List[int]:
- 
+        prod, zero_cnt = 1, 0
+        for num in nums:
+            if num:
+                prod *= num
+            else:
+                zero_cnt += 1
+        if zero_cnt > 1: 
+            return [0] * len(nums)
+        
+        res = [0] * len(nums)
+        for i, c in enumerate(nums):
+            if zero_cnt:
+                res[i] = 0 if c else prod
+            else:
+                res[i] = prod // c
+        return res
+    
+solution = Solution()
+
+nums = [1,2,4,6]
+
+print(solution.product_except_self(nums))
--- Leetcode Exercises/Neetcode 150/Arrays and Hashing/tempCodeRunnerFile.py
+++ Leetcode Exercises/Neetcode 150/Arrays and Hashing/tempCodeRunnerFile.py
@@ -2,22 +2,25 @@ from typing import List
 
 class Solution:
     def product_except_self(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        res = [0] * n
-
-        for i in range(n):
-            prod = 1
-            for j in range(n):
-                if i == j:
-                    continue
-                prod *= nums[j]
-            
-            res[i] = prod
+        prod, zero_cnt = 1, 0
+        for num in nums:
+            if num:
+                prod *= num
+            else:
+                zero_cnt += 1
+        if zero_cnt > 1: 
+            return [0] * len(nums)
+        
+        res = [0] * len(nums)
+        for i, c in enumerate(nums):
+            if zero_cnt:
+                res[i] = 0 if c else prod
+            else:
+                res[i] = prod // c
         return res
-
+    
 solution = Solution()
 
 nums = [1,2,4,6]
 
 print(solution.product_except_self(nums))
-
