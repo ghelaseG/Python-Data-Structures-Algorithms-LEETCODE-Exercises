d3d80be13a684f7bb7fd2e060819166229d76568
ghelaseG <ghelase.georgian@gmail.com>
2024-12-04 23:44:50 +0000

    Practice Questions, Neetcode 150, Arrays and Hashing, exercise 7, products of array except self, 3rd example, prefix and suffix

--- Leetcode Exercises/Neetcode 150/Arrays and Hashing/products_of_array_except_self.py
+++ Leetcode Exercises/Neetcode 150/Arrays and Hashing/products_of_array_except_self.py
@@ -35,27 +35,49 @@ O(n) time without using the division operation?
 
 # part 2 using division
 
+# from typing import List
+
+# class Solution:
+#     def product_except_self(self, nums: List[int]) -> List[int]:
+#         prod, zero_cnt = 1, 0
+#         for num in nums:
+#             if num:
+#                 prod *= num
+#             else:
+#                 zero_cnt += 1
+#         if zero_cnt > 1: 
+#             return [0] * len(nums)
+        
+#         res = [0] * len(nums)
+#         for i, c in enumerate(nums):
+#             if zero_cnt:
+#                 res[i] = 0 if c else prod
+#             else:
+#                 res[i] = prod // c
+#         return res
+    
+# solution = Solution()
+
+# nums = [1,2,4,6]
+
+# print(solution.product_except_self(nums))
+
 from typing import List
 
 class Solution:
     def product_except_self(self, nums: List[int]) -> List[int]:
-        prod, zero_cnt = 1, 0
-        for num in nums:
-            if num:
-                prod *= num
-            else:
-                zero_cnt += 1
-        if zero_cnt > 1: 
-            return [0] * len(nums)
-        
-        res = [0] * len(nums)
-        for i, c in enumerate(nums):
-            if zero_cnt:
-                res[i] = 0 if c else prod
-            else:
-                res[i] = prod // c
-        return res
-    
+        result = [1] * (len(nums))
+
+        prefix = 1
+        for i in range(len(nums)):
+            result[i] = prefix
+            prefix *= nums[i]
+        postfix = 1
+        for i in range(len(nums) -1, -1, -1):
+            result[i] *= postfix
+            postfix *= nums[i]
+        return result
+
 solution = Solution()
 
 nums = [1,2,4,6]
--- Leetcode Exercises/Neetcode 150/Arrays and Hashing/tempCodeRunnerFile.py
+++ Leetcode Exercises/Neetcode 150/Arrays and Hashing/tempCodeRunnerFile.py
@@ -1,24 +1,20 @@
+
 from typing import List
 
 class Solution:
     def product_except_self(self, nums: List[int]) -> List[int]:
-        prod, zero_cnt = 1, 0
-        for num in nums:
-            if num:
-                prod *= num
-            else:
-                zero_cnt += 1
-        if zero_cnt > 1: 
-            return [0] * len(nums)
-        
-        res = [0] * len(nums)
-        for i, c in enumerate(nums):
-            if zero_cnt:
-                res[i] = 0 if c else prod
-            else:
-                res[i] = prod // c
-        return res
-    
+        result = [1] * (len(nums))
+
+        prefix = 1
+        for i in range(len(nums)):
+            result[i] = prefix
+            prefix *= nums[i]
+        postfix = 1
+        for i in range(len(nums) -1, -1, -1):
+            result[i] *= postfix
+            postfix *= nums[i]
+        return result
+
 solution = Solution()
 
 nums = [1,2,4,6]
