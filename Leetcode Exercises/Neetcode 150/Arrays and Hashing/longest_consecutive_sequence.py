772451c6023a91de3682b76a51763581ef96fe4f
ghelaseG <ghelase.georgian@gmail.com>
2024-12-16 22:15:15 +0000

    Practice Questions, Neetcode 150, Arrays and Hashing, exercise 9, longest consecutive sequence, part 1 using brute force

--- Leetcode Exercises/Neetcode 150/Arrays and Hashing/longest_consecutive_sequence.py
+++ Leetcode Exercises/Neetcode 150/Arrays and Hashing/longest_consecutive_sequence.py
@@ -5,4 +5,21 @@ A consecutive sequence is a sequence of elements in which each element is exactl
 
 You must write an algorithm that runs in O(n) time.
 """
+from typing import List
 
+class Solution:
+    def longest_consecutive(self, nums: List[int]) -> int:
+        result = 0
+        store = set(nums)
+
+        for num in nums:
+            streak, curr = 0, num
+            while curr in store:
+                streak += 1
+                curr += 1
+            result = max(result, streak)
+        return result
+    
+solution = Solution()
+nums = [2,20,4,10,3,4,5]
+print(solution.longest_consecutive(nums))
--- Leetcode Exercises/Neetcode 150/Arrays and Hashing/tempCodeRunnerFile.py
+++ Leetcode Exercises/Neetcode 150/Arrays and Hashing/tempCodeRunnerFile.py
@@ -1,108 +1,26 @@
 """
-You are given a a 9 x 9 Sudoku board board. A Sudoku board is valid if the following rules are followed:
+Given an array of integers nums, return the length of the longest consecutive sequence of elements that can be formed.
 
-Each row must contain the digits 1-9 without duplicates.
-Each column must contain the digits 1-9 without duplicates.
-Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without duplicates.
-Return true if the Sudoku board is valid, otherwise return false
+A consecutive sequence is a sequence of elements in which each element is exactly 1 greater than the previous element. The elements do not have to be consecutive in the original array.
 
-Note: A board does not need to be full or be solvable to be valid.
+You must write an algorithm that runs in O(n) time.
 """
-
-from collections import defaultdict
 from typing import List
 
 class Solution:
-    def is_valid_sudoku(self, board: List[List[str]]) -> bool:
+    def longest_consecutive(self, nums: List[int]) -> int:
+        result = 0
+        store = set(nums)
 
-#part 1 using brute force
-        # for row in range(9):
-        #     seen = set()
-        #     for i in range(9):
-        #         if board[row][i] == ".":
-        #             continue
-        #         if board[row][i] in seen:
-        #             return False
-        #         seen.add(board[row][i])
-
-        # for col in range(9):
-        #     seen = set()
-        #     for i in range(9):
-        #         if board[i][col] == ".":
-        #             continue
-        #         if board[i][col] in seen:
-        #             return False
-        #         seen.add(board[i][col])
-
-        # for square in range(9):
-        #     seen = set()
-        #     for i in range(3):
-        #         for j in range(3):
-        #             row = (square//3) * 3 + i
-        #             col = (square % 3) * 3 + j
-        #             if board[row][col] == ".":
-        #                 continue
-        #             if board[row][col] in seen:
-        #                 return False
-        #             seen.add(board[row][col])
-        # return True
-
-# #part 2 Bitmask
-        
-#         rows = [0] * 9
-#         cols = [0] * 9
-#         squares = [0] * 9
-
-#         for r in range(9):
-#             for c in range(9):
-#                 if board[r][c] == ".":
-#                     continue
-                    
-#                 val = int(board[r][c]) - 1
-#                 if (1 << val) & rows[r]:
-#                     return False
-#                 if (1 << val) & cols[c]:
-#                     return False
-#                 if (1 << val) & squares[(r // 3) * 3 + (c // 3)]:
-#                     return False
-                
-#                 rows[r] |= (1 << val)
-#                 cols[c] |= (1 << val)
-#                 squares[(r // 3) * 3 + (c // 3)] |= (1 << val)
-            
-#             return True
-
-# part 3 using Hash Set
-        
-        cols = defaultdict(set)
-        rows = defaultdict(set)
-        squares = defaultdict(set)
-
-        for r in range(9):
-            for c in range(9):
-                if board[r][c] == ".":
-                    continue
-                if ( board[r][c] in rows[r]
-                    or board[r][c] in cols[c]
-                    or board[r][c] in squares[(r // 3, c // 3)]):
-                    return False
-                
-                cols[c].add(board[r][c])
-                rows[r].add(board[r][c])
-                squares[(r // 3, c // 3)].add(board[r][c])
-        
-        return True
+        for num in nums:
+            streak, curr = 0, num
+            while curr in store:
+                streak += 1
+                curr += 1
+            result = max(result, streak)
+        print(streak)
+        return result
     
 solution = Solution()
-
-board = [["1","2",".",".","3",".",".",".","."],
- ["4",".",".","5",".",".",".",".","."],
- [".","9","8",".",".",".",".",".","3"],
- ["5",".",".",".","6",".",".",".","4"],
- [".",".",".","8",".","3",".",".","5"],
- ["7",".",".",".","2",".",".",".","6"],
- [".",".",".",".",".",".","2",".","."],
- [".",".",".","4","1","9",".",".","8"],
- [".",".",".",".","8",".",".","7","9"]]
-
-print(solution.is_valid_sudoku(board))
+nums = [2,20,4,10,3,4,5]
+print(solution.longest_consecutive(nums))
