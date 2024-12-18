96c9361283f091073f242a7b162815d43c147bf7
ghelaseG <ghelase.georgian@gmail.com>
2024-12-18 23:45:01 +0000

    Practice Questions, Neetcode 150, Arrays and Hashing, exercise 9, longest consecutive sequence, part 2 using sorting

--- Leetcode Exercises/Neetcode 150/Arrays and Hashing/tempCodeRunnerFile.py
+++ Leetcode Exercises/Neetcode 150/Arrays and Hashing/tempCodeRunnerFile.py
@@ -1,26 +1,37 @@
-"""
-Given an array of integers nums, return the length of the longest consecutive sequence of elements that can be formed.
-
-A consecutive sequence is a sequence of elements in which each element is exactly 1 greater than the previous element. The elements do not have to be consecutive in the original array.
-
-You must write an algorithm that runs in O(n) time.
-"""
 from typing import List
 
 class Solution:
     def longest_consecutive(self, nums: List[int]) -> int:
-        result = 0
-        store = set(nums)
+        # #part 1
+        # result = 0
+        # store = set(nums)
 
-        for num in nums:
-            streak, curr = 0, num
-            while curr in store:
-                streak += 1
-                curr += 1
+        # for num in nums:
+        #     streak, curr = 0, num
+        #     while curr in store:
+        #         streak += 1
+        #         curr += 1
+        #     result = max(result, streak)
+        # return result
+        #part 2
+        if not nums:
+            return 0
+        result = 0
+        nums.sort()
+
+        current, streak = nums[0], 0
+        i = 0
+        while i < len(nums):
+            if current != nums[i]:
+                current = nums[i]
+                streak = 0
+            while i < len(nums) and nums[i] == current:
+                i += 1
+            streak += 1
+            current += 1
             result = max(result, streak)
-        print(streak)
         return result
-    
+
 solution = Solution()
 nums = [2,20,4,10,3,4,5]
 print(solution.longest_consecutive(nums))
