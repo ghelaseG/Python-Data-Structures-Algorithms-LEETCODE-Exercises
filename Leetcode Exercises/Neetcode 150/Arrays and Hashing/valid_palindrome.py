8f012312b074efa6fffde95d6e824ff5c44f0607
ghelaseG <ghelase.georgian@gmail.com>
2024-12-26 23:51:16 +0000

    Practice Questions, Neetcode 150, Arrays and Hashing, exercise 10, valid palindrome, part 1 using reverse string

--- Leetcode Exercises/Neetcode 150/Arrays and Hashing/valid_palindrome.py
+++ Leetcode Exercises/Neetcode 150/Arrays and Hashing/valid_palindrome.py
@@ -1,3 +1,8 @@
+"""
+Given a string s, return true if it is a palindrome, otherwise return false.
+
+A palindrome is a string that reads the same forward and backward. It is also case-insensitive and ignores all non-alphanumeric characters.
+"""
 
 class Solution:
     def isPlalindrome(self, s: str) -> bool:
