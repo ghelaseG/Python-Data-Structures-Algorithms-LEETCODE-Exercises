90801d2489355ff0ac09023a680af652c35affe5
ghelaseG <ghelase.georgian@gmail.com>
2024-12-28 22:06:15 +0000

    Practice Questions, Neetcode 150, Arrays and Hashing, exercise 11, part 1 using brute force

--- Leetcode Exercises/Neetcode 150/Arrays and Hashing/tempCodeRunnerFile.py
+++ Leetcode Exercises/Neetcode 150/Arrays and Hashing/tempCodeRunnerFile.py
@@ -1,36 +1,15 @@
-"""
-Given a string s, return true if it is a palindrome, otherwise return false.
-
-A palindrome is a string that reads the same forward and backward. It is also case-insensitive and ignores all non-alphanumeric characters.
-"""
+from typing import List
 
 class Solution:
-    def isPlalindrome(self, s: str) -> bool:
-        #part 1
-        # new_string = ''
-        # for c in s:
-        #     if c.isalnum():
-        #         new_string += c.lower()
-        # return new_string == new_string[::-1]
-
-        l, r = 0, len(s) - 1
-
-        while l < r:
-            while l < r and not self.alpha_num(s[l]):
-                l += 1
-            while r > l and not self.alpha_num(s[r]):
-                r -= 1
-            if s[l].lower() != s[r].lower():
-                return False
-            l, r = l + 1, r - 1
-        return True
+    def two_sum_II(self, numbers: List[int], target: int) -> List[int]:
+        for i in range(len(numbers)):
+            for j in range(i + 1, len(numbers)):
+                if numbers[i] + numbers[j] == target:
+                    return [i + 1, j + 1]
+        return []
     
-    def alpha_num(self, c):
-        return (ord('A') <= ord(c) <= ord('Z') or 
-                ord('a') <= ord(c) <= ord('z') or 
-                ord('0') <= ord(c) <= ord('9'))
+numbers = [1,2,3,4]
+target = 3
 
-
-s = "Was it a car or a cat I saw?"
 solution = Solution()
-print(solution.isPlalindrome(s))
+print(solution.two_sum_II(numbers, target))
--- Leetcode Exercises/Neetcode 150/Arrays and Hashing/two_integer_sum_II.py
+++ Leetcode Exercises/Neetcode 150/Arrays and Hashing/two_integer_sum_II.py
@@ -12,4 +12,18 @@ O
 )
 O(1) additional space.
 """
+from typing import List
 
+class Solution:
+    def two_sum_II(self, numbers: List[int], target: int) -> List[int]:
+        for i in range(len(numbers)):
+            for j in range(i + 1, len(numbers)):
+                if numbers[i] + numbers[j] == target:
+                    return [i + 1, j + 1]
+        return []
+    
+numbers = [1,2,3,4]
+target = 3
+
+solution = Solution()
+print(solution.two_sum_II(numbers, target))
