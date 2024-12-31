784ba3367c1fe4a91bc1f3a2842e17e13c797635
ghelaseG <ghelase.georgian@gmail.com>
2024-12-31 23:46:54 +0000

    Practice Questions, Neetcode 150, Arrays and Hashing, exercise 11, part 3 using hash map

--- Leetcode Exercises/Neetcode 150/Arrays and Hashing/tempCodeRunnerFile.py
+++ Leetcode Exercises/Neetcode 150/Arrays and Hashing/tempCodeRunnerFile.py
@@ -10,20 +10,30 @@ class Solution:
         # return []
 
         #part 2
-        for i in range(len(numbers)):
-            l, r = i + 1, len(numbers) - 1
-            tempr = target - numbers[i]
-            while l <= r:
-                mid = l + (r - l) // 2
-                if numbers[mid] == tempr:
-                    return [i + 1, mid + 1]
-                elif numbers[mid] < tempr:
-                    l = mid + 1
-                else:
-                    r = mid - 1
+        # for i in range(len(numbers)):
+        #     l, r = i + 1, len(numbers) - 1
+        #     tempr = target - numbers[i]
+        #     while l <= r:
+        #         mid = l + (r - l) // 2
+        #         if numbers[mid] == tempr:
+        #             return [i + 1, mid + 1]
+        #         elif numbers[mid] < tempr:
+        #             l = mid + 1
+        #         else:
+        #             r = mid - 1
 
+        # return []
+
+        #part 3
+        from collections import defaultdict
+        mp = defaultdict(int)
+        for i in range(len(numbers)):
+            tmp = target - numbers[i]
+            if mp[tmp]:
+                return [mp[tmp], i + 1]
+            mp[numbers[i]] = i + 1
         return []
-     
+    
 numbers = [1,2,3,4]
 target = 3
 
