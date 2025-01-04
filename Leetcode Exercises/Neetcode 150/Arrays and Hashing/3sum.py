472262bbb1ad48e9c4d1e289a6605ffd983ead5d
ghelaseG <ghelase.georgian@gmail.com>
2025-01-04 23:52:34 +0000

    Practice Questions, Neetcode 150, Arrays and Hashing, exercise 12, part 3 using two pointers

--- Leetcode Exercises/Neetcode 150/Arrays and Hashing/3sum.py
+++ Leetcode Exercises/Neetcode 150/Arrays and Hashing/3sum.py
@@ -20,28 +20,54 @@ class Solution:
         # return [list(i) for i in result]
 
         #part 2
-        nums.sort()
-        count = defaultdict(int)
-        for num in nums:
-            count[num] += 1
+        # nums.sort()
+        # count = defaultdict(int)
+        # for num in nums:
+        #     count[num] += 1
         
+        # result = []
+        # for i in range(len(nums)):
+        #     count[nums[i]] -= 1
+        #     if i and nums[i] == nums[i - 1]:
+        #         continue
+
+        #     for j in range(i + 1, len(nums)):
+        #         count[nums[j]] -= 1
+        #         if j -1 > i and nums[j] == nums[j - 1]:
+        #             continue
+        #         target = -(nums[i] + nums[j])
+        #         if count[target] > 0:
+        #             result.append([nums[i], nums[j], target])
+            
+        #     for j in range(i + 1, len(nums)):
+        #         count[nums[j]] += 1
+        
+        # return result
+
+        #part 3
         result = []
-        for i in range(len(nums)):
-            count[nums[i]] -= 1
-            if i and nums[i] == nums[i - 1]:
+        nums.sort()
+
+        for i, a in enumerate(nums):
+            if a > 0:
+                break
+
+            if i > 0 and a == nums[i - 1]:
                 continue
 
-            for j in range(i + 1, len(nums)):
-                count[nums[j]] -= 1
-                if j -1 > i and nums[j] == nums[j - 1]:
-                    continue
-                target = -(nums[i] + nums[j])
-                if count[target] > 0:
-                    result.append([nums[i], nums[j], target])
-            
-            for j in range(i + 1, len(nums)):
-                count[nums[j]] += 1
-        
+            l, r = i + 1, len(nums) - 1
+            while l < r:
+                three_sum = a + nums[l] + nums[r]
+                if three_sum > 0:
+                    r -= 1
+                elif three_sum < 0:
+                    l += 1
+                else:
+                    result.append([a, nums[l], nums[r]])
+                    l += 1
+                    r -= 1
+                    while nums[l] == nums[l - 1] and l < r:
+                        l += 1
         return result
 
 nums = [0,1,1]
