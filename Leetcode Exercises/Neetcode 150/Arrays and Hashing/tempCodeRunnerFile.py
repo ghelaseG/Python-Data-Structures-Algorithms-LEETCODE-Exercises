4988d7c266ea2c1900b78ac24507d8be5124c877
ghelaseG <ghelase.georgian@gmail.com>
2025-01-08 23:47:23 +0000

    Practice Questions, Neetcode 150, Arrays and Hashing, exercise 14, part 2 using prefix and suffix arrays

--- Leetcode Exercises/Neetcode 150/Arrays and Hashing/tempCodeRunnerFile.py
+++ Leetcode Exercises/Neetcode 150/Arrays and Hashing/tempCodeRunnerFile.py
@@ -3,22 +3,44 @@ from typing import List
 class Solution:
     def trap(self, height: List[int]) -> int:
 
-        #part 1
+        # #part 1
 
-        if not height:
-            return 0
+        # if not height:
+        #     return 0
+        # n = len(height)
+        # result = 0
+
+        # for i in range(n):
+        #     leftMax = rightMax = height[i]
+
+        #     for j in range(i):
+        #         leftMax = max(leftMax, height[j])
+        #     for j in range(i + 1, n):
+        #         rightMax = max(rightMax, height[j])
+
+        #     result += min(leftMax, rightMax) - height[i]
+        # return result
+
+        #part 2
         n = len(height)
+        if n == 0:
+            return 0
+        
+        leftMax = [0] * n
+        rightMax = [0] * n
+
+        leftMax[0] = height[0]
+        for i in range(1, n):
+            leftMax[i] = max(leftMax[i - 1], height[i])
+        
+        rightMax[n -  1] = height[n - 1]
+        for i in range(n - 2, -1, -1):
+            rightMax[i] = max(rightMax[i + 1], height[i])
+        
         result = 0
-
         for i in range(n):
-            leftMax = rightMax = height[i]
-
-            for j in range(i):
-                leftMax = max(leftMax, height[j])
-            for j in range(i + 1, n):
-                rightMax = max(rightMax, height[j])
-
-            result += min(leftMax, rightMax) - height[i]
+            result += min(leftMax[i], rightMax[i])- height[i]
+        
         return result
 
 height = [0,2,0,3,1,0,1,3,2,1]    
