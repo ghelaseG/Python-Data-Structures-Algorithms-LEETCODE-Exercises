b401ba23f019fe7fc9ac17e1818560621f83cf4d
ghelaseG <ghelase.georgian@gmail.com>
2025-01-08 23:56:47 +0000

    Practice Questions, Neetcode 150, Arrays and Hashing, exercise 14, part 4 using two pointers

--- Leetcode Exercises/Neetcode 150/Arrays and Hashing/tempCodeRunnerFile.py
+++ Leetcode Exercises/Neetcode 150/Arrays and Hashing/tempCodeRunnerFile.py
@@ -44,21 +44,39 @@ class Solution:
         # return result
 
         #part 3
+        # if not height:
+        #     return 0
+        # stack = []
+        # result = 0
+
+        # for i in range(len(height)):
+        #     while stack and height[i] >= height[stack[-1]]:
+        #         mid = height[stack.pop()]
+        #         if stack:
+        #             right = height[i]
+        #             left = height[stack[-1]]
+        #             h = min(right, left) - mid
+        #             w = i - stack[-1] - 1
+        #             result += h * w
+        #     stack.append(i)
+        # return result
+
+        #part 4
         if not height:
             return 0
-        stack = []
+        
+        l, r = 0, len(height) - 1
+        leftMax, rightMax = height[l], height[r]
         result = 0
-
-        for i in range(len(height)):
-            while stack and height[i] >= height[stack[-1]]:
-                mid = height[stack.pop()]
-                if stack:
-                    right = height[i]
-                    left = height[stack[-1]]
-                    h = min(right, left) - mid
-                    w = i - stack[-1] - 1
-                    result += h * w
-            stack.append(i)
+        while l < r:
+            if leftMax < rightMax:
+                l += 1
+                leftMax = max(leftMax, height[l])
+                result += leftMax - height[l]
+            else:
+                r -= 1
+                rightMax = max(rightMax, height[r])
+                result += rightMax - height[r]
         return result
 
 height = [0,2,0,3,1,0,1,3,2,1]    
