8dbb11302c608c8ba8381b8c9096c5b1c4b12665
ghelaseG <ghelase.georgian@gmail.com>
2025-01-08 23:43:01 +0000

    Practice Questions, Neetcode 150, Arrays and Hashing, exercise 14, part 1 using brute force

--- Leetcode Exercises/Neetcode 150/Arrays and Hashing/trapping_rain_water.py
+++ Leetcode Exercises/Neetcode 150/Arrays and Hashing/trapping_rain_water.py
@@ -1,3 +1,8 @@
+"""
+You are given an array non-negative integers height which represent an elevation map. Each value height[i] represents the height of a bar, which has a width of 1.
+
+Return the maximum area of water that can be trapped between the bars.
+"""
 
 from typing import List
 class Solution:
