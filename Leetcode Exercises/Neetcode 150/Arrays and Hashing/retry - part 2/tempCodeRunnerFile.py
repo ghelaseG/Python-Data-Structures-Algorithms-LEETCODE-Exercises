55db8fcdba5cd19daea7b423eca933f54604b349
ghelaseG <ghelase.georgian@gmail.com>
2025-01-09 22:10:27 +0000

    Practice Questions, Neetcode 150, Arrays and Hashing, exercise 1, retrial without watching, part 2 using sorting

--- Leetcode Exercises/Neetcode 150/Arrays and Hashing/retry - part 2/tempCodeRunnerFile.py
+++ Leetcode Exercises/Neetcode 150/Arrays and Hashing/retry - part 2/tempCodeRunnerFile.py
@@ -3,13 +3,19 @@ from typing import List
 class Solution:
     def has_duplicate(self, nums: List[int]) -> bool:
         #part 1 using brute force
-        for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                if nums[i] == nums[j]:
-                    return True
+        # for i in range(len(nums)):
+        #     for j in range(i+1,len(nums)):
+        #         if nums[i] == nums[j]:
+        #             return True
         
+        # return False
+
+        #part 2 using sorting
+        for i in range(len(nums)):
+            if nums[i+1] == nums[i]:
+                return True
         return False
-    
+
 nums = [1, 2, 3, 3]
 solution = Solution()
 print(solution.has_duplicate(nums))
