56d20b2799ffad55745515d3add53e131fefe68a
ghelaseG <ghelase.georgian@gmail.com>
2025-01-16 00:41:11 +0000

    Practice Questions, Neetcode 150, Arrays and Hashing, exercise 4, retrial part 1

--- Leetcode Exercises/Neetcode 150/Arrays and Hashing/retry - part 2/group_anagrams.py
+++ Leetcode Exercises/Neetcode 150/Arrays and Hashing/retry - part 2/group_anagrams.py
@@ -3,4 +3,18 @@ Given an array of strings strs, group all anagrams together into sublists. You m
 
 An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
 """
+from collections import defaultdict
+from typing import List
 
+class Solution:
+
+    def group_anagram(self, strs: List[str]) -> List[List[str]]:
+        result = defaultdict(list)
+        for s in strs:
+            sorted_str = ' '.join(sorted(s))
+            result[sorted_str].append(s)
+        return list(result.values())
+    
+strs = ["act","pots","tops","cat","stop","hat"]
+solution = Solution()
+print(solution.group_anagram(strs))
--- Leetcode Exercises/Neetcode 150/Arrays and Hashing/retry - part 2/tempCodeRunnerFile.py
+++ Leetcode Exercises/Neetcode 150/Arrays and Hashing/retry - part 2/tempCodeRunnerFile.py
@@ -1,41 +1,20 @@
 """
-Given an array of integers nums and an integer target, return the indices i and j such that nums[i] + nums[j] == target and i != j.
+Given an array of strings strs, group all anagrams together into sublists. You may return the output in any order.
 
-You may assume that every input has exactly one pair of indices i and j that satisfy the condition.
-
-Return the answer with the smaller index first.
+An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
 """
+from collections import defaultdict
 from typing import List
 
 class Solution:
-    def two_sum(self, nums: List[int], target: int) -> List[int]:
-        #part 1
-        # for i in range(len(nums)):
-        #     for j in range(i + 1, len(nums)):
-        #         if nums[i] + nums[j] == target:
-        #             return [i, j]
-        # return []
 
-        # #part 2
-        # prev_map = {}
-
-        # for i, n in enumerate(nums):
-        #     differ = target - n
-        #     if differ in prev_map:
-        #         return [prev_map[differ], i]
-        #     prev_map[n] = i
-
-        #part 3
-        indices = {}
-
-        for i, n in enumerate(nums):
-            indices[n] = i
-
-        for i, n in enumerate(nums):
-            differ = target - n
-            if differ in indices and indices[differ] != i:
-                return [i, indices[differ]] 
-nums = [3,4,5,6]
-target = 7
+    def group_anagram(self, strs: List[str]) -> List[List[str]]:
+        result = defaultdict(list)
+        for s in strs:
+            sorted_str = ' '.join(sorted(s))
+            result[sorted_str].append(s)
+        return list(result.values())
+    
+strs = ["act","pots","tops","cat","stop","hat"]
 solution = Solution()
-print(solution.two_sum(nums, target))
+print(solution.group_anagram(strs))
