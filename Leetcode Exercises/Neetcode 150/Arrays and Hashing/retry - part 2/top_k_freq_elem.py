b7c96a7db2817ce0ed42226d3ddfa36ed63c964d
ghelaseG <ghelase.georgian@gmail.com>
2025-01-18 00:36:07 +0000

    Practice Questions, Neetcode 150, Arrays and Hashing, exercise 5, retrial part 1

--- Leetcode Exercises/Neetcode 150/Arrays and Hashing/retry - part 2/top_k_freq_elem.py
+++ Leetcode Exercises/Neetcode 150/Arrays and Hashing/retry - part 2/top_k_freq_elem.py
@@ -6,3 +6,18 @@ The test cases are generated such that the answer is always unique.
 You may return the output in any order.
 """
 
+class Solution:
+    def top_k_element(self, nums: List[int], k: int) -> List[int]:
+        count = {}
+        for num in nums:
+            count[num] = 1 + count.get(num, 0)
+        
+        arry = []
+        for num, cnt in count.items():
+            arry.append([cnt, num])
+        arry.sort()
+
+        result = []
+        while len(result) < k:
+            result.append(arry.pop()[1])
+        return result
