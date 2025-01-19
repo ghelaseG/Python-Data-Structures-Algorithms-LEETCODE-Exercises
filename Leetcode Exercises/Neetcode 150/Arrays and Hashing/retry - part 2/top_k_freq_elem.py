5907d5c408a80d7a2c8596217204afc51ef69c36
ghelaseG <ghelase.georgian@gmail.com>
2025-01-19 23:38:28 +0000

    Practice Questions, Neetcode 150, Arrays and Hashing, exercise 5, retrial part 2 using heap

--- Leetcode Exercises/Neetcode 150/Arrays and Hashing/retry - part 2/top_k_freq_elem.py
+++ Leetcode Exercises/Neetcode 150/Arrays and Hashing/retry - part 2/top_k_freq_elem.py
@@ -5,22 +5,39 @@ The test cases are generated such that the answer is always unique.
 
 You may return the output in any order.
 """
+import heapq
 from typing import List
 
 class Solution:
     def top_k_element(self, nums: List[int], k: int) -> List[int]:
+        # count = {}
+        # for num in nums:
+        #     count[num] = 1 + count.get(num, 0)
+        
+        # arry = []
+        # for num, cnt in count.items():
+        #     arry.append([cnt, num])
+        # arry.sort()
+
+        # result = []
+        # while len(result) < k:
+        #     result.append(arry.pop()[1])
+        # return result
+
+        #Part 2
         count = {}
         for num in nums:
             count[num] = 1 + count.get(num, 0)
         
-        arry = []
-        for num, cnt in count.items():
-            arry.append([cnt, num])
-        arry.sort()
-
+        heap = []
+        for num in count.keys():
+            heapq.heappush(heap, (count[num], num))
+            if len(heap) > k:
+                heapq.heappop(heap)
+        
         result = []
-        while len(result) < k:
-            result.append(arry.pop()[1])
+        for i in range(k):
+            result.append(heapq.heappop(heap)[1])
         return result
 
 nums = [1,2,2,3,3,3]
