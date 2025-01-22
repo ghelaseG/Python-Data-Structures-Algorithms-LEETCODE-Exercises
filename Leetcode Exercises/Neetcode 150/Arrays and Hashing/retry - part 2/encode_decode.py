38843eb5b30eefa6e19ad71e207d976e2fc8f645
ghelaseG <ghelase.georgian@gmail.com>
2025-01-22 00:16:12 +0000

    Practice Questions, Neetcode 150, Arrays and Hashing, exercise 6, retrial part 1

--- Leetcode Exercises/Neetcode 150/Arrays and Hashing/retry - part 2/encode_decode.py
+++ Leetcode Exercises/Neetcode 150/Arrays and Hashing/retry - part 2/encode_decode.py
@@ -6,8 +6,38 @@ Please implement encode and decode
 
 class Solution:
     def encode(self, strs: List[str]) -> str:
+        if not strs:
+            return ''
+        sizes = []
+        result = ''
+        for s in strs:
+            sizes.append(len(s))
+        for sz in sizes:
+            result += str(sz)
+            result += ","
+        result += "#"
+        for s in strs:
+            result += s
+        return result
     
     def decode(self, s: str) -> List[str]:
-        
+        if not s:
+            return []
+        sizes = []
+        result = []
+        i = 0
+        while s[i] != '#':
+            curr = ''
+            while s[i] != ",":
+                curr += s[i]
+                i += 1
+            sizes.append(int(curr))
+            i += 1
+        i += 1
+        for sz in sizes:
+            result.append(s[i:i + sz])
+            i += sz
+        return result
+
 
         
