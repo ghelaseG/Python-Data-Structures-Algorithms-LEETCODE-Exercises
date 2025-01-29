afc6bf8c0ff9beffcac68b78a6872e4a63ed3058
ghelaseG <ghelase.georgian@gmail.com>
2025-01-29 21:45:35 +0000

    Practice Questions, Neetcode 150, Arrays and Hashing, exercise 8, part 2 using hash set - one pass

--- Leetcode Exercises/Neetcode 150/Arrays and Hashing/retry - part 2/valid_sudoku.py
+++ Leetcode Exercises/Neetcode 150/Arrays and Hashing/retry - part 2/valid_sudoku.py
@@ -9,41 +9,64 @@ Return true if the Sudoku board is valid, otherwise return false
 Note: A board does not need to be full or be solvable to be valid.
 """
 from typing import List
+from collections import defaultdict
 
 class Solution:
     def check_sudoku(self, board: List[List[str]]) -> bool:
-        for row in range(9):
-            seen = set()
-            for i in range(9):
-                if board[row][i] == ".":
-                    continue
-                if board[row][i] in seen:
-                    return False
-                seen.add(board[row][i])
+        # part 1
+        # for row in range(9):
+        #     seen = set()
+        #     for i in range(9):
+        #         if board[row][i] == ".":
+        #             continue
+        #         if board[row][i] in seen:
+        #             return False
+        #         seen.add(board[row][i])
         
-        for col in range(9):
-            seen = set()
-            for i in range(9):
-                if board[i][col] == ".":
-                    continue
-                if board[i][col] in seen:
-                    return False
-                seen.add(board[i][col])
+        # for col in range(9):
+        #     seen = set()
+        #     for i in range(9):
+        #         if board[i][col] == ".":
+        #             continue
+        #         if board[i][col] in seen:
+        #             return False
+        #         seen.add(board[i][col])
         
-        for square in range(9):
-            seen = set()
-            for i in range(3):
-                for j in range(3):
-                    row = (square//3) * 3 + i
-                    col = (square % 3) * 3 + j
-                    if board[row][col] == ".":
-                        continue
-                    if board[row][col] in seen:
-                        return False
-                    seen.add(board[row][col])
+        # for square in range(9):
+        #     seen = set()
+        #     for i in range(3):
+        #         for j in range(3):
+        #             row = (square//3) * 3 + i
+        #             col = (square % 3) * 3 + j
+        #             if board[row][col] == ".":
+        #                 continue
+        #             if board[row][col] in seen:
+        #                 return False
+        #             seen.add(board[row][col])
         
-        return True
+        # return True
 
+        # part 2
+
+        cols = defaultdict(set)
+        rows = defaultdict(set)
+        squares = defaultdict(set)
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
+                    continue
+                if ( board[r][c] in rows [r] 
+                    or board[r][c] in cols[c] 
+                    or board[r][c] in squares[(r // 3, c // 3)]):
+                    return False 
+
+                cols[c].add(board[r][c])
+                rows[r].add(board[r][c])
+                squares[(r // 3, c // 3)].add(board[r][c])
+            
+        return True
+    
 board = [["1","2",".",".","3",".",".",".","."],
  ["4",".",".","5",".",".",".",".","."],
  [".","9","8",".",".",".",".",".","3"],
