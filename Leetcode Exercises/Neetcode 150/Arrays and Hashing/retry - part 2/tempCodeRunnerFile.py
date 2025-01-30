1844a0d4f48e1309687c8c8972e979719f2d7fc1
ghelaseG <ghelase.georgian@gmail.com>
2025-01-30 23:14:07 +0000

    Practice Questions, Neetcode 150, Arrays and Hashing, exercise 8, part 3 using bitmask

--- Leetcode Exercises/Neetcode 150/Arrays and Hashing/retry - part 2/tempCodeRunnerFile.py
+++ Leetcode Exercises/Neetcode 150/Arrays and Hashing/retry - part 2/tempCodeRunnerFile.py
@@ -48,23 +48,48 @@ class Solution:
 
         # part 2
 
-        cols = defaultdict(set)
-        rows = defaultdict(set)
-        squares = defaultdict(set)
+        # cols = defaultdict(set)
+        # rows = defaultdict(set)
+        # squares = defaultdict(set)
+
+        # for r in range(9):
+        #     for c in range(9):
+        #         if board[r][c] == ".":
+        #             continue
+        #         if ( board[r][c] in rows [r] 
+        #             or board[r][c] in cols[c] 
+        #             or board[r][c] in squares[(r // 3, c // 3)]):
+        #             return False 
+
+        #         cols[c].add(board[r][c])
+        #         rows[r].add(board[r][c])
+        #         squares[(r // 3, c // 3)].add(board[r][c])
+            
+        # return True
+
+        # part 3
+
+        rows = [0] * 9
+        cols = [0] * 9
+        squares = [0] * 9
 
         for r in range(9):
             for c in range(9):
                 if board[r][c] == ".":
                     continue
-                if ( board[r][c] in rows [r] 
-                    or board[r][c] in cols[c] 
-                    or board[r][c] in squares[(r // 3, c // 3)]):
-                    return False 
 
-                cols[c].add(board[r][c])
-                rows[r].add(board[r][c])
-                squares[(r // 3, c // 3)].add(board[r][c])
-            
+                val = int(board[r][c]) - 1
+                if (1 << val) & rows[r]:
+                    return False
+                if (1 << val) & cols[c]:
+                    return False
+                if (1 << val) & squares[(r // 3) * 3 + (c // 3)]:
+                    return False
+                
+                rows[r] |= (1 << val)
+                cols[c] |= (1 << val)
+                squares[(r // 3) * 3 + (c // 3)] |= (1 << val)
+        
         return True
     
 board = [["1","2",".",".","3",".",".",".","."],
