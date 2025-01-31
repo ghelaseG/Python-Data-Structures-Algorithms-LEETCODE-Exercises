6098eb9cdc74c43a69099f237ec12412f92c6615
ghelaseG <ghelase.georgian@gmail.com>
2025-01-31 23:09:52 +0000

    Practice Questions, Neetcode 150, Arrays and Hashing, exercise 1, contains duplicate - part 1 using brute force

---
+++ Leetcode Exercises/Neetcode 150/Arrays and Hashing/retry - part 3/contains_dupl.py
@@ -0,0 +1,16 @@
+"""
+Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+"""
+from typing import List
+
+class Solution:
+    def contains_duplicate(self, nums: List[int]) -> bool:
+        # part 1 using brute force
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] == nums[j]:
+                    return True
+        return False
+    
+nums = [1, 2, 3, 3]
+print(Solution().contains_duplicate(nums))
