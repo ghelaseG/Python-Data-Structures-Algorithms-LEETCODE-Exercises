ea9659286134413d78eca793f73963ccc760a3e8
ghelaseG <ghelase.georgian@gmail.com>
2025-02-01 20:33:30 +0000

    Practice Questions, Neetcode 150, Arrays and Hashing, exercise 2, valid anagram - part 2 using hash table

--- Leetcode Exercises/Neetcode 150/Arrays and Hashing/retry - part 3/valid_anagram.py
+++ Leetcode Exercises/Neetcode 150/Arrays and Hashing/retry - part 3/valid_anagram.py
@@ -6,12 +6,24 @@ An anagram is a string that contains the exact same characters as another string
 
 class Solution:
     def chech_is_anagram(self, s: str, t: str) -> bool:
-        # part 1
+        # # part 1
+        # if len(s) != len(t):
+        #     return False
+        
+        # return sorted(s) == sorted(t)
+        
+        # part 2
         if len(s) != len(t):
             return False
         
-        return sorted(s) == sorted(t)
+        countS = {}
+        countT = {}
 
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+        return countS == countT
+    
 s = "racecar"
 t = "carrace"
 print(Solution().chech_is_anagram(s, t))
