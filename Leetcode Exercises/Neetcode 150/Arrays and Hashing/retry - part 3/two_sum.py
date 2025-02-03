c87a71ca0a5659e086a04a7ae242feef07e5ebb0
ghelaseG <ghelase.georgian@gmail.com>
2025-02-03 23:18:14 +0000

    Practice Questions, Neetcode 150, Arrays and Hashing, exercise 3, two sum - part 2 using sorting

--- Leetcode Exercises/Neetcode 150/Arrays and Hashing/retry - part 3/two_sum.py
+++ Leetcode Exercises/Neetcode 150/Arrays and Hashing/retry - part 3/two_sum.py
@@ -12,12 +12,29 @@ from typing import List
 class Solution:
     def two_sum(self, nums: List[int], target: int) -> List[int]:
         # part 1 using brute force
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+        # for i in range(len(nums)):
+        #     for j in range(i + 1, len(nums)):
+        #         if nums[i] + nums[j] == target:
+        #             return [i, j]
         
-    
+        # part 2 using sorting
+        A = []
+        for i, num in enumerate(nums):
+            A.append([num, i])
+        
+        A.sort()
+        i, j = 0, len(nums) - 1
+        while i < j:
+            cur = A[i][0] + A[j][0]
+            if cur == target:
+                return [min(A[i][1], A[j][1]),
+                        max(A[i][1], A[j][1])]
+            elif cur < target:
+                i += 1
+            else:
+                j -= 1
+        return []
+
 nums = [4,5,6]
 target = 10
 print(Solution().two_sum(nums, target))
