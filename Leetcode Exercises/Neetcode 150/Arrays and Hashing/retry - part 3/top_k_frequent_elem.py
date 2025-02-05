0695b1ccfebe365edd4c69109056fb38827109d8
ghelaseG <ghelase.georgian@gmail.com>
2025-02-05 20:46:23 +0000

    Practice Questions, Neetcode 150, Arrays and Hashing, exercise 5, top k frequent element - part 3 using bucket sort

--- Leetcode Exercises/Neetcode 150/Arrays and Hashing/retry - part 3/top_k_frequent_elem.py
+++ Leetcode Exercises/Neetcode 150/Arrays and Hashing/retry - part 3/top_k_frequent_elem.py
@@ -27,20 +27,36 @@ class Solution:
         #     result.append(arr.pop()[1])
         # return result
 
-        # part 2 using heap
+        # # part 2 using heap
+        # count = {}
+        # for num in nums:
+        #     count[num] = 1 + count.get(num, 0)
+        
+        # heap = []
+        # for num in count.keys():
+        #     heapq.heappush(heap, (count[num], num))
+        #     if len(heap) > k:
+        #         heapq.heappop(heap)
+        
+        # result = []
+        # for i in range(k):
+        #     result.append(heapq.heappop(heap)[1])
+        # return result
+
+        # part 3 using bucket sort
         count = {}
+        freq = [[] for i in range(len(nums) + 1)]
+
         for num in nums:
             count[num] = 1 + count.get(num, 0)
-        
-        heap = []
-        for num in count.keys():
-            heapq.heappush(heap, (count[num], num))
-            if len(heap) > k:
-                heapq.heappop(heap)
+        for num, cnt in count.items():
+            freq[cnt].append(num)
         
         result = []
-        for i in range(k):
-            result.append(heapq.heappop(heap)[1])
-        return result
+        for i in range(len(freq) - 1, 0, -1):
+            for num in freq[i]:
+                result.append(num)
+                if len(result) == k:
+                    return result
 
 print(Solution().top_k_frequent_element(nums = [1,2,2,3,3,3], k = 2))
