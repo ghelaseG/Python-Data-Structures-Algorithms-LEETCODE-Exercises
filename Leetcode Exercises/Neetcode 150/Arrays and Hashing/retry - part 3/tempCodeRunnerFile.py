4b8a482507ddce80615f5546d9650b98e7f79c0c
ghelaseG <ghelase.georgian@gmail.com>
2025-02-05 20:36:39 +0000

    Practice Questions, Neetcode 150, Arrays and Hashing, exercise 5, top k frequent element - part 2 using heap

--- Leetcode Exercises/Neetcode 150/Arrays and Hashing/retry - part 3/tempCodeRunnerFile.py
+++ Leetcode Exercises/Neetcode 150/Arrays and Hashing/retry - part 3/tempCodeRunnerFile.py
@@ -7,23 +7,40 @@ You may return the output in any order.
 """
 
 from typing import List
+import heapq
 
 class Solution:
     def top_k_frequent_element(self, nums: List[int], k: int) -> List[int]:
         #part 1 using sorting
 
+        # count = {}
+        # for num in nums:
+        #     count[num] = 1 + count.get(num, 0)
+        
+        # arr = []
+        # for num, cnt in count.items():
+        #     arr.append([cnt, num])
+        # arr.sort()
+
+        # result = []
+        # while len(result) < k:
+        #     result.append(arr.pop()[1])
+        # return result
+
+        # part 2 using heap
         count = {}
         for num in nums:
             count[num] = 1 + count.get(num, 0)
         
-        arr = []
-        for num, cnt in count.items():
-            arr.append([cnt, num])
-        arr.sort()
-
+        heap = []
+        for num in count.keys():
+            heapq.heappush(heap, (count[num], num))
+            if len(heap) > k:
+                heapq.heappop(heap)
+        
         result = []
-        while len(result) < k:
-            result.append(arr.pop()[1])
+        for i in range(k):
+            result.append(heapq.heappop(heap)[1])
         return result
 
 print(Solution().top_k_frequent_element(nums = [1,2,2,3,3,3], k = 2))
