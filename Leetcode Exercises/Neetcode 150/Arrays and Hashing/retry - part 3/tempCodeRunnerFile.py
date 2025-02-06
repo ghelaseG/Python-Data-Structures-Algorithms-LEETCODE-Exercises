09ba1ed65f0841a783d37fbc7b7e6530ae0693ce
ghelaseG <ghelase.georgian@gmail.com>
2025-02-06 23:09:36 +0000

    Practice Questions, Neetcode 150, Arrays and Hashing, exercise 6, encode decode - part 2 optimal version

--- Leetcode Exercises/Neetcode 150/Arrays and Hashing/retry - part 3/tempCodeRunnerFile.py
+++ Leetcode Exercises/Neetcode 150/Arrays and Hashing/retry - part 3/tempCodeRunnerFile.py
@@ -8,42 +8,64 @@ from typing import List
 
 class Solution:
     #part 1
+    
+    # def encode(self, strs: List[str]) -> str:
+    #     if not strs:
+    #         return ""
+    #     sizes = []
+    #     result = ""
+    #     for s in strs:
+    #         sizes.append(len(s))
+    #     for sz in sizes:
+    #         result += str(sz)
+    #         result += ','
+    #     result += '#'
+    #     for s in strs:
+    #         result += s
+    #     return result
+
+    # def decode(self, s: str) -> List[str]:
+    #     if not s:
+    #         return []
+    #     sizes = []
+    #     result = []
+    #     i = 0
+    #     while s[i] != '#':
+    #         curr = ""
+    #         while s[i] != ',':
+    #             curr += s[i]
+    #             i += 1
+    #         sizes.append(int(curr))
+    #         i += 1
+    #     i += 1
+    #     for sz in sizes:
+    #         result.append(s[i:i + sz])
+    #         i += sz
+    #     return result
+
+    #part 2
     def encode(self, strs: List[str]) -> str:
-        if not strs:
-            return ""
-        sizes = []
         result = ""
         for s in strs:
-            sizes.append(len(s))
-        for sz in sizes:
-            result += str(sz)
-            result += ','
-        result += '#'
-        for s in strs:
-            result += s
+            result += str(len(s)) + "#" + s
         return result
-
     def decode(self, s: str) -> List[str]:
-        if not s:
-            return []
-        sizes = []
         result = []
         i = 0
-        while s[i] != '#':
-            curr = ""
-            while s[i] != ',':
-                curr += s[i]
-                i += 1
-            sizes.append(int(curr))
-            i += 1
-        i += 1
-        for sz in sizes:
-            result.append(s[i:i + sz])
-            i += sz
+
+        while i < len(s):
+            j = i
+            while s[j] != '#':
+                j += 1
+            length = int(s[i:j])
+            i = j + 1
+            j = i + length
+            result.append(s[i:j])
+            i = j
+        
         return result
 
-
-strs = ["george","code","love","you"]
+strs = ["george","code","loves","you"]
 print("Encode:")
 encode1 = Solution().encode(strs)
 print(encode1)
