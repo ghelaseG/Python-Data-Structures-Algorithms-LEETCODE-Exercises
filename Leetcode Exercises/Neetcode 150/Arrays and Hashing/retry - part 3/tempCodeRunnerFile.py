f737caaf7ffed6da3d339415a3480eacdce93e9e
ghelaseG <ghelase.georgian@gmail.com>
2025-02-07 23:57:46 +0000

    Practice Questions, Neetcode 150, Arrays and Hashing, exercise 7, products of array except self, part 2 using prefix and suffix

--- Leetcode Exercises/Neetcode 150/Arrays and Hashing/retry - part 3/tempCodeRunnerFile.py
+++ Leetcode Exercises/Neetcode 150/Arrays and Hashing/retry - part 3/tempCodeRunnerFile.py
@@ -9,17 +9,37 @@ from typing import List
 
 class Solution:
     def product_except_self(self, nums: List[int]) -> List[int]:
+        
+        #part 1 using brute force
+
+        # n = len(nums)
+        # result = [0] * n
+
+        # for i in range(n):
+        #     prod = 1
+        #     for j in range(n):
+        #         if i == j:
+        #             continue
+        #         prod *= nums[j]
+            
+        #     result[i] = prod
+        # return result
+
+        # part 2 using prefix & suffix
+
         n = len(nums)
         result = [0] * n
+        pref = [0] * n
+        suff = [0] * n
 
+        pref[0] = suff[n - 1] = 1
+
+        for i in range(1, n):
+            pref[i] = nums[i - 1] * pref[i - 1]
+        for i in range(n - 2, -1, -1):
+            suff[i] = nums[i + 1] * suff[i + 1]
         for i in range(n):
-            prod = 1
-            for j in range(n):
-                if i == j:
-                    continue
-                prod *= nums[j]
-            
-            result[i] = prod
+            result[i] = pref[i] * suff[i]
         return result
-
+    
 print(Solution().product_except_self(nums = [1,2,4,6]))
