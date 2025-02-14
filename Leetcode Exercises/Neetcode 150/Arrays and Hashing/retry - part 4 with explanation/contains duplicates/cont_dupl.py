e19b75026338a67189b3fae366e955e6ff99bc9c
ghelaseG <ghelase.georgian@gmail.com>
2025-02-14 21:25:17 +0000

    Practice Questions, Neetcode 150, Arrays and Hashing, exercise 1, contains duplicates - part 2 using sorting with explanation

--- Leetcode Exercises/Neetcode 150/Arrays and Hashing/retry - part 4 with explanation/contains duplicates/cont_dupl.py
+++ Leetcode Exercises/Neetcode 150/Arrays and Hashing/retry - part 4 with explanation/contains duplicates/cont_dupl.py
@@ -5,11 +5,19 @@ from typing import List
 
 class Solution:
     def contains_duplicates(self, nums: List[int]) -> bool:
-        # part 1 using brute force
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if nums[i] == nums[j]:
-                    return True
+        # # part 1 using brute force
+        # for i in range(len(nums)):
+        #     for j in range(i + 1, len(nums)):
+        #         if nums[i] == nums[j]:
+        #             return True
+        # return False
+    
+        #part 2 using sorting
+
+        nums.sort()
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i-1]:
+                return True
         return False
 
 print(Solution().contains_duplicates(nums=[1,2,3,4,1]))
