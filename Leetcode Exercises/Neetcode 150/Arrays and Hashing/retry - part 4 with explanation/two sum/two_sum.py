46601ddb1b6a4746fdb4ea80f208281413432437
ghelaseG <ghelase.georgian@gmail.com>
2025-02-20 20:16:16 +0000

    Practice Questions, Neetcode 150, Arrays and Hashing, exercise 3, two sum - part 3 using hash map - two pass with explanation

--- Leetcode Exercises/Neetcode 150/Arrays and Hashing/retry - part 4 with explanation/two sum/two_sum.py
+++ Leetcode Exercises/Neetcode 150/Arrays and Hashing/retry - part 4 with explanation/two sum/two_sum.py
@@ -15,26 +15,36 @@ class Solution:
         #             return [i, j]
         # return []
 
-        # part 2 using sorting
-        A = []
-        for i, num in enumerate(nums):
-            A.append([num, i])
+        # # part 2 using sorting
+        # A = []
+        # for i, num in enumerate(nums):
+        #     A.append([num, i])
         
-        A.sort()
-        i = 0
-        j = len(nums) - 1
-        while i < j:
-            cur = A[i][0] + A[j][0]
-            if cur == target:
-                return [min(A[i][1], A[j][1]),
-                        max(A[i][1], A[j][1])]
-            elif cur < target:
-                i += 1
-            else:
-                j -= 1
-        return []
-            
-            
+        # A.sort()
+        # i = 0
+        # j = len(nums) - 1
+        # while i < j:
+        #     cur = A[i][0] + A[j][0]
+        #     if cur == target:
+        #         return [min(A[i][1], A[j][1]),
+        #                 max(A[i][1], A[j][1])]
+        #     elif cur < target:
+        #         i += 1
+        #     else:
+        #         j -= 1
+        # return []
+        
+        # part 3 using hash map - two pass
+        indices = {}
+
+        for i, num in enumerate(nums):
+            indices[num] = i
+        
+        for i, num in enumerate(nums):
+            diff = target - num
+            if diff in indices and indices[diff] != i:
+                return [i, indices[diff]]
+
 
 
 print(Solution().two_sum(nums=[3,4,5,6], target=7))
