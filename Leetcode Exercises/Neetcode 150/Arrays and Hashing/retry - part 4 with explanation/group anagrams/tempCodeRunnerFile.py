a9db1578919f666454d236ebab0da6dbafb9c9fb
ghelaseG <ghelase.georgian@gmail.com>
2025-02-22 23:15:40 +0000

    Practice Questions, Neetcode 150, Arrays and Hashing, exercise 4, group anagrams - using hash table

--- Leetcode Exercises/Neetcode 150/Arrays and Hashing/retry - part 4 with explanation/group anagrams/tempCodeRunnerFile.py
+++ Leetcode Exercises/Neetcode 150/Arrays and Hashing/retry - part 4 with explanation/group anagrams/tempCodeRunnerFile.py
@@ -7,10 +7,20 @@ from collections import defaultdict
 
 class Solution():
     def group_anagrams(self, strs: list[str]) -> list[list[str]]:
+        # part 1 using sorting
+        # result = defaultdict(list)
+        # for s in strs:
+        #     sorted_s = ''.join(sorted(s))
+        #     result[sorted_s].append(s)
+        # return list(result.values())
+
+        # part 2 using hash table
         result = defaultdict(list)
         for s in strs:
-            sorted_s = ''.join(sorted(s))
-            result[sorted_s].append(s)
+            count = [0] * 26
+            for char in s:
+                count[ord(char) - ord('a')] += 1
+            result[tuple(count)].append(s)
         return list(result.values())
     
 print(Solution().group_anagrams(strs = ["act","pots","tops","cat","stop","hat"]))
