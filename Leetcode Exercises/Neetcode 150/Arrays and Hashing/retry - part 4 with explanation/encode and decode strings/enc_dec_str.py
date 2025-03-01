203804e5a75b176690cb7684081a684bef15479e
ghelaseG <ghelase.georgian@gmail.com>
2025-03-01 23:51:24 +0000

    Practice Questions, Neetcode 150, Arrays and Hashing, exercise 6, encode and decode strings, mock

---
+++ Leetcode Exercises/Neetcode 150/Arrays and Hashing/retry - part 4 with explanation/encode and decode strings/enc_dec_str.py
@@ -0,0 +1,11 @@
+"""
+Design an algorithm to encode a list of strings to a single string. The encoded string is then decoded back to the original list of strings.
+
+Please implement encode and decode
+"""
+
+class Solution:
+    def encode(self, strs: list[str]) -> str:
+    
+    def decode(self, s: str) -> list[str]:
+        
