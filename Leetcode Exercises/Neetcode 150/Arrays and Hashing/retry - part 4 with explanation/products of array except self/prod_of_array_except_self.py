174a515e1cd89bec7a4394805bac3e9417f6a8ad
ghelaseG <ghelase.georgian@gmail.com>
2025-03-06 22:05:00 +0000

    Practice Questions, Neetcode 150, Arrays and Hashing, exercise 7, product of array except self, part 2 using division

--- Leetcode Exercises/Neetcode 150/Arrays and Hashing/retry - part 4 with explanation/products of array except self/prod_of_array_except_self.py
+++ Leetcode Exercises/Neetcode 150/Arrays and Hashing/retry - part 4 with explanation/products of array except self/prod_of_array_except_self.py
@@ -10,19 +10,40 @@ class Solution:
     def products_of_array_except_self(self, nums: list[int]) -> list[int]:
 
         # part 1 using brute force
-        n = len(nums)
-        result = [0] * n
-
-        for i in range(n):
-            prod = 1
-            for j in range(n):
-                if i == j:
-                    continue
-                prod *= nums[j]
-            
-            result[i] = prod
+        # n = len(nums)
+        # result = [0] * n
         
+        # for i in range(n):
+        #     prod = 1
+        #     for j in range(n):
+        #         if i == j:
+        #             continue
+        #         prod *= nums[j]
+            
+        #     result[i] = prod
+        
+        # return result
+
+        # part 2 using division
+        product = 1
+        zero_cnt = 0
+
+        for num in nums:
+            if num:
+                product *= num
+            else:
+                zero_cnt += 1
+        if zero_cnt > 1:
+            return [0] * len(nums)
+        
+        result = [0] * len(nums)
+        for i, c in enumerate(nums):
+            if zero_cnt:
+                result[i] = 0 if c else product
+            else:
+                result[i] = product // c
         return result
+
 print(Solution().products_of_array_except_self(nums = [1,2,4,6]))
 
 
