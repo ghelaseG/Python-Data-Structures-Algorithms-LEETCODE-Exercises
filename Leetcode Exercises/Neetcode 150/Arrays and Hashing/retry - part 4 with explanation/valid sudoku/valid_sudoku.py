22860cb5157b1f49eaf08dfe8ddecfd31d7a8e69
ghelaseG <ghelase.georgian@gmail.com>
2025-03-15 21:24:49 +0000

    Practice Questions, Neetcode 150, Arrays and Hashing, exercise 8, valid sudoku, part 2 using hash set - one pass

--- Leetcode Exercises/Neetcode 150/Arrays and Hashing/retry - part 4 with explanation/valid sudoku/valid_sudoku.py
+++ Leetcode Exercises/Neetcode 150/Arrays and Hashing/retry - part 4 with explanation/valid sudoku/valid_sudoku.py
@@ -8,40 +8,62 @@ Return true if the Sudoku board is valid, otherwise return false
 
 Note: A board does not need to be full or be solvable to be valid.
 """
+from collections import defaultdict
 
 class Solution:
     def is_valid_sudoku(self, board: list[list[str]]) -> bool:
-        for row in range(9):
-            seen = set()
-            for i in range(9):
-                if board[row][i] == ".":
-                    continue
-                if board[row][i] in seen:
-                    return False
-                seen.add(board[row][i])
-        
-        for col in range(9):
-            seen = set()
-            for i in range(9):
-                if board[i][col] == ".":
-                    continue
-                if board[i][col] in seen:
-                    return False
-                seen.add(board[i][col])
-        
-        for square in range(9):
-            seen = set()
-            for i in range(3):
-                for j in range(3):
-                    row = (square//3) * 3 + i
-                    col = (square % 3) * 3 + j
-                    if board[row][col] == ".":
-                        continue
-                    if board[row][col] in seen:
-                        return False
-                    seen.add(board[row][col])
-        return True
+        # part 1 using brute force
 
+        # for row in range(9):
+        #     seen = set()
+        #     for i in range(9):
+        #         if board[row][i] == ".":
+        #             continue
+        #         if board[row][i] in seen:
+        #             return False
+        #         seen.add(board[row][i])
+        
+        # for col in range(9):
+        #     seen = set()
+        #     for i in range(9):
+        #         if board[i][col] == ".":
+        #             continue
+        #         if board[i][col] in seen:
+        #             return False
+        #         seen.add(board[i][col])
+        
+        # for square in range(9):
+        #     seen = set()
+        #     for i in range(3):
+        #         for j in range(3):
+        #             row = (square//3) * 3 + i
+        #             col = (square % 3) * 3 + j
+        #             if board[row][col] == ".":
+        #                 continue
+        #             if board[row][col] in seen:
+        #                 return False
+        #             seen.add(board[row][col])
+        # return True
+
+        # part 2 using hash set - one pass
+
+        cols = defaultdict(set)
+        rows = defaultdict(set)
+        squares = defaultdict(set)
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
+                    continue
+                if (board[r][c] in rows[r] or board[r][c] in cols[c] or board[r][c] in squares[(r // 3, c // 3)]):
+                    return False
+                
+                cols[c].add(board[r][c])
+                rows[r].add(board[r][c])
+                squares[(r // 3, c // 3)].add(board[r][c])
+
+        return True
+    
 print(Solution().is_valid_sudoku(board = 
 [["1","2",".",".","3",".",".",".","."],
  ["4",".",".","5",".",".",".",".","."],
