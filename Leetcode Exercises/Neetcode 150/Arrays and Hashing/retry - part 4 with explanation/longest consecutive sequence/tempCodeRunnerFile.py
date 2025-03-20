39dbca2de0822a34de88aab2184bd47ba489192c
ghelaseG <ghelase.georgian@gmail.com>
2025-03-20 19:36:59 +0000

    Practice Questions, Neetcode 150, Arrays and Hashing, exercise 9, longest consecutive sequence, part 2 using sorting

--- Leetcode Exercises/Neetcode 150/Arrays and Hashing/retry - part 4 with explanation/longest consecutive sequence/tempCodeRunnerFile.py
+++ Leetcode Exercises/Neetcode 150/Arrays and Hashing/retry - part 4 with explanation/longest consecutive sequence/tempCodeRunnerFile.py
@@ -10,15 +10,36 @@ class Solution:
     def longest_cons_sequence(self, nums: list[int]) -> int:
 
         # part 1 using brute force
-        result = 0
-        store = set(nums)
+        # result = 0
+        # store = set(nums)
 
-        for num in nums:
-            streak = 0
-            curr = num
-            while curr in store:
-                streak += 1
-                curr += 1
+        # for num in nums:
+        #     streak = 0
+        #     curr = num
+        #     while curr in store:
+        #         streak += 1
+        #         curr += 1
+        #     result = max(result, streak)
+        # return result
+
+        # part 2 using sorting
+        if not nums:
+            return 0
+        
+        result = 0
+        nums.sort()
+
+        curr = nums[0]
+        streak = 0
+        i = 0
+        while i < len(nums):
+            if curr != nums[i]:
+                curr = nums[i]
+                streak = 0
+            while i < len(nums) and nums[i] == curr:
+                i += 1
+            streak += 1
+            curr += 1
             result = max(result, streak)
         return result
 
